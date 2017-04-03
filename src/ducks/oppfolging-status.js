f37d051a9574d41cb0956eff6ef23b76b52fa419
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-04-03 15:21:51 +0200

    la til mulighet for avslag av vilkar

--- src/ducks/oppfolging-status.js
+++ src/ducks/oppfolging-status.js
@@ -6,6 +6,8 @@ export const OK = 'oppfolgingStatus/OK';
 export const FEILET = 'oppfolgingStatus/FEILET';
 export const PENDING = 'oppfolgingStatus/PENDING';
 
+export const AVSLAG = 'oppfolgingStatus/avslag';
+
 export const GODTA_OK = 'oppfolgingStatus/godta/OK';
 export const GODTA_FEILET = 'oppfolgingStatus/godta/FEILET';
 export const GODTA_PENDING = 'oppfolgingStatus/godta/PNEDING';
@@ -27,6 +29,8 @@ export default function reducer(state = initalState, action) {
             return { ...state, status: STATUS.OK, data: action.data };
         case GODTA_OK:
             return { ...state, data: action.data };
+        case AVSLAG:
+            return { ...state, data: { ...state.data, avslag: true } };
         default:
             return state;
     }
@@ -48,3 +52,8 @@ export function godtaVilkar(hash) {
         PENDING: GODTA_PENDING
     });
 }
+
+export function avslaVilkar() {
+    return (dipatch) => dipatch({ type: AVSLAG });
+}
+
