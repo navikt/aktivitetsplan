059247fd1d2ba3e763ef63a1f4d5b30bc20171a5
alex <alexander.svendsen@nav.no>
2017-04-05 13:00:08 +0200

    Lag tester for dato validering koden

--- src/modal/skjema/datovelger/datovelger.js
+++ src/modal/skjema/datovelger/datovelger.js
@@ -4,43 +4,9 @@ import { autofill, touch } from 'redux-form';
 import { CustomField } from 'react-redux-form-validation';
 import { connect } from 'react-redux';
 import MaskedInput from 'react-maskedinput';
-import moment from 'moment';
-import { autobind, dateToISODate, toDatePrettyPrint, erGyldigISODato, ISODateToDatePicker, datePickerToISODate, erGyldigFormattertDato } from '../../../utils';
+import { autobind, dateToISODate, erGyldigISODato, ISODateToDatePicker, datePickerToISODate, erGyldigFormattertDato } from '../../../utils';
+import { validerDatoField } from './utils'
 import DayPickerComponent from './day-picker';
-
-function validerPeriode(input, alternativer) {
-    const { fra, til } = alternativer;
-    const inputDato = moment(input);
-
-    const fraDato = moment(fra);
-    const tilDato = moment(til);
-
-    if (fra && til && (inputDato.isAfter(tilDato, 'day') || fraDato.isAfter(inputDato, 'day'))) {
-        tilDato.add(1, 'day');
-        fraDato.subtract(1, 'day');
-        return `Datoen må være innenfor perioden ${toDatePrettyPrint(fraDato.toDate())}-${toDatePrettyPrint(tilDato.toDate())}`;
-    }
-    if (til && inputDato.isAfter(tilDato, 'day')) {
-        tilDato.add(1, 'day');
-        return `Datoen må være før ${toDatePrettyPrint(tilDato.toDate())}`;
-    }
-    if (fra && fraDato.isAfter(inputDato, 'day')) {
-        fraDato.subtract(1, 'day');
-        return `Datoen må være etter ${toDatePrettyPrint(fraDato.toDate())}`;
-    }
-    return undefined;
-}
-
-function validerDatoField(input, alternativer) {
-    if (!input) {
-        return undefined;
-    } else if (!erGyldigISODato(input)) {
-        return 'Ugyldig dato';
-    } else if (alternativer && (alternativer.fra || alternativer.til)) {
-        return validerPeriode(input, alternativer);
-    }
-    return undefined;
-}
 
 function stopEvent(event) {
     try {
--- src/modal/skjema/datovelger/utils.js
+++ src/modal/skjema/datovelger/utils.js
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import { toDatePrettyPrint, erGyldigISODato } from '../../../utils';
+
 export function dateLessOrEqual(date1, date2) {
     const year1 = date1.getFullYear();
     const year2 = date2.getFullYear();
@@ -11,7 +14,7 @@ export function dateLessOrEqual(date1, date2) {
     if (year1 < year2) return true;
     else if (year1 === year2 && mon1 < mon2) return true;
 
-    return year1 === year2 && mon1 === mon2 && day1 < day2;
+    return year1 === year2 && mon1 === mon2 && day1 <= day2;
 }
 
 export function dateGreaterOrEqual(date1, date2) {
@@ -27,5 +30,39 @@ export function dateGreaterOrEqual(date1, date2) {
     if (year1 > year2) return true;
     else if (year1 === year2 && mon1 > mon2) return true;
 
-    return year1 === year2 && mon1 === mon2 && day1 > day2;
+    return year1 === year2 && mon1 === mon2 && day1 >= day2;
+}
+
+export function validerPeriode(input, alternativer) {
+    const { fra, til } = alternativer;
+    const inputDato = moment(input);
+
+    const fraDato = moment(fra);
+    const tilDato = moment(til);
+
+    if (fra && til && (inputDato.isAfter(tilDato, 'day') || fraDato.isAfter(inputDato, 'day'))) {
+        tilDato.add(1, 'day');
+        fraDato.subtract(1, 'day');
+        return `Datoen må være innenfor perioden ${toDatePrettyPrint(fraDato.toDate())}-${toDatePrettyPrint(tilDato.toDate())}`;
+    }
+    if (til && inputDato.isAfter(tilDato, 'day')) {
+        tilDato.add(1, 'day');
+        return `Datoen må være før ${toDatePrettyPrint(tilDato.toDate())}`;
+    }
+    if (fra && fraDato.isAfter(inputDato, 'day')) {
+        fraDato.subtract(1, 'day');
+        return `Datoen må være etter ${toDatePrettyPrint(fraDato.toDate())}`;
+    }
+    return undefined;
+}
+
+export function validerDatoField(input, alternativer) {
+    if (!input) {
+        return undefined;
+    } else if (!erGyldigISODato(input)) {
+        return 'Ugyldig dato';
+    } else if (alternativer && (alternativer.fra || alternativer.til)) {
+        return validerPeriode(input, alternativer);
+    }
+    return undefined;
 }
