08904c5d551a75642112fb06a2c9a6b0f8c3190f
alex <alexander.svendsen@nav.no>
2017-04-21 15:19:49 +0200

    Sett linken til å linke til eksterne url-er

--- src/modal/visning/aktivitetsdetaljer.js
+++ src/modal/visning/aktivitetsdetaljer.js
@@ -57,9 +57,11 @@ function Aktivitetsdetaljer({ valgtAktivitet, className }) {
         }
     };
 
+    const httpRegex = /^(https?):\/\/.*$/;
+
     const lenkeKomponent = () => (
         <DetaljFelt key="lenke" tittel="Lenke" visible={lenke != null}>
-            <Link href={lenke} className="detaljfelt__lenke" target="_blank">
+            <Link href={lenke.match(httpRegex) ? lenke : `http://${lenke}`} className="detaljfelt__lenke" target="_blank">
                 {lenke}
             </Link>
         </DetaljFelt>
