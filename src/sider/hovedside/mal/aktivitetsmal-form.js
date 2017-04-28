c0c7c733e52540558c6e5ad58e209281c9ecea4d
Steffen Hageland <steffen.hageland@nav.no>
2017-04-28 10:41:00 +0200

    F17HL3-78 Legge til validering i aktivitetsmål

--- src/sider/hovedside/mal/aktivitetsmal-form.js
+++ src/sider/hovedside/mal/aktivitetsmal-form.js
@@ -2,27 +2,37 @@ import React, { PropTypes as PT } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
-import { reduxForm } from 'redux-form';
+import { validForm, rules } from 'react-redux-form-validation';
 import Textarea from '../../../modal/skjema/textarea/textarea';
 
+const MALTEKST_MAKSLENGDE = 500;
 
 function AktivitetsmalForm(props) {
     return (
         <form onSubmit={props.handleSubmit}>
-            <Textarea feltNavn="mal" labelId="Hva er ditt mål" maxLength={500} />
-            <Hovedknapp className="aktivitetmal__redigering--knapp"><FormattedMessage id="Lagre" /></Hovedknapp>
-            <Knapp type="button" onClick={props.handleCancel}>Avbryt</Knapp>
+            <Textarea feltNavn="mal" labelId="aktivitetsmal.tekst.label" maxLength={MALTEKST_MAKSLENGDE}/>
+            <Hovedknapp className="aktivitetmal__redigering--knapp"><FormattedMessage id="aktivitetsmal.lagre" /></Hovedknapp>
+            <Knapp type="button" onClick={props.handleCancel}>
+                <FormattedMessage id="aktivitetsmal.avbryt"/>
+            </Knapp>
         </form>
     );
 }
+
+const forLangMaltekst = rules.maxLength(MALTEKST_MAKSLENGDE,
+    <FormattedMessage id='aktivitetsmal.tekst.makslengde.feilmelding' values={{antall_tegn: MALTEKST_MAKSLENGDE}} />
+);
 
 AktivitetsmalForm.propTypes = {
     handleSubmit: PT.func.isRequired,
     handleCancel: PT.func.isRequired
 };
 
-const AktivitetsmalReduxForm = reduxForm({
-    form: 'aktivitetsmal-form'
+const AktivitetsmalReduxForm = validForm({
+    form: 'aktivitetsmal-form',
+    validate: {
+        mal: [forLangMaltekst]
+    }
 })(AktivitetsmalForm);
 
 const mapStateToProps = (state, props) => ({
