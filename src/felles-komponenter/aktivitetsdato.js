6a852c65fa67d7e1e50f54b7c64876d71c1c9d8a
Steffen Hageland <steffen.hageland@nav.no>
2017-05-03 13:30:50 +0200

    PL-40079 trekker ut cmstekster i aktivitetsdato

--- src/felles-komponenter/aktivitetsdato.js
+++ src/felles-komponenter/aktivitetsdato.js
@@ -1,16 +1,28 @@
 import React, { PropTypes as PT } from 'react';
 import { Normaltekst, EtikettStor } from 'nav-frontend-typografi';
+import { FormattedMessage } from 'react-intl';
 import Lenke from './utils/lenke';
 
 const datoklasse = 'aktivitetvisning-info aktivitetsdato__tekst';
 const erDatoSatt = (fraDato, tilDato) => fraDato === '' || tilDato === '';
-const visDato = (fraDato, tilDato) => <Normaltekst className={datoklasse}>{fraDato} - {tilDato}</Normaltekst>;
-const visSettDato = () => <Lenke href="/" className={datoklasse}>Legg inn til og fra dato</Lenke>;
+
+const visDato = (fraDato, tilDato) => (
+    <Normaltekst className={datoklasse}>
+        <FormattedMessage id="aktivitetsdato.vis-dato" values={{fraDato, tilDato}} />
+    </Normaltekst>);
+
+const visSettDato = () => (
+    <Lenke href="/" className={datoklasse}>
+        <FormattedMessage id="aktivitetsdato.vis-sett-dato" />
+    </Lenke>
+);
 
 function Aktivitetsdato({ fra, til }) {
     return (
         <div className="aktivitetsdato">
-            <EtikettStor className="aktivitetvisning-etikett aktivitetsdato__etikett">dato</EtikettStor>
+            <EtikettStor className="aktivitetvisning-etikett aktivitetsdato__etikett">
+                <FormattedMessage id="aktivitetsdato.dato-etikett" />
+            </EtikettStor>
             {erDatoSatt(fra, til) ? visDato(fra, til) : visSettDato()}
         </div>
     );
