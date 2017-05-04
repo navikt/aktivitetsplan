7d63fe05a9ed933ec44c73d8dba1e2b1f4057d5f
Martin Solheim <martin.solheim@nav.no>
2017-05-04 10:46:26 +0200

    Retter navn på importert svg

--- src/modal/visning/bekreft-slett-visning.js
+++ src/modal/visning/bekreft-slett-visning.js
@@ -3,7 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import Bilde from 'nav-react-design/dist/bilde';
 import { Systemtittel } from 'nav-frontend-typografi';
 import { Knapp, Fareknapp } from 'nav-frontend-knapper';
-import stopVG from './stop.svg';
+import stopSvg from './stop.svg';
 import './bekreft-slett-visning.less';
 
 
@@ -12,7 +12,7 @@ function BekreftSlettVisning({ slettAction, avbrytAction }) {
         <div className="bekreft-slett-container">
             <header className="modal-header" />
 
-            <Bilde className="bekreft-slett-container__stop-svg" src={stopVG} alt="stop icon" />
+            <Bilde className="bekreft-slett-container__stop-svg" src={stopSvg} alt="stop icon" />
             <Systemtittel tag="h1" className="bekreft-slett-container__stop-tekst">
                 <FormattedMessage id="aktivitetvisning.bekreft-sletting.tittel" />
             </Systemtittel>
