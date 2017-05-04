33aa10bd5effe5bf09e6a3e263de4cf1c96222b3
alex <alexander.svendsen@nav.no>
2017-05-04 14:09:03 +0200

    Fiks FormattedMessage import bug

--- src/modal/visning/aktivitetsbeskrivelse.js
+++ src/modal/visning/aktivitetsbeskrivelse.js
@@ -1,12 +1,12 @@
 import React, { PropTypes as PT } from 'react';
-import { FormatedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { Normaltekst, Element } from 'nav-frontend-typografi';
 
 function Aktivitetsbeskrivelse({ beskrivelse }) {
     return beskrivelse ? (
         <section className="aktivitetsbeskrivelse">
             <Element className="aktivitetsbeskrivelse__tittel">
-                <FormatedMessage id="aktivitetvisning.beskrivelse-label" />
+                <FormattedMessage id="aktivitetvisning.beskrivelse-label" />
             </Element>
             <Normaltekst className="aktivitetsbeskrivelse__tekst">{beskrivelse}</Normaltekst>
         </section>
