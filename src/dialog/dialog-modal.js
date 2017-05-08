3fb9153d570a89df06b31e5c4d7f75b34feb66d5
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-05-08 18:48:23 +0200

    justerte dialog-modal PKULV-2616 PKULV-2641

--- src/dialog/dialog-modal.js
+++ src/dialog/dialog-modal.js
@@ -2,134 +2,98 @@ import React, { Component, PropTypes as PT } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Knapp } from 'nav-frontend-knapper';
+import { Undertittel } from 'nav-frontend-typografi';
 import Lukknapp from 'nav-frontend-lukknapp';
-import { autobind } from '../utils';
+import classNames from 'classnames';
 import Dialog from './dialog';
 import Dialoger from './dialoger';
 import Modal from '../modal/modal';
 import Hovedside from '../sider/hovedside/hovedside';
 import history from '../history';
 import Knappelenke from '../felles-komponenter/utils/knappelenke';
+import PilKnapp from '../felles-komponenter/utils/pil-knapp';
 import NyHenvendelse from './ny-henvendelse';
 import { visibleIfHOC } from '../hocs/visible-if';
 import './dialog-modal.less';
 import * as AppPT from '../proptypes';
 
 const animationTime = 300;
-const width = 750;
-const initialLeft = -(width + 10);
-const dialogHeight = 250;
-const initialDialogMargin = -dialogHeight;
-const kolonneStyle = {
-    width: `${width / 2}px`
-};
-
 const VisibleDiv = visibleIfHOC((props) => <div {...props} />);
 
-class DialogModalContent extends Component {
 
-    constructor() {
-        super();
-        autobind(this);
-        this.state = {
-            dialogPanelMargin: initialDialogMargin
-        };
-    }
+function nyDialog() {
+    history.push('/dialog/ny');
+}
 
-    nyDialog() {
-        this.setState({
-            dialogPanelMargin: 0
-        });
-    }
+function tilbake() {
+    history.push('/dialog');
+}
 
-    lukkNyDialog() {
-        this.setState({
-            dialogPanelMargin: initialDialogMargin
-        });
-    }
+function dialogOpprettet(dialog) {
+    history.push(`/dialog/${dialog.id}`);
+}
 
-    dialogOpprettet(dialog) {
-        this.lukkNyDialog();
-        history.push(`/dialog/${dialog.id}`);
-    }
+function DialogModalContent({ valgtDialog, valgtAktivitetId, navigerTil, harNyDialog, harNyDialogEllerValgtDialog }) {
+    const harValgtDialog = !!valgtDialog;
 
-    render() {
-        const state = this.state;
-        const { valgtDialog, valgtAktivitetId, navigerTil } = this.props;
-        const { dialogPanelMargin } = state;
-
-
-        function lukkDialog() {
-            history.push('/dialog');
-        }
-
-        const nyDialogApen = dialogPanelMargin === 0;
-        return (
+    return (
+        <div className="dialog-modal__wrapper">
+            <div className="dialog-modal__header">
+                <PilKnapp visible={harNyDialogEllerValgtDialog} className="dialog-modal__tilbake-knapp" onClick={tilbake} />
+                <Undertittel className="dialog-modal__tittel">
+                    <FormattedMessage id="dialog.tittel" />
+                </Undertittel>
+                <Lukknapp overstHjorne onClick={() => navigerTil('/')}>
+                    <FormattedMessage id="dialog.modal.lukk-dialog" />
+                </Lukknapp>
+            </div>
             <div className="dialog-modal__innhold">
-                <div
-                    className="dialog-modal__kolonne dialog-modal__kolonne--dialoger"
-                    style={kolonneStyle}
-                >
-                    <div className="dialog-modal__kolonne-header dialog-modal__kolonne-header--dialoger">
-                        <Knapp onClick={this.nyDialog} disabled={nyDialogApen}>
+                <div className={`dialog-modal__kolonne dialog-modal__kolonne--dialoger ${harNyDialogEllerValgtDialog && 'dialog-modal__kolonne--dialoger-valgt-dialog'}`}>
+                    <section className="dialog-modal__ny-dialog">
+                        <Knapp
+                            onClick={nyDialog}
+                            disabled={harNyDialog}
+                            className="dialog-modal__ny-dialog-knapp"
+                        >
                             <FormattedMessage id="dialog.modal.ny-dialog" />
                         </Knapp>
-                        <Lukknapp overstHjorne onClick={() => navigerTil('/')}>
-                            <FormattedMessage id="dialog.modal.lukk-dialog" />
-                        </Lukknapp>
-                    </div>
-                    <div
-                        style={{
-                            marginTop: `${dialogPanelMargin}px`,
-                            transition: `margin-top ${animationTime}ms`
-                        }}
-                    >
-                        <div
-                            className="dialog-modal__ny-dialog"
-                            style={{
-                                minHeight: `${dialogHeight}px`
-                            }}
-                        >
-                            <VisibleDiv visible={nyDialogApen}>
-                                <Lukknapp overstHjorne onClick={this.lukkNyDialog}>
-                                    <FormattedMessage id="dialog.modal.lukk-ny-dialog" />
-                                </Lukknapp>
-                                <NyHenvendelse
-                                    formNavn="ny-dialog"
-                                    onComplete={this.dialogOpprettet} // TODO velg denne dialogen!
-                                />
-                            </VisibleDiv>
-                        </div>
-                        <Dialoger className="dialog-modal__kolonne-innhold" valgtDialog={valgtDialog} />
-                    </div>
+                    </section>
+                    <Dialoger className="dialog-modal__dialoger" valgtDialog={valgtDialog} />
                 </div>
                 <VisibleDiv
-                    visible={!!valgtDialog}
+                    visible={harNyDialogEllerValgtDialog}
                     className="dialog-modal__kolonne dialog-modal__kolonne--dialog"
-                    style={kolonneStyle}
                 >
-                    <div className="dialog-modal__kolonne-header">
+                    <VisibleDiv visible={harNyDialog}>
+                        <Undertittel>
+                            <FormattedMessage id="dialog.ny-dialog" />
+                        </Undertittel>
+                        <NyHenvendelse
+                            formNavn="ny-dialog"
+                            onComplete={dialogOpprettet}
+                        />
+                    </VisibleDiv>
+                    <VisibleDiv visible={harValgtDialog}>
                         <Knappelenke
                             visible={!!valgtAktivitetId}
                             onClick={() => navigerTil(`/aktivitet/aktivitet/${valgtAktivitetId}`)}
                         >
                             <FormattedMessage id="dialog.modal.til-aktiviteten" />
                         </Knappelenke>
-                        <Lukknapp overstHjorne onClick={lukkDialog}>
-                            <FormattedMessage id="dialog.modal.lukk-dialog" />
-                        </Lukknapp>
-                    </div>
-                    <Dialog className="dialog-modal__kolonne-innhold dialog-modal__kolonne-innhold--dialog" dialog={valgtDialog} />
+                        <Dialog dialog={valgtDialog} />
+                    </VisibleDiv>
                 </VisibleDiv>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 DialogModalContent.propTypes = {
     valgtDialog: AppPT.dialog,
     valgtAktivitetId: PT.string,
-    navigerTil: PT.func.isRequired
+    navigerTil: PT.func.isRequired,
+    harNyDialog: PT.bool.isRequired,
+    harNyDialogEllerValgtDialog: PT.bool.isRequired
 };
 
 DialogModalContent.defaultProps = {
@@ -141,21 +105,18 @@ class DialogModal extends Component { // eslint-disable-line react/no-multi-comp
 
     constructor() {
         super();
-        this.state = {
-            left: initialLeft
-        };
         this.navigerTil = this.navigerTil.bind(this);
     }
 
     componentDidMount() {
         this.setState({ // eslint-disable-line react/no-did-mount-set-state
-            left: 0
+            vis: true
         });
     }
 
     navigerTil(sti) {
         this.setState({
-            left: initialLeft
+            vis: false
         });
         setTimeout(() => {
             history.push(sti);
@@ -164,19 +125,12 @@ class DialogModal extends Component { // eslint-disable-line react/no-multi-comp
 
     render() {
         const state = this.state || {};
-        const { valgtDialog } = this.props;
+        const { harNyDialogEllerValgtDialog } = this.props;
         return (
             <div>
                 <Hovedside />
                 <Modal
-                    className="dialog-modal"
-                    style={{
-                        content: {
-                            transition: `left ${animationTime}ms, width ${animationTime}ms`,
-                            width: `${valgtDialog ? width : width / 2}px`,
-                            left: `${state.left}px`
-                        }
-                    }}
+                    className={classNames('dialog-modal', state.vis && 'dialog-modal--vis', harNyDialogEllerValgtDialog && 'dialog-modal--full-bredde')}
                     isOpen
                     closeButton={false}
                     onRequestClose={() => this.navigerTil('/')}
@@ -189,11 +143,7 @@ class DialogModal extends Component { // eslint-disable-line react/no-multi-comp
 }
 
 DialogModal.propTypes = {
-    valgtDialog: AppPT.dialog
-};
-
-DialogModal.defaultProps = {
-    valgtDialog: undefined
+    harNyDialogEllerValgtDialog: PT.bool.isRequired
 };
 
 const mapStateToProps = (state, props) => {
@@ -203,8 +153,13 @@ const mapStateToProps = (state, props) => {
     const valgtDialog = dialoger.find((d) => d.id === id);
     const valgtAktivitetId = valgtDialog && valgtDialog.aktivitetId;
 
+    const harNyDialog = id === 'ny';
+    const harValgtDialog = !!valgtDialog;
     return {
+        harNyDialog,
         valgtDialog,
+        harValgtDialog,
+        harNyDialogEllerValgtDialog: harNyDialog || harValgtDialog,
         valgtAktivitetId
     };
 };
