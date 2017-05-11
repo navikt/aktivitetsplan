5f5206b8cc2538e64f5e80a534845f0f57299f28
Steffen Hageland <steffen.hageland@nav.no>
2017-05-11 14:31:43 +0200

    Må returnere null og ikke undefined fra tom komponent

--- src/felles-komponenter/feature/feature.js
+++ src/felles-komponenter/feature/feature.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Feature({ name, children }) {
     const enabled = window.features && window.features[name] === true;
-    return enabled && <div>{children}</div>;
+    return enabled ? <div>{children}</div> : null;
 }
 
