e123283c2feb30656d094ba3391dc279dccfc198
alex <alexander.svendsen@nav.no>
2017-05-25 15:14:19 +0200

    Sett aktivitet api-et direkte til backenden

--- example/config.js
+++ example/config.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const API_BASE_URL = '/veilarbaktivitetsplanfs/api';
 export const DIALOG_BASE_URL = '/veilarbdialog/api';
-export const AKTIVITET_PROXY_BASE_URL = '/veilarbaktivitetproxy/api';
+export const AKTIVITET_PROXY_BASE_URL = '/veilarbaktivitet/api';
 export const SITUASJON_PROXY_BASE_URL = '/veilarbsituasjon/api';
 export const PERSON_BASE_URL = '/veilarbperson/tjenester';
 
