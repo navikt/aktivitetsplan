522861970f2ba8ab32cf688b4dd92b151f584b60
Oscar Eckbo <oscar.eckbo@nav.no>
2017-05-25 15:59:20 +0200

    Styler metadata til vilkår

--- src/modal/vilkar/vilkar-innhold.js
+++ src/modal/vilkar/vilkar-innhold.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Bilde from 'nav-react-design/dist/bilde';
-import { Innholdstittel } from 'nav-frontend-typografi';
+import { Element, Innholdstittel } from 'nav-frontend-typografi';
 import * as AppPT from '../../proptypes';
 import UnsafeHtml from '../../felles-komponenter/utils/unsafe-html';
 import vilkarSvg from './vilkar-illustrasjon.svg';
 import VisibleIfHOC from '../../hocs/visible-if';
 
-const VisibleIfFormattedMessage = VisibleIfHOC(FormattedMessage);
+const VisibleIfElementFormattedMessage = VisibleIfHOC((props) => (
+    <Element className="vilkar__metaData">
+        <FormattedMessage {...props} />
+    </Element>
+));
 
 function VilkarInnhold({ vilkar }) {
     return (
@@ -15,7 +19,7 @@ function VilkarInnhold({ vilkar }) {
             <Bilde src={vilkarSvg} alt="Dekorativ illustrajon" />
             <Innholdstittel className="vilkar__tittel"><FormattedMessage id="vilkar.modal.gjeldende-tittel" /></Innholdstittel>
             <UnsafeHtml className="vilkar__tekst">{vilkar.tekst}</UnsafeHtml>
-            <VisibleIfFormattedMessage
+            <VisibleIfElementFormattedMessage
                 visible={!!vilkar.vilkarstatus && !!vilkar.dato}
                 id="vilkar.modal.gjeldende-status-dato"
                 values={{ status: vilkar.vilkarstatus, dato: vilkar.dato }}
