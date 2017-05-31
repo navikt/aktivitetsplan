940d490919fde546df5b77ab92c6bbe00b3040c0
Johan Rusvik <johan.rusvik@nav.no>
2017-05-31 15:49:50 +0200

    Setter fokus på textarea når bruker går til redigering av mål

--- src/sider/hovedside/mal/aktivitetsmal-form.js
+++ src/sider/hovedside/mal/aktivitetsmal-form.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
 import { validForm, rules } from 'react-redux-form-validation';
 import Textarea from '../../../modal/skjema/textarea/textarea';
+import { autobind } from '../../../utils';
 
 const MALTEKST_MAKSLENGDE = 500;
 
-function AktivitetsmalForm(props) {
-    function avbryt(e) {
-        e.preventDefault();
-        props.handleCancel();
+class AktivitetsmalForm extends Component {
+    constructor(props) {
+        super(props);
+        autobind(this);
     }
-
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <Textarea feltNavn="mal" labelId="aktivitetsmal.tekst.label" maxLength={MALTEKST_MAKSLENGDE} />
-            <Hovedknapp className="aktivitetmal__redigering--knapp"><FormattedMessage id="aktivitetsmal.lagre" /></Hovedknapp>
-            <Knapp onClick={avbryt}> {/* TODO: Vi må få inn type="button"*/}
-                <FormattedMessage id="aktivitetsmal.avbryt" />
-            </Knapp>
-        </form>
-    );
+    componentDidMount() {
+        this.textarea.focus();
+    }
+    avbryt(e) {
+        e.preventDefault();
+        this.props.handleCancel();
+    }
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit}>
+                <Textarea
+                    feltNavn="mal"
+                    labelId="aktivitetsmal.tekst.label"
+                    maxLength={MALTEKST_MAKSLENGDE}
+                    textareaRef={(textarea) => { this.textarea = textarea; }}
+                />
+                <Hovedknapp className="aktivitetmal__redigering--knapp"><FormattedMessage id="aktivitetsmal.lagre" /></Hovedknapp>
+                <Knapp onClick={this.avbryt}> {/* TODO: Vi må få inn type="button"*/}
+                    <FormattedMessage id="aktivitetsmal.avbryt" />
+                </Knapp>
+            </form>
+        );
+    }
 }
 
 const forLangMaltekst = rules.maxLength(MALTEKST_MAKSLENGDE,
