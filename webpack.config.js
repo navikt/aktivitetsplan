cb89a70d6d9a72e9c368821b2c31fcb1f71dadb2
Steffen Hageland <steffen.hageland@nav.no>
2017-06-02 09:53:41 +0200

    PKULV-2598 små tewaks etter feedback i PR

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PLUGINS = [
     new HtmlWebpackPlugin({
-        template: 'example/index.html'
-    })
+        template: 'example/index.html',
+    }),
 ];
 
-const BABEL_INCLUDE = [
-    /src/,
-    /example/
-];
+const BABEL_INCLUDE = [/src/, /example/];
 
 const RULES = [
     {
         test: /\.jsx?/,
         include: BABEL_INCLUDE,
         enforce: 'pre',
-        loader: 'babel-loader'
+        loader: 'babel-loader',
     },
     {
         test: /\.(svg)$/,
         use: {
             loader: 'url-loader',
-            options: { noquotes: true }
-        }
+            options: { noquotes: true },
+        },
     },
-    { test: /\.less$/, loader: 'ignore-loader' }
+    { test: /\.less$/, loader: 'ignore-loader' },
 ];
 
 const LOADERS = [
@@ -36,40 +33,42 @@ const LOADERS = [
         include: BABEL_INCLUDE,
         loader: 'babel-loader',
         query: {
-            plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
-        }
-    }
+            plugins: [
+                'react-html-attrs',
+                'transform-decorators-legacy',
+                'transform-class-properties',
+            ],
+        },
+    },
 ];
 
-module.exports = function (env) {
+module.exports = function(env) {
     const dev = env && env.dev;
     return {
         context: __dirname,
         devtool: dev ? 'source-map' : false,
-        entry: [
-            'whatwg-fetch',
-            './example/example.js'
-        ],
+        entry: ['whatwg-fetch', './example/example.js'],
         module: {
             rules: RULES,
-            loaders: LOADERS
+            loaders: LOADERS,
         },
         resolve: {
             alias: {
-                '~config': path.resolve(__dirname, './example/config')
+                '~config': path.resolve(__dirname, './example/config'),
             },
-            extensions: ['.js', '.jsx', '.json']
+            extensions: ['.js', '.jsx', '.json'],
         },
         output: {
             path: path.resolve(__dirname, 'example/build'),
-            publicPath: '/aktivitetsplanfelles/'
+            filename: 'bundle.js',
+            publicPath: '/aktivitetsplanfelles/',
         },
         devServer: {
             contentBase: path.resolve(__dirname, 'example'),
             historyApiFallback: {
-                index: '/aktivitetsplanfelles/'
-            }
+                index: '/aktivitetsplanfelles/',
+            },
         },
-        plugins: PLUGINS
+        plugins: PLUGINS,
     };
 };
