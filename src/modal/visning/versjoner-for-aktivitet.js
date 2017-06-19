838514469bca7e099f11d78eb42f9854f7636f2c
alex <alexander.svendsen@nav.no>
2017-06-19 10:33:40 +0200

    Refaktorer koden

--- src/modal/visning/versjoner-for-aktivitet.js
+++ src/modal/visning/versjoner-for-aktivitet.js
@@ -1,89 +1,17 @@
 import React, { Component } from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
 import {
     hentVersjonerForAktivtet,
     fjernVersjoner,
 } from '../../ducks/aktivitet-versjoner';
 import * as AppPT from '../../proptypes';
-import {
-    formaterDatoDatoEllerTidSiden,
-    formaterDatoKortManed,
-} from '../../utils';
 import visibleIfHOC from '../../hocs/visible-if';
 import VisibleIfDiv from '../../felles-komponenter/utils/visible-if-div';
 import Innholdslaster from '../../felles-komponenter/utils/innholdslaster';
-import {
-    TRANSAKSJON_TYPE_ETIKETT_ENDRET,
-    TRANSAKSJON_TYPE_STATUS_ENDRET,
-    TRANSAKSJON_TYPE_AVTALT_DATO_ENDRET,
-} from '../../constant';
-import Accordian from '../../felles-komponenter/accordion';
-
-function VersjonInnslag({ versjon, prevVersjon }) {
-    function endringsTekst() {
-        switch (versjon.transaksjonsType) {
-            case TRANSAKSJON_TYPE_STATUS_ENDRET: {
-                return (
-                    <FormattedMessage
-                        id={`endringstype.${versjon.transaksjonsType}`}
-                        values={{
-                            fra: prevVersjon.status,
-                            til: versjon.status,
-                        }}
-                    />
-                );
-            }
-            case TRANSAKSJON_TYPE_AVTALT_DATO_ENDRET: {
-                return (
-                    <FormattedMessage
-                        id={`endringstype.${versjon.transaksjonsType}`}
-                        values={{
-                            fra: formaterDatoKortManed(prevVersjon.tilDato),
-                            til: formaterDatoKortManed(versjon.tilDato),
-                        }}
-                    />
-                );
-            }
-            case TRANSAKSJON_TYPE_ETIKETT_ENDRET: {
-                return (
-                    <FormattedMessage
-                        id={`endringstype.${versjon.transaksjonsType}`}
-                        values={{
-                            til: versjon.etikett ? versjon.etikett : 'INGEN',
-                        }}
-                    />
-                );
-            }
-            default: {
-                return (
-                    <FormattedMessage
-                        id={`endringstype.${versjon.transaksjonsType}`}
-                    />
-                );
-            }
-        }
-    }
-
-    return (
-        <p className="versjon-for-aktivitet__innslag">
-            <b><FormattedMessage id={`lagtInnAv.${versjon.lagtInnAv}`} /> </b>
-            {endringsTekst()}
-            <br />
-            {formaterDatoDatoEllerTidSiden(versjon.endretDato)}
-        </p>
-    );
-}
-
-VersjonInnslag.propTypes = {
-    versjon: AppPT.aktivitet.isRequired,
-    prevVersjon: AppPT.aktivitet,
-};
-
-VersjonInnslag.defaultProps = {
-    prevVersjon: undefined,
-};
+import Accordion from '../../felles-komponenter/accordion';
+import { autobind } from '../../utils';
+import VersjonInnslag from './versjoninnslag';
 
 const MAX_SIZE = 10;
 
@@ -93,6 +21,7 @@ class VersjonerForAktivitet extends Component {
         this.state = {
             apen: false,
         };
+        autobind(this);
     }
 
     componentWillMount() {
@@ -108,13 +37,42 @@ class VersjonerForAktivitet extends Component {
         this.props.doFjernVersjoner();
     }
 
-    render() {
-        const onClick = () => {
-            this.setState({ apen: !this.state.apen });
-        };
+    onClick() {
+        this.setState({ apen: !this.state.apen });
+    }
 
+    render() {
         const { versjonerData, className } = this.props;
         const versjoner = versjonerData.data;
+
+        const versjonerInnslag = versjoner
+            .slice(0, MAX_SIZE)
+            .map((versjon, index) => (
+                <VersjonInnslag
+                    key={versjon.endretDato}
+                    versjon={versjon}
+                    prevVersjon={versjoner[index + 1]}
+                />
+            ));
+
+        const versjonerInnslagUnderAccordion = (
+            <Accordion
+                onClick={this.onClick}
+                labelId={
+                    this.state.apen ? 'endringer.skjul' : 'endringer.vis-mer'
+                }
+            >
+                {versjoner
+                    .slice(MAX_SIZE)
+                    .map((versjon, index) => (
+                        <VersjonInnslag
+                            key={versjon.endretDato}
+                            versjon={versjon}
+                            prevVersjon={versjoner[index + 1]}
+                        />
+                    ))}
+            </Accordion>
+        );
 
         return (
             <Innholdslaster
@@ -122,34 +80,9 @@ class VersjonerForAktivitet extends Component {
                 spinnerStorrelse="m"
             >
                 <section className={className}>
-                    {versjoner
-                        .slice(0, MAX_SIZE)
-                        .map((versjon, index) => (
-                            <VersjonInnslag
-                                key={versjon.endretDato}
-                                versjon={versjon}
-                                prevVersjon={versjoner[index + 1]}
-                            />
-                        ))}
+                    {versjonerInnslag}
                     <VisibleIfDiv visible={versjoner.length > MAX_SIZE}>
-                        <Accordian
-                            onClick={onClick}
-                            labelId={
-                                this.state.apen
-                                    ? 'endringer.skjul'
-                                    : 'endringer.vis-mer'
-                            }
-                        >
-                            {versjoner
-                                .slice(MAX_SIZE)
-                                .map((versjon, index) => (
-                                    <VersjonInnslag
-                                        key={versjon.endretDato}
-                                        versjon={versjon}
-                                        prevVersjon={versjoner[index + 1]}
-                                    />
-                                ))}
-                        </Accordian>
+                        {versjonerInnslagUnderAccordion}
                     </VisibleIfDiv>
                 </section>
             </Innholdslaster>
@@ -158,7 +91,7 @@ class VersjonerForAktivitet extends Component {
 }
 
 VersjonerForAktivitet.propTypes = {
-    versjonerData: AppPT.reducer,
+    versjonerData: AppPT.reducer.isRequired,
     aktivitet: AppPT.aktivitet.isRequired,
     doHentVersjonerForAktivitet: PT.func.isRequired,
     doFjernVersjoner: PT.func.isRequired,
@@ -166,7 +99,6 @@ VersjonerForAktivitet.propTypes = {
 };
 
 VersjonerForAktivitet.defaultProps = {
-    versjoner: undefined,
     className: '',
 };
 
---
+++ src/modal/visning/versjoninnslag.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import * as AppPT from '../../proptypes';
+import { formaterDatoKortManed } from '../../utils';
+import Dato from '../../felles-komponenter/dato';
+import {
+    TRANSAKSJON_TYPE_ETIKETT_ENDRET,
+    TRANSAKSJON_TYPE_STATUS_ENDRET,
+    TRANSAKSJON_TYPE_AVTALT_DATO_ENDRET,
+} from '../../constant';
+
+function VersjonInnslag({ versjon, prevVersjon }) {
+    function endringsTekst() {
+        const textId = `endringstype.${versjon.transaksjonsType}`;
+        switch (versjon.transaksjonsType) {
+            case TRANSAKSJON_TYPE_STATUS_ENDRET: {
+                return (
+                    <FormattedMessage
+                        id={textId}
+                        values={{
+                            fra: prevVersjon.status,
+                            til: versjon.status,
+                        }}
+                    />
+                );
+            }
+            case TRANSAKSJON_TYPE_AVTALT_DATO_ENDRET: {
+                return (
+                    <FormattedMessage
+                        id={textId}
+                        values={{
+                            fra: formaterDatoKortManed(prevVersjon.tilDato),
+                            til: formaterDatoKortManed(versjon.tilDato),
+                        }}
+                    />
+                );
+            }
+            case TRANSAKSJON_TYPE_ETIKETT_ENDRET: {
+                return (
+                    <FormattedMessage
+                        id={textId}
+                        values={{
+                            til: versjon.etikett ? versjon.etikett : 'INGEN',
+                        }}
+                    />
+                );
+            }
+            default: {
+                return <FormattedMessage id={textId} />;
+            }
+        }
+    }
+
+    return (
+        <p className="versjon-for-aktivitet__innslag">
+            <b><FormattedMessage id={`lagtInnAv.${versjon.lagtInnAv}`} /> </b>
+            {endringsTekst()}
+            <br />
+            <Dato>
+                {versjon.endretDato}
+            </Dato>
+        </p>
+    );
+}
+
+VersjonInnslag.propTypes = {
+    versjon: AppPT.aktivitet.isRequired,
+    prevVersjon: AppPT.aktivitet,
+};
+
+VersjonInnslag.defaultProps = {
+    prevVersjon: undefined,
+};
+
+export default VersjonInnslag;
