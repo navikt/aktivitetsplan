addfa641aaa39067289e34d5e449e10daaec44c5
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2017-06-28 15:39:15 +0200

    Styling ihht til lapper fra bughunt.

--- src/moduler/sidebanner/sidebanner.js
+++ src/moduler/sidebanner/sidebanner.js
@@ -10,6 +10,7 @@ function Sidebanner(props) {
         <div className="sidebanner">
             <Bilde
                 src={aktivitetsplanSvg}
+
                 alt={props.intl.formatMessage({
                     id: 'sidebanner-illustrasjon.alt-tekst',
                 })}
