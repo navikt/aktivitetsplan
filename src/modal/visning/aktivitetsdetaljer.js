f245a5af81e13278d968975085023e3d47163cde
alex <alexander.svendsen@nav.no>
2017-07-07 14:08:06 +0200

    Prettier

--- src/modal/visning/aktivitetsdetaljer.js
+++ src/modal/visning/aktivitetsdetaljer.js
@@ -355,45 +355,43 @@ function Aktivitetsdetaljer({ valgtAktivitet, className }) {
     ];
 
     const behandlingFelter = () => [
-            <Informasjonsfelt
-                key="behandlingtype"
-                tittel={
-                    <FormattedMessage id="aktivitetdetaljer.behandling-type-label" />
-                }
-                innhold={behandlingType}
-            />,
-            <Informasjonsfelt
-                key="behandlingsted"
-                tittel={
-                    <FormattedMessage id="aktivitetdetaljer.behandling-sted-label" />
-                }
-                innhold={behandlingSted}
-            />,
-            <Informasjonsfelt
-                key="fradato"
-                tittel={fraDatoTekst(aktivitetstype)}
-                innhold={fraDato}
-            />,
-            <Informasjonsfelt
-                key="tildato"
-                tittel={tilDatoTekst(aktivitetstype)}
-                innhold={tilDato}
-            />,
-            <Informasjonsfelt
-                key="effekt"
-                tittel={
-                    <FormattedMessage id="aktivitetdetaljer.effekt-label" />
-                }
-                innhold={effekt}
-            />,
-            <Informasjonsfelt
-                key="behandlingOppfolging"
-                tittel={
-                    <FormattedMessage id="aktivitetdetaljer.behandling-oppfolging-label" />
-                }
-                innhold={behandlingOppfolging}
-            />,
-        ];
+        <Informasjonsfelt
+            key="behandlingtype"
+            tittel={
+                <FormattedMessage id="aktivitetdetaljer.behandling-type-label" />
+            }
+            innhold={behandlingType}
+        />,
+        <Informasjonsfelt
+            key="behandlingsted"
+            tittel={
+                <FormattedMessage id="aktivitetdetaljer.behandling-sted-label" />
+            }
+            innhold={behandlingSted}
+        />,
+        <Informasjonsfelt
+            key="fradato"
+            tittel={fraDatoTekst(aktivitetstype)}
+            innhold={fraDato}
+        />,
+        <Informasjonsfelt
+            key="tildato"
+            tittel={tilDatoTekst(aktivitetstype)}
+            innhold={tilDato}
+        />,
+        <Informasjonsfelt
+            key="effekt"
+            tittel={<FormattedMessage id="aktivitetdetaljer.effekt-label" />}
+            innhold={effekt}
+        />,
+        <Informasjonsfelt
+            key="behandlingOppfolging"
+            tittel={
+                <FormattedMessage id="aktivitetdetaljer.behandling-oppfolging-label" />
+            }
+            innhold={behandlingOppfolging}
+        />,
+    ];
 
     const map = {
         [EGEN_AKTIVITET_TYPE]: egenStillingFelter,
