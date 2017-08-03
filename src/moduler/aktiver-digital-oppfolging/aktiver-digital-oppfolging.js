c7d4c3c86bead7b0d97226394f65b26f72bf60e8
Oscar Eckbo <oscar.eckbo@nav.no>
2017-08-03 16:58:40 +0200

    Tilpasninger for å vise forskjellige tekster/knapper
    - Gjør om til class
    - Setter lokal state for om man har trykket på knappen
    - henter situasjon på nytt når man trykker for å se om KRR er endret
    
    [PKULV-3105]

--- src/moduler/aktiver-digital-oppfolging/aktiver-digital-oppfolging.js
+++ src/moduler/aktiver-digital-oppfolging/aktiver-digital-oppfolging.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import { Hovedknapp } from 'nav-frontend-knapper';
 import {
     AdvarselVarsling,
     Varsling,
@@ -11,10 +10,17 @@ import {
 import * as AppPT from '../../proptypes';
 import { settDigital } from './aktiver-digital-oppfolging-reducer';
 import { STATUS } from '../../ducks/utils';
+import { hentSituasjon } from '../situasjon/situasjon';
+import {
+    selectReservasjonKRR,
+    selectSituasjonReducer,
+} from '../situasjon/situasjon-selector';
+import { HiddenIfHovedknapp } from '../../felles-komponenter/hidden-if/hidden-if-knapper';
 
 export function AktiverDigitalOppfolgingVarsel({
     reservertIKRR,
     settDigitalFeilet,
+    harTrykketRefresh,
 }) {
     if (!reservertIKRR && !settDigitalFeilet) {
         return (
@@ -24,11 +30,14 @@ export function AktiverDigitalOppfolgingVarsel({
             />
         );
     } else if (reservertIKRR && !settDigitalFeilet) {
+        const resertvertTekst = harTrykketRefresh
+            ? 'sett-digital.reservert-i-krr.fjern.reservasjon.infotekst'
+            : 'sett-digital.reservert-i-krr.infotekst';
         return (
             <FormattedMessage id="sett-digital.reservert-i-krr.url-lenke">
                 {url =>
                     <VarslingMedLenke
-                        tekstId="sett-digital.reservert-i-krr.infotekst"
+                        tekstId={resertvertTekst}
                         lenkeTekstId="sett-digital.reservert-i-krr.lenketekst"
                         href={url}
                         className="sett-digital__varsel"
@@ -48,56 +57,92 @@ export function AktiverDigitalOppfolgingVarsel({
 AktiverDigitalOppfolgingVarsel.propTypes = {
     reservertIKRR: PT.bool.isRequired,
     settDigitalFeilet: PT.bool.isRequired,
+    harTrykketRefresh: PT.bool.isRequired,
 };
 
-export function AktiverDigitalOppfolgingPure({
-    reservertIKRR,
-    doSettDigital,
-    aktiverDigitalOppfolgingReducer,
-}) {
-    const setterDigital =
-        aktiverDigitalOppfolgingReducer.status === STATUS.PENDING ||
-        aktiverDigitalOppfolgingReducer.status === STATUS.RELOADING;
+export class AktiverDigitalOppfolgingPure extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            harTrykketRefresh: false,
+        };
+    }
 
-    return (
-        <div className="sett-digital">
-            <AktiverDigitalOppfolgingVarsel
-                reservertIKRR={reservertIKRR}
-                settDigitalFeilet={
-                    aktiverDigitalOppfolgingReducer.status === STATUS.ERROR
-                }
-            />
+    render() {
+        const {
+            reservertIKRR,
+            doSettDigital,
+            doHentSituasjon,
+            aktiverDigitalOppfolgingReducer,
+            situasjonReducer,
+        } = this.props;
 
-            <Hovedknapp
+        const setterDigital =
+            aktiverDigitalOppfolgingReducer.status === STATUS.PENDING ||
+            aktiverDigitalOppfolgingReducer.status === STATUS.RELOADING;
+
+        const lasterSituasjon =
+            situasjonReducer.status === STATUS.PENDING ||
+            situasjonReducer.status === STATUS.RELOADING;
+
+        const ReservasjonDifiKnapp = () =>
+            <HiddenIfHovedknapp
+                mini
+                spinner={lasterSituasjon}
+                disabled={lasterSituasjon}
+                hidden={!reservertIKRR}
+                onClick={() => {
+                    doHentSituasjon();
+                    this.setState({ harTrykketRefresh: true });
+                }}
+            >
+                <FormattedMessage id="sett-digital.manuell-oppfolging.reservasjon-difi-knapp" />
+            </HiddenIfHovedknapp>;
+
+        const AktiverDigitalOppfolgingKnapp = () =>
+            <HiddenIfHovedknapp
                 mini
                 spinner={setterDigital}
                 disabled={setterDigital}
+                hidden={reservertIKRR}
                 onClick={doSettDigital}
             >
                 <FormattedMessage id="sett-digital.manuell-oppfolging.aktiver-digital-knapp" />
-            </Hovedknapp>
-        </div>
-    );
-}
+            </HiddenIfHovedknapp>;
 
-AktiverDigitalOppfolgingPure.defaultProps = {
-    doSettDigital: undefined,
-    aktiverDigitalOppfolgingReducer: {},
-};
+        return (
+            <div className="sett-digital">
+                <AktiverDigitalOppfolgingVarsel
+                    reservertIKRR={reservertIKRR}
+                    settDigitalFeilet={
+                        aktiverDigitalOppfolgingReducer.status === STATUS.ERROR
+                    }
+                    harTrykketRefresh={this.state.harTrykketRefresh}
+                />
+                <ReservasjonDifiKnapp />
+                <AktiverDigitalOppfolgingKnapp />
+            </div>
+        );
+    }
+}
 
 AktiverDigitalOppfolgingPure.propTypes = {
     reservertIKRR: PT.bool.isRequired,
-    doSettDigital: PT.func,
-    aktiverDigitalOppfolgingReducer: AppPT.reducer,
+    doSettDigital: PT.func.isRequired,
+    doHentSituasjon: PT.func.isRequired,
+    aktiverDigitalOppfolgingReducer: AppPT.reducer.isRequired,
+    situasjonReducer: AppPT.reducer.isRequired,
 };
 
 const mapStateToProps = state => ({
-    reservertIKRR: state.data.situasjon.data.reservasjonKRR,
+    reservertIKRR: selectReservasjonKRR(state),
+    situasjonReducer: selectSituasjonReducer(state),
     aktiverDigitalOppfolgingReducer: state.data.aktiverDigitalOppfolging,
 });
 
 const mapDispatchToProps = dispatch => ({
     doSettDigital: () => dispatch(settDigital()),
+    doHentSituasjon: () => dispatch(hentSituasjon()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(
