cf4ec057f7df38963815a52336c6fa2e09b16ab1
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2017-08-17 12:08:30 +0200

    Endret test på privatmodus.

--- src/moduler/privat-modus/privat-modus-selector.js
+++ src/moduler/privat-modus/privat-modus-selector.js
@@ -3,12 +3,8 @@ import { aggregerStatus } from '../../ducks/utils';
 export function selectErPrivatModus(state) {
     const stateData = state.data;
     const situasjonReducer = stateData.situasjon;
-    const identitetReducer = stateData.identitet;
 
-    return (
-        situasjonReducer.data.underOppfolging === false &&
-        identitetReducer.data.erVeileder
-    );
+    return situasjonReducer.data.underOppfolging === false;
 }
 
 export function selectPrivatModusSlice(state) {
