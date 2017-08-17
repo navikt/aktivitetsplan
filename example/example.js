37d4afeb48f81bf1ff68790bccfe7ef85c64d947
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2017-08-17 17:26:55 +0200

    prettier

--- example/example.js
+++ example/example.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { EKSEMPEL_FNR, CONTEXT_PATH } from './config';
 import App from '../src/app';
 import { fnrFraUrl } from '../src/bootstrap/fnr-provider';
-import * as Modal from "react-modal";
+import * as Modal from 'react-modal';
 
 if (!fnrFraUrl() && EKSEMPEL_FNR) {
     window.history.replaceState(
