cf8ce0fb67c503e36bc02eec0c584d72cbfe3bff
Martin Solheim <martin.solheim@nav.no>
2017-08-21 15:23:53 +0200

    prettier

--- src/moduler/arbeidsliste/arbeidsliste-rediger-form.js
+++ src/moduler/arbeidsliste/arbeidsliste-rediger-form.js
@@ -91,7 +91,10 @@ const mapDispatchToProps = (dispatch, props) => {
     return {
         onSubmit: formData => {
             dispatch(
-                redigerArbeidsliste(fnr, lagArbeidsliste(fnr, formData, props, true))
+                redigerArbeidsliste(
+                    fnr,
+                    lagArbeidsliste(fnr, formData, props, true)
+                )
             ).then(() => {
                 dispatch({ type: LUKK_MODAL });
                 history.push('/');
