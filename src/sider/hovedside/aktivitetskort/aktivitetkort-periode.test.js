bec34aeaf078180ba117447978ff19d392c7f97a
Alexandra Back <alexandra.back@nav.no>
2017-08-22 15:44:06 +0200

    prettier

--- src/sider/hovedside/aktivitetskort/aktivitetkort-periode.test.js
+++ src/sider/hovedside/aktivitetskort/aktivitetkort-periode.test.js
@@ -1,45 +1,50 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
 import React from 'react';
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import AktiviteskortPeriodeVisning from './aktivitetskort-periode';
-import { MOTE_TYPE,IJOBB_AKTIVITET_TYPE,SAMTALEREFERAT_TYPE } from '../../../constant';
+import {
+    MOTE_TYPE,
+    IJOBB_AKTIVITET_TYPE,
+    SAMTALEREFERAT_TYPE,
+} from '../../../constant';
 
 describe('Aktivitet-periode', () => {
     it('Skal vise kun fra dato hvis aktiviten er ett møte med NAV', () => {
         const aktivitet = {
-            fraDato: "2017-08-17",
-            tilDato: "2017-08-20",
-            type : MOTE_TYPE,
+            fraDato: '2017-08-17',
+            tilDato: '2017-08-20',
+            type: MOTE_TYPE,
         };
         const wrapper = shallow(
-            <AktiviteskortPeriodeVisning aktivitet={aktivitet}/>
+            <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
         );
         expect(wrapper.find({ children: '17. aug 2017' })).to.have.length(1);
     });
 
     it('Skal vise til og fra dato hvis aktiviten er ikke ett møte med NAV', () => {
         const aktivitet = {
-            fraDato: "2017-08-17",
-            tilDato: "2017-08-20",
-            type : IJOBB_AKTIVITET_TYPE,
+            fraDato: '2017-08-17',
+            tilDato: '2017-08-20',
+            type: IJOBB_AKTIVITET_TYPE,
         };
         const wrapper = shallow(
-            <AktiviteskortPeriodeVisning aktivitet={aktivitet}/>
+            <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
         );
-        expect(wrapper.find({ children: '17. aug 2017 - 20. aug 2017' })).to.have.length(1);
+        expect(
+            wrapper.find({ children: '17. aug 2017 - 20. aug 2017' })
+        ).to.have.length(1);
     });
 
     it('Skal vise fra dato hvis aktiviten er ett samtalereferat', () => {
         const aktivitet = {
-            fraDato: "2017-08-17",
-            type : SAMTALEREFERAT_TYPE,
+            fraDato: '2017-08-17',
+            type: SAMTALEREFERAT_TYPE,
         };
         const wrapper = shallow(
-            <AktiviteskortPeriodeVisning aktivitet={aktivitet}/>
+            <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
         );
         expect(wrapper.find({ children: '17. aug 2017' })).to.have.length(1);
     });
-
 });
