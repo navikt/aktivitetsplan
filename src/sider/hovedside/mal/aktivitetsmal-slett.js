3d8c2fe96a6ac1accee9e8b04814a530074bb92f
Alexandra Back <alexandra.back@nav.no>
2017-09-13 15:43:38 +0200

    Sortering
    JIRA: PKULV-3244

--- src/sider/hovedside/mal/aktivitetsmal-slett.js
+++ src/sider/hovedside/mal/aktivitetsmal-slett.js
@@ -5,12 +5,18 @@ import BekreftSlettVisning from '../../../moduler/aktivitet/visning/bekreft-slet
 import history from '../../../history';
 import { slettMal } from '../../../moduler/mal/mal-reducer';
 import AktivitetsmalModal from './aktivitetsmal-modal';
+import { selectMalListe } from '../../../moduler/mal/mal-selector';
+import * as AppPT from '../../../proptypes';
 
 // denne brukes i routing og må da være component
 // eslint-disable-next-line react/prefer-stateless-function
 class AktivitetmalSlett extends Component {
     render() {
-        const { doSlettMal } = this.props;
+        const { doSlettMal, malData } = this.props;
+        const bekrefSlettingAvMalListe =
+            malData.length > 1
+                ? 'aktivitetsmal.bekreft-slettning.undertittel'
+                : null;
         return (
             <BekreftSlettVisning
                 slettAction={() => {
@@ -19,6 +25,7 @@ class AktivitetmalSlett extends Component {
                 }}
                 avbrytAction={() => history.push('/mal')}
                 tittelId="aktivitetsmal.bekreft-sletting.tittel"
+                undertekstId={bekrefSlettingAvMalListe}
             />
         );
     }
@@ -26,12 +33,17 @@ class AktivitetmalSlett extends Component {
 
 AktivitetmalSlett.propTypes = {
     doSlettMal: PT.func.isRequired,
+    malData: PT.arrayOf(AppPT.mal).isRequired,
 };
+
+const mapStateToProps = state => ({
+    malData: selectMalListe(state),
+});
 
 const mapDispatchToProps = dispatch => ({
     doSlettMal: () => dispatch(slettMal()),
 });
 
 export default AktivitetsmalModal(
-    connect(null, mapDispatchToProps)(AktivitetmalSlett)
+    connect(mapStateToProps, mapDispatchToProps)(AktivitetmalSlett)
 );
