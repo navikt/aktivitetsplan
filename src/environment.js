562bf46293677b56aae3a9b93a99669226a044dc
Christian Finstad <christian.finstad@nav.no>
2017-09-21 15:36:20 +0200

    Sjekker sone før url til veilarboppgave returneres

--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,5 @@
+const FSS = 'FSS';
+
 function getEnviromentVariable(variableName) {
     const value = window.aktivitetsplan[variableName];
 
@@ -7,6 +9,8 @@ function getEnviromentVariable(variableName) {
 
     return value;
 }
+
+const sone = getEnviromentVariable('sone');
 
 export const DIALOG_BASE_URL = getEnviromentVariable('veilarbdialog_url');
 export const AKTIVITET_PROXY_BASE_URL = getEnviromentVariable(
@@ -18,8 +22,9 @@ export const SITUASJON_PROXY_BASE_URL = getEnviromentVariable(
 export const ONBOARDING_VIDEO_URL = getEnviromentVariable(
     'onboarding_video_url'
 );
-export const OPPGAVE_BASE_URL = getEnviromentVariable(
+
+export const OPPGAVE_BASE_URL = sone === FSS ? getEnviromentVariable(
     'veilarboppgave_url'
-);
+) : null;
 
 export default {};
