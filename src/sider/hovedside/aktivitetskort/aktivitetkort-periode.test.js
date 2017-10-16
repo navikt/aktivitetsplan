8af2c96224b02d6b37d817cf0669e1c7260976a3
Alexandra Back <alexandra.back2@nav.no>
2017-10-16 10:40:01 +0200

    Nytt test før aktivitetskort periode

--- src/sider/hovedside/aktivitetskort/aktivitetkort-periode.test.js
+++ src/sider/hovedside/aktivitetskort/aktivitetkort-periode.test.js
@@ -2,13 +2,14 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
 import AktiviteskortPeriodeVisning from './aktivitetskort-periode';
 import {
     MOTE_TYPE,
     IJOBB_AKTIVITET_TYPE,
-    SAMTALEREFERAT_TYPE,
 } from '../../../constant';
+import {
+    mountWithIntl
+} from '../../../../test/intl-enzyme-test-helper';
 
 describe('Aktivitet-periode', () => {
     it('Skal vise kun fra dato hvis aktiviten er ett møte med NAV', () => {
@@ -17,10 +18,10 @@ describe('Aktivitet-periode', () => {
             tilDato: '2017-08-20',
             type: MOTE_TYPE,
         };
-        const wrapper = shallow(
+        const wrapper = mountWithIntl(
             <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
         );
-        expect(wrapper.find({ children: '17. aug 2017' })).to.have.length(1);
+        expect(wrapper.text()).to.equal('17. aug 2017');
     });
 
     it('Skal vise til og fra dato hvis aktiviten er ikke ett møte med NAV', () => {
@@ -29,22 +30,10 @@ describe('Aktivitet-periode', () => {
             tilDato: '2017-08-20',
             type: IJOBB_AKTIVITET_TYPE,
         };
-        const wrapper = shallow(
+        const wrapper = mountWithIntl(
             <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
         );
-        expect(
-            wrapper.find({ children: '17. aug 2017 - 20. aug 2017' })
-        ).to.have.length(1);
+        expect(wrapper.text()).to.equal('17. aug 2017 - 20. aug 2017');
     });
 
-    it('Skal vise fra dato hvis aktiviten er ett samtalereferat', () => {
-        const aktivitet = {
-            fraDato: '2017-08-17',
-            type: SAMTALEREFERAT_TYPE,
-        };
-        const wrapper = shallow(
-            <AktiviteskortPeriodeVisning aktivitet={aktivitet} />
-        );
-        expect(wrapper.find({ children: '17. aug 2017' })).to.have.length(1);
-    });
 });
