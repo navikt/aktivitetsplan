25cd127a84befdd6b6c146c594c2fa2c0a4e71c8
Martin Solheim <martin.solheim@nav.no>
2017-10-25 11:11:51 +0200

    PKULV-3073 Forenkler reducere

--- src/moduler/aktivitet/kanaler-reducer.js
+++ src/moduler/aktivitet/kanaler-reducer.js
@@ -1,36 +1,17 @@
-import * as AT from './aktivitet-action-types';
-import { doThenDispatch, STATUS } from '../../ducks/utils';
+import { STATUS } from '../../ducks/utils';
 import * as Api from './aktivitet-api';
 import { selectKanalerStatus } from './kanaler-selector';
+import { createActionsAndReducer } from '../../ducks/rest-reducer';
 
-const initalState = {
-    data: [],
-    status: STATUS.NOT_STARTED,
-};
+const { reducer, action } = createActionsAndReducer('kanal', []);
 
-export default function reducer(state = initalState, action) {
-    const data = action.data;
-    switch (action.type) {
-        case AT.HENT_KANALER:
-            return { ...state, status: STATUS.PENDING };
-        case AT.HENT_KANALER_FEILET:
-            return { ...state, status: STATUS.ERROR, feil: data };
-        case AT.HENT_KANALER_OK:
-            return { ...state, status: STATUS.OK, data };
-        default:
-            return state;
-    }
-}
+export default reducer;
 
 export function hentKanaler() {
     return (dispatch, getState) => {
         const status = selectKanalerStatus(getState());
         if (status === STATUS.NOT_STARTED || status === STATUS.ERROR) {
-            doThenDispatch(() => Api.hentKanaler(), {
-                OK: AT.HENT_KANALER_OK,
-                FEILET: AT.HENT_KANALER_FEILET,
-                PENDING: AT.HENT_KANALER,
-            })(dispatch);
+            action(() => Api.hentKanaler());
         }
     };
 }
