06987f460766f20b58a2693441704177c26be5eb
Martin Solheim <martin.solheim@nav.no>
2017-10-26 11:28:22 +0200

    PKULV-3073 Bruker cashed action i identitet reducer
    - Sorterer imports og reducere alfabetisk i reducer aka combineReducers

--- src/moduler/identitet/identitet-duck.js
+++ src/moduler/identitet/identitet-duck.js
@@ -1,17 +1,10 @@
 import * as Api from '../situasjon/situasjon-api';
-import { STATUS } from '../../ducks/utils';
 import { createActionsAndReducer } from '../../ducks/rest-reducer';
-import { selectIdentitetStatus } from './identitet-selector';
 
-const { reducer, action } = createActionsAndReducer('identitet');
+const { reducer, cashedAction } = createActionsAndReducer('identitet');
 
 export default reducer;
 
 export function hentIdentitet() {
-    return (dispatch, getState) => {
-        const status = selectIdentitetStatus(getState());
-        if (status === STATUS.NOT_STARTED || status === STATUS.ERROR) {
-            action(() => Api.hentIdentitet())(dispatch);
-        }
-    };
+    return cashedAction(() => Api.hentIdentitet());
 }
