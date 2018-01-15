7b8c333fa72bbd426aadb938c470aaf309dcc642
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-01-15 14:11:18 +0100

    FO-257: Ikke feil hele planen hvis feature feiler
    Anta at alle features er false hvis feature feiler.

--- src/ducks/feature-api.js
+++ src/ducks/feature-api.js
@@ -2,7 +2,8 @@ import { FEATURE_BASE_URL } from '../environment';
 import { fetchToJson } from './../ducks/utils';
 
 export function hentFeature() {
-    return fetchToJson(FEATURE_BASE_URL);
+    return fetchToJson(FEATURE_BASE_URL).catch(() => {});
+    // Ikke gi feilmelding hvis feature filer, men anta alle features=false
 }
 
 export default {};
--- src/initiell-data-last.js
+++ src/initiell-data-last.js
@@ -5,7 +5,6 @@ import { bindActionCreators } from 'redux';
 import Innholdslaster from './felles-komponenter/utils/innholdslaster';
 import { hentFeature } from './ducks/feature-reducer';
 import { hentLedetekster } from './ducks/ledetekster-reducer';
-import { selectFeatureStatus } from './felles-komponenter/feature/feature-selector';
 import { selectLedeteksterStatus } from './ducks/ledetekster-selector';
 
 class InitiellDataLast extends Component {
@@ -15,12 +14,10 @@ class InitiellDataLast extends Component {
     }
 
     render() {
-        const { children, ledeteksterStatus, featureStatus } = this.props;
+        const { children, ledeteksterStatus } = this.props;
         return (
             <div>
-                <Innholdslaster
-                    avhengigheter={[ledeteksterStatus, featureStatus]}
-                >
+                <Innholdslaster avhengigheter={[ledeteksterStatus]}>
                     {children}
                 </Innholdslaster>
             </div>
@@ -32,12 +29,10 @@ InitiellDataLast.propTypes = {
     children: PT.node.isRequired,
     actions: PT.objectOf(PT.func).isRequired,
     ledeteksterStatus: PT.string.isRequired,
-    featureStatus: PT.string.isRequired,
 };
 
 const mapStateToProps = state => ({
     ledeteksterStatus: selectLedeteksterStatus(state),
-    featureStatus: selectFeatureStatus(state),
 });
 const mapDispatchToProps = dispatch => ({
     actions: bindActionCreators(
