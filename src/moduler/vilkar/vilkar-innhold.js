d27ff7457fd2cf12d34afb643a673dbd1e994963
Oscar Eckbo <oscar.eckbo@nav.no>
2018-01-22 09:14:53 +0100

    [FO-312] Sender med status for å vise om vilkår er avslått/godkjent

--- src/moduler/vilkar/vilkar-innhold.js
+++ src/moduler/vilkar/vilkar-innhold.js
@@ -50,7 +50,7 @@ function VilkarInnhold({
                 <Innholdstittel className="vilkar__tittel">
                     <FormattedMessage
                         id={hentTittelTekst()}
-                        values={{ dato: formattertDato }}
+                        values={{ dato: formattertDato, status: vilkar.vilkarstatus }}
                     />
                 </Innholdstittel>
                 <UnsafeHtml className="vilkar__tekst">
