f118f1963ada85a0525c0fbfb9ceaa2f49059562
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-02-21 16:13:10 +0100

    FO-268: Remove hack for kvp-feature

--- src/ducks/feature-api.js
+++ src/ducks/feature-api.js
@@ -1,24 +1,8 @@
-// import { FEATURE_BASE_URL } from '../environment';
-// import { fetchToJson } from './../ducks/utils';
+import { FEATURE_BASE_URL } from '../environment';
+import { fetchToJson } from './../ducks/utils';
 
 export function hentFeature() {
-    // *************************************************
-    //   TODO: Dette er et midlertidig hack for å unngå
-    //   sertifikat problemer med fo-feature i prod
-    // *************************************************
-    return new Promise(resolve => {
-        resolve({
-            aktivitetsplan: {
-                kvp: false,
-            },
-            veilarbaktivitetsplanfs: {
-                kvp: true,
-            },
-        });
-    });
-
-    // TODO: Linjen under er den som egentlig skal kjøre når hacken over er løst
-    // return fetchToJson(FEATURE_BASE_URL).catch(() => {});
+    return fetchToJson(FEATURE_BASE_URL).catch(() => {});
     // Ikke gi feilmelding hvis feature feiler, men anta alle features=false
 }
 
--- src/ducks/feature-reducer.js
+++ src/ducks/feature-reducer.js
@@ -1,16 +1,7 @@
 import * as Api from '../ducks/feature-api';
 import { createActionsAndReducer } from '../ducks/rest-reducer';
 
-const initialData = {
-    aktivitetsplan: {
-        kvp: false,
-    },
-    veilarbaktivitetsplanfs: {
-        kvp: true,
-    },
-};
-
-const { reducer, cashedAction } = createActionsAndReducer('feature', 'feature', initialData);
+const { reducer, cashedAction } = createActionsAndReducer('feature');
 
 export default reducer;
 
