a0413cfa1cdbe3ae7edfec01fae4eb9437118cb8
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-03-20 14:49:20 +0100

    [FO-367] legger tilbake logik for vis nøkler

--- src/intl-provider.js
+++ src/intl-provider.js
@@ -6,9 +6,17 @@ import tekster from './tekster'; // eslint-disable-line import/no-unresolved, im
 
 addLocaleData(nb);
 
+function keys(local) {
+    return Object.keys(tekster[local]).map(key => ({key, value: `[${key}]`  })).reduce((previous, current) => {
+        previous[current.key] = current.value; // eslint-disable-line no-param-reassign
+        return previous;
+    }, {});
+}
+
 function IntlProvider({ children, locale, ...props }) {
+    const tekst =  window.location.href.indexOf('vistekster=true') >= 0 ? keys(locale) : tekster[locale];
     return (
-        <Provider {...props} locale={locale} messages={tekster.nb || []}>
+        <Provider {...props} locale={locale} messages={tekst || {}}>
             {children}
         </Provider>
     );
@@ -17,6 +25,7 @@ function IntlProvider({ children, locale, ...props }) {
 IntlProvider.propTypes = {
     children: PT.node.isRequired,
     locale: PT.string.isRequired,
+    showKeys: PT.bool
 };
 
 export default IntlProvider;
