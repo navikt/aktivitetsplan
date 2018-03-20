8bea4b37886219fded3c7dc1e36a860041b28913
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-03-20 14:56:15 +0100

    [FO-367] fjerner proptypes som ble med ved feil

--- src/intl-provider.js
+++ src/intl-provider.js
@@ -7,14 +7,14 @@ import tekster from './tekster'; // eslint-disable-line import/no-unresolved, im
 addLocaleData(nb);
 
 function keys(local) {
-    return Object.keys(tekster[local]).map(key => ({key, value: `[${key}]`  })).reduce((previous, current) => {
+    return Object.keys(tekster[local]).map(key => ({ key, value: `[${key}]` })).reduce((previous, current) => {
         previous[current.key] = current.value; // eslint-disable-line no-param-reassign
         return previous;
     }, {});
 }
 
 function IntlProvider({ children, locale, ...props }) {
-    const tekst =  window.location.href.indexOf('vistekster=true') >= 0 ? keys(locale) : tekster[locale];
+    const tekst = window.location.href.indexOf('vistekster=true') >= 0 ? keys(locale) : tekster[locale];
     return (
         <Provider {...props} locale={locale} messages={tekst || {}}>
             {children}
