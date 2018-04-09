de1680e259380beeaed65bf7161ee2e931585511
Oscar Eckbo <oscar.eckbo@nav.no>
2018-04-09 16:22:47 +0200

    [FO-692] Bytter ut vilkår- og videomeny-lenke med informasjonlenke
    - Bytter ut vilkår og infovideo med ny infomodal.
    - Ny informasjonsmodal med video, tekst og evt. historiske vilkår.
    - Endringer ligger bak feature-toggle (aktivitetsplan.brukervilkar).
    - Tekstendringer

--- src/moduler/informasjon/informasjon-modal.js
+++ src/moduler/informasjon/informasjon-modal.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PT from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import {
     Innholdstittel,
@@ -9,39 +11,83 @@ import Modal from '../../felles-komponenter/modal/modal';
 import ModalContainer from '../../felles-komponenter/modal/modal-container';
 import { ONBOARDING_VIDEO_URL } from '../../environment';
 import { HtmlText } from '../../text';
+import VisibleIfDiv from '../../felles-komponenter/utils/visible-if-div';
+import VilkarHistorikk from '../vilkar/vilkar-historikk';
+import { hentHistoriskeVilkar } from '../vilkar/historiske-vilkar';
+import {
+    selectHistoriskeVilkar,
+    selectHistoriskeVilkarStatus,
+} from '../vilkar/historiske-vilkar-selector';
+import * as AppPT from '../../proptypes';
+import Innholdslaster from '../../felles-komponenter/utils/innholdslaster';
 
-function InformasjonModal() {
-    return (
-        <Modal
-            contentLabel="informasjon-modal"
-            contentClass="informasjon-visnign"
-        >
-            <ModalContainer className="informasjon-modal-container">
-                <Innholdstittel>
-                    <FormattedMessage id="informasjon.tittel" />
-                </Innholdstittel>
-                <Normaltekst>
-                    <FormattedMessage id="informasjon.hjelpetekst" />
-                </Normaltekst>
-                <iframe
-                    title="onboarding-video"
-                    frameBorder="0"
-                    scrolling="no"
-                    src={ONBOARDING_VIDEO_URL}
-                    className="video-player"
-                />
-                <Undertittel>
-                    <FormattedMessage id="informasjon.videokontent.tittel" />
-                </Undertittel>
-                <HtmlText
-                    className="mellomrom"
-                    id="informasjon.videokontent.text"
-                />
-            </ModalContainer>
-        </Modal>
-    );
+class InformasjonModal extends Component {
+    componentDidMount() {
+        const { doHentHistoriskeVilkar } = this.props;
+        doHentHistoriskeVilkar();
+    }
+    render() {
+        const { historiskeVilkar, historiskeVilkarStatus } = this.props;
+
+        return (
+            <Modal
+                contentLabel="informasjon-modal"
+                contentClass="informasjon-visnign"
+            >
+                <ModalContainer className="informasjon-modal-container">
+                    <Innholdstittel>
+                        <FormattedMessage id="informasjon.tittel" />
+                    </Innholdstittel>
+                    <Normaltekst>
+                        <FormattedMessage id="informasjon.hjelpetekst" />
+                    </Normaltekst>
+                    <iframe
+                        title="onboarding-video"
+                        frameBorder="0"
+                        scrolling="no"
+                        src={ONBOARDING_VIDEO_URL}
+                        className="video-player"
+                    />
+                    <Undertittel>
+                        <FormattedMessage id="informasjon.informasjonstekst.tittel" />
+                    </Undertittel>
+                    <HtmlText
+                        className="mellomrom"
+                        id="informasjon.informasjonstekst.tekst"
+                    />
+                    <Innholdslaster
+                        spinnerStorrelse="S"
+                        avhengigheter={[historiskeVilkarStatus]}
+                    >
+                        <VisibleIfDiv
+                            visible={historiskeVilkar.length > 0}
+                            className="vilkar__historikk-container"
+                        >
+                            <hr className="vilkar__delelinje" />
+                            <VilkarHistorikk
+                                resterendeVilkar={historiskeVilkar}
+                            />
+                        </VisibleIfDiv>
+                    </Innholdslaster>
+                </ModalContainer>
+            </Modal>
+        );
+    }
 }
 
-InformasjonModal.propTypes = {};
+InformasjonModal.propTypes = {
+    doHentHistoriskeVilkar: PT.func.isRequired,
+    historiskeVilkarStatus: AppPT.status.isRequired,
+    historiskeVilkar: PT.arrayOf(AppPT.vilkar).isRequired,
+};
 
-export default InformasjonModal;
+const mapStateToProps = state => ({
+    historiskeVilkarStatus: selectHistoriskeVilkarStatus(state),
+    historiskeVilkar: selectHistoriskeVilkar(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+    doHentHistoriskeVilkar: () => dispatch(hentHistoriskeVilkar()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(InformasjonModal);
