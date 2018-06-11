2e52de1a5e7a48dd9b3806d890f1cea64e61466d
Izis Frøyhov <izis.froyhov@nav.no>
2018-06-11 12:31:35 +0200

    FO-1063: Teste at dialogliste vises og at man kan trykke pa dialog

--- test/nightwatch-tester/integration/tests/browserstack.test.js
+++ test/nightwatch-tester/integration/tests/browserstack.test.js
@@ -1,14 +1,18 @@
-import { aktivitetsplanOversiktSide } from '../pages/sider';
+import {
+    aktivitetsplanOversiktSide,
+    dialogOversiktModal,
+} from '../pages/sider';
 
 let env, path;
 module.exports = {
     tags: ['browserstack'],
 
     before: function(browser) {
-        browser.useXpath();
         const capabilities = browser.options.desiredCapabilities;
         path = browser.options.screenshotsPath;
         env = `${capabilities.os}_${capabilities.browser}`;
+
+        browser.useXpath();
     },
 
     'Naviger til side': function(browser) {
@@ -18,6 +22,20 @@ module.exports = {
             .ventPaSideLast();
 
         browser.saveScreenshot(screenshots);
+    },
+
+    'Åpne dialog og trykk på første': function(browser) {
+        const screenshotsListe = `${path}/Dialogliste_${env}.png`;
+        const screenshotsDialog = `${path}/Dialog_${env}.png`;
+        const oversikt = aktivitetsplanOversiktSide(browser);
+
+        const dialogOversikt = oversikt.klikkDialog(
+            dialogOversiktModal(browser)
+        );
+        browser.saveScreenshot(screenshotsListe);
+        dialogOversikt.trykkPaDialog(0);
+
+        browser.saveScreenshot(screenshotsDialog);
         browser.end();
     },
 };
