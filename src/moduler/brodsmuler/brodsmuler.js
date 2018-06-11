8dceaa080b1e69c2791c97533ac3477bf577403b
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-11 16:53:59 +0200

    FO-961 Fjern brødsmulesti til Veien til arbeid for brukere uten oppfølging

--- src/moduler/brodsmuler/brodsmuler.js
+++ src/moduler/brodsmuler/brodsmuler.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PT from 'prop-types';
 import personSvg from './person.svg';
+import { selectUnderOppfolging } from '../oppfolging-status/oppfolging-selector';
 
 const DITTNAVN_PATH = '/dittnav/';
 const VEIENTILARBEID_PATH = '/veientilarbeid/';
 
-function Brodsmuler() {
+function Brodsmuler(props) {
     return (
         <div className="brodsmuler">
             <img
@@ -18,15 +21,24 @@ function Brodsmuler() {
                         Ditt NAV
                     </a>
                 </li>
-                <li className="brodsmuler__item typo-normal">
-                    <a href={VEIENTILARBEID_PATH} className="lenke">
-                        Veien til arbeid
-                    </a>
-                </li>
+                {props.underOppfolging &&
+                    <li className="brodsmuler__item typo-normal">
+                        <a href={VEIENTILARBEID_PATH} className="lenke">
+                            Veien til arbeid
+                        </a>
+                    </li>}
                 <li className="brodsmuler__item typo-normal">Aktivitetsplan</li>
             </ol>
         </div>
     );
 }
 
-export default Brodsmuler;
+Brodsmuler.propTypes = {
+    underOppfolging: PT.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+    underOppfolging: selectUnderOppfolging(state),
+});
+
+export default connect(mapStateToProps)(Brodsmuler);
