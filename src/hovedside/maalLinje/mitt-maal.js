8d2a71b13120dfbfa6f12f5af0acae796357d511
Sneha Desai <sneha.desai@nav.no>
2018-06-11 14:26:49 +0200

    [FO-899] Setter fokus til hele mitt mal boks

--- src/hovedside/maalLinje/mitt-maal.js
+++ src/hovedside/maalLinje/mitt-maal.js
@@ -7,24 +7,14 @@ import { Element } from 'nav-frontend-typografi';
 import Lenke from '../../felles-komponenter/utils/lenke';
 import mittMalSvg from './Illustrasjon_dette_gjor_du_bra.svg';
 import Innholdslaster from '../../felles-komponenter/utils/innholdslaster';
+import { selectErVeileder } from '../../moduler/identitet/identitet-selector';
+import { div as HiddenIfDiv } from '../../felles-komponenter/hidden-if/hidden-if';
 import {
     hentMal,
     selectGjeldendeMal,
     selectMalStatus,
 } from '../../moduler/mal/aktivitetsmal-reducer';
 import * as AppPT from '../../proptypes';
-
-const MalHeaderLenke = ({ harMaal }) => {
-    const url = harMaal ? '/mal' : '/mal/endre';
-
-    return (
-        <Lenke href={url} className="maalheader__lenke">
-            <Element className="maalheader__element">
-                <FormattedMessage id={'aktivitetsmal.mitt-mal'} />
-            </Element>
-        </Lenke>
-    );
-};
 
 const Mal = ({ mal }) => {
     if (!mal) {
@@ -45,26 +35,37 @@ const Mal = ({ mal }) => {
 
 class MittMaal extends Component {
     componentDidMount() {
-        this.props.doHentMal();
+        if (this.props.erPaInnsiden) {
+            this.props.doHentMal();
+        }
     }
 
     render() {
-        const { avhengigheter, mal } = this.props;
+        const { avhengigheter, mal, erPaInnsiden } = this.props;
+        const url = !!mal ? '/mal' : '/mal/endre';
 
         return (
-            <div className="mitt-maal">
-                <img
-                    src={mittMalSvg}
-                    alt="mittmal-illustrasjon"
-                    className="mittmal__illustrasjon"
-                />
-                <div className="mittmal_content">
-                    <MalHeaderLenke harMaal={!!mal} />
-                    <Innholdslaster avhengigheter={avhengigheter}>
-                        <Mal mal={mal} />
-                    </Innholdslaster>
-                </div>
-            </div>
+            <HiddenIfDiv hidden={!erPaInnsiden}>
+                <Lenke
+                    brukLenkestyling={false}
+                    href={url}
+                    className="mitt-maal"
+                >
+                    <img
+                        src={mittMalSvg}
+                        alt="mittmal-illustrasjon"
+                        className="mittmal__illustrasjon"
+                    />
+                    <div className="mittmal_content">
+                        <Element>
+                            <FormattedMessage id={'aktivitetsmal.mitt-mal'} />
+                        </Element>
+                        <Innholdslaster avhengigheter={avhengigheter}>
+                            <Mal mal={mal} />
+                        </Innholdslaster>
+                    </div>
+                </Lenke>
+            </HiddenIfDiv>
         );
     }
 }
@@ -79,21 +80,20 @@ Mal.propTypes = {
 
 MittMaal.defaultProps = {
     mal: undefined,
+    erPaInnsiden: undefined,
 };
 
 MittMaal.propTypes = {
     avhengigheter: AppPT.avhengigheter.isRequired,
     mal: PT.string,
     doHentMal: PT.func.isRequired,
-};
-
-MalHeaderLenke.propTypes = {
-    harMaal: PT.bool.isRequired,
+    erPaInnsiden: PT.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
     avhengigheter: [selectMalStatus(state)],
     mal: selectGjeldendeMal(state) && selectGjeldendeMal(state).mal,
+    erPaInnsiden: selectErVeileder(state),
 });
 
 const mapDispatchToProps = dispatch => ({
