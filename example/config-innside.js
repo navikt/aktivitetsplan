c92c9e158bfc8c6331f27e6b4a8cc1db6a900ee2
Izis Frøyhov <izis.froyhov@nav.no>
2018-06-27 09:42:12 +0200

    KAIZEN-0: bytte ut role med html tag

---
+++ example/config-innside.js
@@ -0,0 +1,19 @@
+export const API_BASE_URL = '/veilarbaktivitetsplanfs/api';
+
+export const CONTEXT_PATH = '/aktivitetsplanfelles';
+
+export const TILLAT_SLETTING = true;
+
+export const TILLAT_SET_AVTALT = true;
+
+export const VIS_SIDEBANNER = true;
+
+export const FNR_I_URL = true;
+
+export const EKSEMPEL_FNR = '12345678912';
+
+export const EKSEMPEL_VEILEDER = 'Z123456';
+
+export const VIS_MALER = true;
+
+export const INTERNFLATE = true;
