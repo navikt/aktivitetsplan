e8640d0469e72aef52cf615aae9ea819813be8f5
alexander-svendsen <alexander.svendsen@nav.no>
2018-09-07 13:29:07 +0200

    FO-908: Legg til tittel i arbeidsliste container

--- example/mocks/arbeidsliste.js
+++ example/mocks/arbeidsliste.js
@@ -4,6 +4,7 @@ export const arbeidsliste = {
     },
     endringstidspunkt: '2018-04-04T14:25:45.882Z',
     kommentar: 'Pirat virksomhet er vanskelig',
+    overskrift: 'Pirate life',
     frist: '2020-04-05T10:00:00Z',
     isOppfolgendeVeileder: true,
     arbeidslisteAktiv: null,
@@ -14,6 +15,7 @@ export function fjernArbeidsliste() {
     arbeidsliste.sistEndretAv = null;
     arbeidsliste.endringstidspunkt = null;
     arbeidsliste.kommentar = null;
+    arbeidsliste.overskrift = null;
     arbeidsliste.frist = null;
     arbeidsliste.arbeidslisteAktiv = null;
 
@@ -24,6 +26,7 @@ export function putArbeidsliste(body) {
     arbeidsliste.sistEndretAv = { veilederId: 'Z123456' };
     arbeidsliste.endringstidspunkt = new Date();
 
+    arbeidsliste.overskrift = body.overskrift;
     arbeidsliste.kommentar = body.kommentar;
     arbeidsliste.frist = body.frist;
 
