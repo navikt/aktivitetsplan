3958545944f18a352dabab2165d57897ead62110
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-09-18 15:05:55 +0200

    FO-1366: Use constants for feature names

--- src/felles-komponenter/feature/feature.js
+++ src/felles-komponenter/feature/feature.js
@@ -2,6 +2,8 @@ import PT from 'prop-types';
 import { connect } from 'react-redux';
 import { selectFeatureData } from './feature-selector';
 
+export const BRUKERVILKAR = 'brukervilkar';
+
 export function harFeature(name, features) {
     const app = window.app;
     const feature = features && features[app] && features[app][name];
