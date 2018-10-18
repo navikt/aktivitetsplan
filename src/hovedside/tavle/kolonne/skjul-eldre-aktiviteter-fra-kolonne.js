6cce944022be91441c8686c71cf638edac4bcbed
Alexandra Back <alexandra.back2@nav.no>
2018-10-18 09:08:02 +0200

    FO-645: Logging og fiks feilende nightwatch tester

--- src/hovedside/tavle/kolonne/skjul-eldre-aktiviteter-fra-kolonne.js
+++ src/hovedside/tavle/kolonne/skjul-eldre-aktiviteter-fra-kolonne.js
@@ -5,13 +5,19 @@ import { injectIntl, intlShape } from 'react-intl';
 import { VisibleIfLesmerpanel } from '../../../felles-komponenter/utils/visible-if-lesmerpanel';
 import { selectErVeileder } from '../../../moduler/identitet/identitet-selector';
 import { lagAktivitetsListe } from './../aktivitetstavle';
+import loggEvent from './../../../felles-komponenter/utils/logging';
 
-// const LOGGING_VISELDREAKITIVITETER = 'aktivitetsplan.viseldreaktiviter';
-// const LOGGING_SKJULELDREAKTIVITETER = 'aktivitetsplan.skjuleldreaktiviter';
+const LOGGING_VISELDREAKITIVITETER = 'aktivitetsplan.viseldreaktiviter';
+const LOGGING_SKJULELDREAKTIVITETER = 'aktivitetsplan.skjuleldreaktiviter';
+
+function loggingAvSkjulEldreAktiviteter(typeEvent, hvem) {
+    loggEvent(typeEvent, hvem);
+}
 
 function SkjulEldreAktiviteter({
     aktiviteteterTilDatoMerEnnToManederSiden,
     intl,
+    erVeileder,
 }) {
     const visible = aktiviteteterTilDatoMerEnnToManederSiden.length > 0;
     const apneTekst = intl.formatMessage({
@@ -24,6 +30,14 @@ function SkjulEldreAktiviteter({
         <VisibleIfLesmerpanel
             visible={visible}
             className="aktivitetstavle__kolonne-lesmerpanel"
+            onOpen={loggingAvSkjulEldreAktiviteter(
+                LOGGING_VISELDREAKITIVITETER,
+                erVeileder
+            )}
+            onClose={loggingAvSkjulEldreAktiviteter(
+                LOGGING_SKJULELDREAKTIVITETER,
+                erVeileder
+            )}
             apneTekst={apneTekst}
             lukkTekst={lukkTekst}
         >
