4570c4c58bce2e1cc3f37441c370f4c929be39ee
Alexandra Back <alexandra.back2@nav.no>
2018-11-07 15:30:13 +0100

    KAIZEN-0: Fjern etikett-reducern

--- src/felles-komponenter/aktivitet-etikett/aktivitet-etiketter-api.js
+++
@@ -1,6 +0,0 @@
-import { AKTIVITET_BASE_URL } from '../../environment';
-import { fetchToJson } from './../../ducks/utils';
-
-export default function hentEtiketter() {
-    return fetchToJson(`${AKTIVITET_BASE_URL}/aktivitet/etiketter`);
-}
