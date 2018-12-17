7e53f2a26676db8f9a18274e926de946c8fda3f7
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-12-17 17:00:29 +0100

    FO-1773: Fnr is automatically added to all xhr requests in fss, and in sbs we resolve fnr from oidc token

--- src/moduler/siste-innlogging/siste-innlogging-api.js
+++ src/moduler/siste-innlogging/siste-innlogging-api.js
@@ -2,6 +2,6 @@ import { VEILARBLEST_BASE_URL } from '../../environment';
 import { fetchToJson } from '../../ducks/utils';
 
 // eslint-disable-next-line import/prefer-default-export
-export function hentSisteInnlogging(fnr) {
-    return fetchToJson(`${VEILARBLEST_BASE_URL}/aktivitetsplan/les?fnr=${fnr}`);
+export function hentSisteInnlogging() {
+    return fetchToJson(`${VEILARBLEST_BASE_URL}/aktivitetsplan/les`);
 }
