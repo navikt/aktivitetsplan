3325d441a907b6b87f13cdb89a238297c3e70388
alexander-svendsen <alexander.svendsen@nav.no>
2019-01-28 12:36:38 +0100

    FO-876: LestInfo kan mangle i info-modalen

--- example/mocks/lest.js
+++ example/mocks/lest.js
@@ -4,4 +4,9 @@ export default [
         ressurs: 'aktivitetsplan',
         verdi: null,
     },
+    {
+        tidspunkt: '2018-08-31T11:46:10.971+01:00',
+        ressurs: 'informasjon',
+        verdi: 'v1',
+    },
 ];
