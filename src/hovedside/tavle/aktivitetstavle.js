cfc5c990912cde03a6f069f736b052a311112318
Joselito Poblete <joselito.poblete@gmail.com>
2019-02-06 16:47:46 +0100

    FO-1759 Logg antall eksterne brukere og veiledere

--- src/hovedside/tavle/aktivitetstavle.js
+++ src/hovedside/tavle/aktivitetstavle.js
@@ -27,6 +27,13 @@ import {
     NYENDRINGIAKTIVITET,
 } from '../../felles-komponenter/feature/feature';
 import { selectFeatureData } from '../../felles-komponenter/feature/feature-selector';
+import loggEvent from '../../felles-komponenter/utils/logging';
+
+const LOGGING_ANTALLBRUKERE = 'aktivitetsplan.antallBrukere';
+
+function loggingAntallBrukere(typeEvent, hvem) {
+    loggEvent(typeEvent, hvem);
+}
 
 export function lagAktivitetsListe(aktiviteter) {
     return aktiviteter.map(aktivitet =>
@@ -51,11 +58,14 @@ function renderFullFortAvbryt(aktiviteter) {
 class AktivitetsTavle extends Component {
     componentDidMount() {
         if (this.props.reducersNotStarted) {
-            if (this.props.erVeileder) {
+            const { erVeileder } = this.props;
+            if (erVeileder) {
                 doLesAktivitetsplan();
             }
             this.props.doHentAktiviteter();
             this.props.doHentArenaAktiviteter();
+
+            loggingAntallBrukere(LOGGING_ANTALLBRUKERE, { erVeileder });
         }
     }
 
