db992056e9ae5130d3def47554ab325943354878
alexander-svendsen <alexander.svendsen@nav.no>
2019-04-03 13:52:46 +0200

    KAIZEN-0: Inline svg
    Delete unused svgs and move the inlined svg to the folder they are used.
    Maybe we will inline them properly with react when we bump react to 16

--- src/moduler/sidebanner/sidebanner.js
+++ src/moduler/sidebanner/sidebanner.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Sidetittel } from 'nav-frontend-typografi';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
-import aktivitetsplanSvg from './aktivitetsplan.svg';
-import Bilde from '../../felles-komponenter/bilde/bilde';
+import { FormattedMessage } from 'react-intl';
+import BannerIcon from './banner-icon';
 import visibleIf from '../../hocs/visible-if';
 import Brodsmuler from '../brodsmuler/brodsmuler';
 
-function Sidebanner(props) {
+function Sidebanner() {
     return (
         <div className="sidebanner-container">
             <div className="sidebanner-grid">
                 <Brodsmuler />
                 <div className="sidebanner">
-                    <Bilde
-                        src={aktivitetsplanSvg}
-                        alt={props.intl.formatMessage({
-                            id: 'sidebanner-illustrasjon.alt-tekst',
-                        })}
-                        className="sidebanner__illustrasjon"
-                    />
+                    <BannerIcon/>
                     <Sidetittel className="sidebanner__tittel">
                         <FormattedMessage id="hovedside.tittel" />
                     </Sidetittel>
@@ -28,8 +21,4 @@ function Sidebanner(props) {
     );
 }
 
-Sidebanner.propTypes = {
-    intl: intlShape.isRequired,
-};
-
-export default visibleIf(injectIntl(Sidebanner));
+export default visibleIf(Sidebanner);
