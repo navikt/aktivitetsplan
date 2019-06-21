978ee4656ffe54ba6b37260d967e7fe611f35d4e
Izis Frøyhov <izisfroyhov@gmail.com>
2019-06-21 13:20:39 +0200

    FO-2337: demomock styling

---
+++ src/mocks/demoBanner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { DemoIkon } from './demoIkon';
+import Modal from 'nav-frontend-modal';
+import DemoDashboard from './demoDashboard';
+
+class DemoBanner extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalIsOpen: this.props.modalIsOpen,
+        };
+    }
+
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    };
+
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    };
+
+    render() {
+        return (
+            <div>
+                <DemoIkon onClick={this.openModal} />
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    closeButton={true}
+                >
+                    <DemoDashboard />
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default DemoBanner;
