496b625e95b89f6fed630eb99938f457cb2f0b81
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-07-25 10:34:53 +0200

    FO-1780: Bruk rett selector for mal i test

--- integrationtest/startside-model.js
+++ integrationtest/startside-model.js
@@ -4,11 +4,17 @@ export default class Startside {
     constructor() {
         this.side = Selector('.aktivitetsplanfs');
         this.btnDialog = Selector('[href$="/dialog"]');
-        this.btnMittMal = Selector('.maal .lenke-knapp');
+        this.btnMittMal = Selector('.maal__inner .lenke-knapp');
         this.btnPrint = Selector('.verktoylinje__print-knapp');
         this.aktivitetsTavle = Selector('.aktivitetstavle');
-        this.kolForslag = Selector('[data-testid="aktivitetstavle.BRUKER_ER_INTERESSERT"]');
-        this.kolPlanlegger = Selector('[data-testid="aktivitetstavle.PLANLAGT"]');
-        this.kolGjennomforer = Selector('[data-testid="aktivitetstavle.GJENNOMFORES"]');
+        this.kolForslag = Selector(
+            '[data-testid="aktivitetstavle.BRUKER_ER_INTERESSERT"]'
+        );
+        this.kolPlanlegger = Selector(
+            '[data-testid="aktivitetstavle.PLANLAGT"]'
+        );
+        this.kolGjennomforer = Selector(
+            '[data-testid="aktivitetstavle.GJENNOMFORES"]'
+        );
     }
 }
