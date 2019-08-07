39d9e3dc9265b160d3ecebb5f96133efd2cf6958
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-08-07 11:06:19 +0200

    KAIZEN-0: Fiks onSubmit for old forms

--- src/utils/object.js
+++ src/utils/object.js
@@ -2,7 +2,12 @@
 
 export function removeEmptyKeysFromObject(object) {
     return Object.keys(object).reduce((obj, key) => {
-        if (object[key].length > 0) {
+        // todo: maybe the forms don't have the booleans at a later date, so remove this
+        if (
+            object[key] === false ||
+            object[key] === true ||
+            (object[key] && object[key].length > 0)
+        ) {
             obj[key] = object[key]; // eslint-disable-line
         }
         return obj;
