7d420d2d2f4f0faf9141cab834a3011753851b84
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-08-07 16:52:07 +0200

    KAIZEN-0: Sett tomme verdier i filter til null

--- src/utils/object.js
+++ src/utils/object.js
@@ -2,12 +2,9 @@
 
 export function removeEmptyKeysFromObject(object) {
     return Object.keys(object).reduce((obj, key) => {
-        // todo: maybe the forms don't have the booleans at a later date, so remove this
-        if (
-            object[key] === false ||
-            object[key] === true ||
-            (object[key] && object[key].length > 0)
-        ) {
+        if (object[key] !== null && object[key].length <= 0) {
+            obj[key] = null; // eslint-disable-line
+        } else {
             obj[key] = object[key]; // eslint-disable-line
         }
         return obj;
