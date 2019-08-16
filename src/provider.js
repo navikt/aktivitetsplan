3b4ec8b0b139a391f68902be0a934bcfb29e0f72
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-08-16 15:26:30 +0200

    KAIZEN-0: Fjern reducere som ikke var i bruk

--- src/provider.js
+++ src/provider.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PT from 'prop-types';
 import { Provider as ReduxProvider } from 'react-redux';
 import { DragDropContext } from 'react-dnd';
@@ -6,31 +6,23 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import IntlProvider from './intl-provider';
 import InitiellDataLast from './initiell-data-last';
 import componentize from './componentize';
-import { RESET_STORE } from './reducer';
 import createStore from './store';
 
 const store = createStore(window.history);
 
-class Provider extends Component {
-    constructor(props) {
-        super(props);
-        store.dispatch(RESET_STORE);
-    }
-
-    render() {
-        const {children} = this.props;
-        return (
-            <ReduxProvider store={store}>
-                <InitiellDataLast>
-                    <IntlProvider defaultLocale="nb" locale="nb" messages={{}}>
-                        <div>
-                            {children}
-                        </div>
-                    </IntlProvider>
-                </InitiellDataLast>
-            </ReduxProvider>
-        );
-    }
+function Provider(props) {
+    const { children } = props;
+    return (
+        <ReduxProvider store={store}>
+            <InitiellDataLast>
+                <IntlProvider defaultLocale="nb" locale="nb" messages={{}}>
+                    <div>
+                        {children}
+                    </div>
+                </IntlProvider>
+            </InitiellDataLast>
+        </ReduxProvider>
+    );
 }
 
 Provider.propTypes = {
