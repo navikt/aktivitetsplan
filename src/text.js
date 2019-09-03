694cb5766ead92d3384552f8db9be6308967c3d1
Izis Frøyhov <izisfroyhov@gmail.com>
2019-09-03 09:49:12 +0200

    FO-2474: flytter ut htmlText komponent til egent fil

--- src/text.js
+++ src/text.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PT from 'prop-types';
 import { connect } from 'react-redux';
 import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 import hiddenIf from './felles-komponenter/hidden-if/hidden-if';
@@ -42,30 +41,7 @@ function textHOC(Component, props) {
     return hiddenIf(connect(mapStateToProps)(Text));
 }
 
-function FormattedHTMLMessageProxy(props) {
-    const { id, className, ...restValues } = props;
-    return (
-        <FormattedHTMLMessage id={id} values={restValues}>
-            {content =>
-                <div
-                    className={className}
-                    dangerouslySetInnerHTML={{ __html: content }} // eslint-disable-line react/no-danger
-                />}
-        </FormattedHTMLMessage>
-    );
-}
-
-FormattedHTMLMessageProxy.propTypes = {
-    id: PT.string.isRequired,
-    className: PT.string,
-};
-
-FormattedHTMLMessageProxy.defaultProps = {
-    className: undefined,
-};
-
 export default textHOC(FormattedMessage);
-export const HtmlText = FormattedHTMLMessageProxy;
 export const FailsafeText = textHOC(FormattedHTMLMessage, {
     visChildrenVedFeil: true,
 });
