26028f07e56664f16afdb01f079f7f42ecc02054
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-10-04 14:54:25 +0200

    KAIZEN-0: Fjern intl i har-ikke-aktivitetsplan

--- src/moduler/oppfolging-status/har-ikke-aktivitetsplan.js
+++ src/moduler/oppfolging-status/har-ikke-aktivitetsplan.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { HoyreChevron } from 'nav-frontend-chevron';
@@ -10,18 +9,16 @@ export const arbeidssokerregistreringHref = '/arbeidssokerregistrering';
 
 function HarIkkeAktivitetsplan({ erVeileder }) {
     const advarsel = erVeileder
-        ? 'har.ikke.aktivitetsplan.advarsel.veileder'
-        : 'har.ikke.aktivitetsplan.advarsel.bruker';
+        ? 'Denne brukeren er ikke registrert.'
+        : 'Du må være registrert hos NAV for å bruke aktivitetsplanen.';
 
     return (
         <div className="har-ikke-aktivitetsplan-container">
             <AlertStripeAdvarsel>
-                <Normaltekst>
-                    <FormattedMessage id={advarsel} />
-                </Normaltekst>
+                <Normaltekst>{advarsel}</Normaltekst>
                 {!erVeileder && (
                     <Lenke href={arbeidssokerregistreringHref} erEksternLenke>
-                        <FormattedMessage id="ikke.under.oppfolging.reaktivering.lenke" />
+                        Register deg hos NAV
                         <HoyreChevron />
                     </Lenke>
                 )}
