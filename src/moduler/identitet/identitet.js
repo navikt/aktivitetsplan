50d64c549d04b5c4a9a45d598644148566812f95
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-10-07 10:17:19 +0200

    KAIZEN-0: Fjern intl i identitet

--- src/moduler/identitet/identitet.js
+++ src/moduler/identitet/identitet.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PT from 'prop-types';
-import { injectIntl, intlShape } from 'react-intl';
 import { connect } from 'react-redux';
 import { hentIdentitet } from './identitet-reducer';
 
