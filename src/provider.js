71629586554e7c56f6df52baee638939361f1a64
alexander.svendsen <alexandersvendsen1990@gmail.com>
2019-11-08 15:44:46 +0100

    KAIZEN-0: Bump react-dnd

--- src/provider.js
+++ src/provider.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import PT from 'prop-types';
 import { Provider as ReduxProvider } from 'react-redux';
-import { DragDropContext } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import IntlProvider from './intl-provider';
 import InitiellDataLast from './initiell-data-last';
@@ -15,11 +15,13 @@ class Provider extends Component {
         const { children } = this.props;
         return (
             <ReduxProvider store={store}>
-                <InitiellDataLast>
-                    <IntlProvider defaultLocale="nb" locale="nb" messages={{}}>
-                        <div>{children}</div>
-                    </IntlProvider>
-                </InitiellDataLast>
+                <DndProvider backend={HTML5Backend}>
+                    <InitiellDataLast>
+                        <IntlProvider defaultLocale="nb" locale="nb" messages={{}}>
+                            <div>{children}</div>
+                        </IntlProvider>
+                    </InitiellDataLast>
+                </DndProvider>
             </ReduxProvider>
         );
     }
@@ -29,4 +31,4 @@ Provider.propTypes = {
     children: PT.node.isRequired
 };
 
-export default DragDropContext(HTML5Backend)(Provider);
+export default Provider;
