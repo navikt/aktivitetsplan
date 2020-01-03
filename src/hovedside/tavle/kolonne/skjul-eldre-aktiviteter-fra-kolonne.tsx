054258781e262dec89d00709ef706b5930bcd165
hakon planke holm <hakon.planke.holm@nav.no>
2020-01-03 12:26:15 +0100

    [FO-2567] flyte toglen over de eldre aktivitetene

---
+++ src/hovedside/tavle/kolonne/skjul-eldre-aktiviteter-fra-kolonne.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PT from 'prop-types';
+import { selectErVeileder } from '../../../moduler/identitet/identitet-selector';
+import loggEvent from '../../../felles-komponenter/utils/logging';
+import AktivitetsKort from '../../../moduler/aktivitet/aktivitet-kort/aktivitetskort';
+import InvertedLestMer from './InvertedLesmer';
+import { Aktivitet } from '../../../types';
+
+const LOGGING_VISELDREAKITIVITETER = 'aktivitetsplan.viseldreaktiviter';
+const LOGGING_SKJULELDREAKTIVITETER = 'aktivitetsplan.skjuleldreaktiviter';
+
+interface AktivitetListeProps {
+    aktiviteter: Aktivitet[];
+}
+
+function AktivitetsListe(props: AktivitetListeProps) {
+    const liste = props.aktiviteter.map(aktivitet => <AktivitetsKort key={aktivitet.id} aktivitet={aktivitet} />);
+    return <> {liste} </>;
+}
+
+interface Props {
+    aktiviteteterTilDatoMerEnnToManederSiden: Aktivitet[];
+}
+
+function SkjulEldreAktiviteter(props: Props) {
+    const erVeileder = useSelector(selectErVeileder);
+    const aktiviteter = props.aktiviteteterTilDatoMerEnnToManederSiden;
+    const hiden = aktiviteter.length === 0;
+
+    return (
+        <InvertedLestMer
+            hidden={hiden}
+            onOpen={() => loggEvent(LOGGING_VISELDREAKITIVITETER, { erVeileder })}
+            onClose={() => loggEvent(LOGGING_SKJULELDREAKTIVITETER, { erVeileder })}
+            apneTekst="Vis kort eldre enn 1 måned"
+            lukkTekst="Skjul kort eldre enn 1 måned"
+        >
+            <AktivitetsListe aktiviteter={aktiviteter} />
+        </InvertedLestMer>
+    );
+}
+
+SkjulEldreAktiviteter.propTypes = {
+    aktiviteteterTilDatoMerEnnToManederSiden: PT.arrayOf(PT.object).isRequired,
+    erVeileder: PT.bool.isRequired
+};
+
+export default SkjulEldreAktiviteter;
