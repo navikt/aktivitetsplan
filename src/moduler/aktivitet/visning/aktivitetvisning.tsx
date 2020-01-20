4c5a74297a78bcc35d6bb1c73fb01128952819dc
hakon planke holm <hakon.planke.holm@nav.no>
2020-01-20 11:56:10 +0100

    [FO-2552] lenke til ny dialogløsnin for veileder

--- src/moduler/aktivitet/visning/aktivitetvisning.tsx
+++ src/moduler/aktivitet/visning/aktivitetvisning.tsx
@@ -4,7 +4,7 @@ import UnderelementerForAktivitet from './underelement-for-aktivitet/underelemen
 import * as AppPT from '../../../proptypes';
 import ModalContainer from '../../../felles-komponenter/modal/modal-container';
 import AvtaltContainer from './avtalt-container/avtalt-container';
-import { TILTAK_AKTIVITET_TYPE, GRUPPE_AKTIVITET_TYPE, UTDANNING_AKTIVITET_TYPE } from '../../../constant';
+import { GRUPPE_AKTIVITET_TYPE, TILTAK_AKTIVITET_TYPE, UTDANNING_AKTIVITET_TYPE } from '../../../constant';
 import BegrunnelseBoks from './hjelpekomponenter/begrunnelse-boks';
 import VarslingBoks from './hjelpekomponenter/varsling-boks';
 import AktivitetinformasjonVisning from './hjelpekomponenter/aktivitetinformasjon-visning';
@@ -12,13 +12,30 @@ import Statusadministrasjon from './hjelpekomponenter/statusadministrasjon';
 import ReferatContainer from './referat/referat-container';
 import lazyHOC from '../../../felles-komponenter/lazy/lazyHOC';
 import { trengerBegrunnelse } from '../aktivitet-util';
+import DialogLink from './underelement-for-aktivitet/DialogLink';
+import { useSelector } from 'react-redux';
+import { selectFeatureData } from '../../../felles-komponenter/feature/feature-selector';
+import { harFeature, VIS_NY_DIALOG } from '../../../felles-komponenter/feature/feature';
+import EndringsLogg from './underelement-for-aktivitet/EndringsLogg';
+import { Aktivitet } from '../../../types';
 
-function Aktivitetvisning({ aktivitet, tillatEndring, laster, underOppfolging }) {
+interface Props {
+    aktivitet: Aktivitet;
+    tillatEndring: boolean;
+    laster: boolean;
+    underOppfolging: boolean;
+}
+
+function Aktivitetvisning(props: Props) {
+    const { aktivitet, tillatEndring, laster, underOppfolging } = props;
+
+    const features = useSelector(selectFeatureData);
+    const nyDialog = harFeature(VIS_NY_DIALOG, features);
     const arenaAktivitet = [TILTAK_AKTIVITET_TYPE, GRUPPE_AKTIVITET_TYPE, UTDANNING_AKTIVITET_TYPE].includes(
         aktivitet.type
     );
 
-    const visBegrunnelse = !arenaAktivitet && trengerBegrunnelse(aktivitet.avtalt, aktivitet.status, aktivitet.type);
+    const visBegrunnelse = !arenaAktivitet && trengerBegrunnelse(!!aktivitet.avtalt, aktivitet.status, aktivitet.type);
 
     return (
         <div>
@@ -33,7 +50,6 @@ function Aktivitetvisning({ aktivitet, tillatEndring, laster, underOppfolging })
 
                 <AktivitetinformasjonVisning
                     valgtAktivitet={aktivitet}
-                    arenaAktivitet={arenaAktivitet}
                     tillatEndring={tillatEndring}
                     underOppfolging={underOppfolging}
                     laster={laster}
@@ -45,11 +61,17 @@ function Aktivitetvisning({ aktivitet, tillatEndring, laster, underOppfolging })
                     className="aktivitetvisning__underseksjon"
                 />
 
-                <ReferatContainer aktivitet={aktivitet} underOppfolging={underOppfolging} />
+                <ReferatContainer aktivitet={aktivitet} />
 
                 <Statusadministrasjon aktivitet={aktivitet} arenaAktivitet={arenaAktivitet} />
 
-                <UnderelementerForAktivitet aktivitet={aktivitet} className="aktivitetvisning__underseksjon" />
+                <UnderelementerForAktivitet
+                    aktivitet={aktivitet}
+                    hidden={nyDialog}
+                    className="aktivitetvisning__underseksjon"
+                />
+                <DialogLink aktivitet={aktivitet} hidden={!nyDialog} skulDelelingje={arenaAktivitet} />
+                <EndringsLogg aktivitet={aktivitet} hidden={!nyDialog || arenaAktivitet} />
             </ModalContainer>
         </div>
     );
