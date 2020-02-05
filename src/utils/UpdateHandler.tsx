9e19298b556a98864e89f6dc9a5cc557b334381a
hakon planke holm <hakon.planke.holm@nav.no>
2020-02-05 16:54:19 +0100

    [FO-2721] komunikasjon mellom flatene

---
+++ src/utils/UpdateHandler.tsx
@@ -0,0 +1,61 @@
+import { useEventListener } from '../felles-komponenter/hooks/useEventListner';
+import { useDispatch } from 'react-redux';
+import { hentAktiviteter } from '../moduler/aktivitet/aktivitet-actions';
+import { hentDialog } from '../moduler/dialog/dialog-reducer';
+import { hentOppfolging } from '../moduler/oppfolging-status/oppfolging-reducer';
+import React from 'react';
+
+export enum UpdateTypes {
+    Dialog = 'DIALOG',
+    Oppfolging = 'OPPFOLGING',
+    Aktivitet = 'AKTIVITET'
+}
+
+interface UpdateEventType {
+    uppdate: string;
+    avsender?: string;
+}
+
+const eventName = 'uppdate';
+
+export function widowEvent(update: UpdateTypes) {
+    window.dispatchEvent(
+        new CustomEvent<UpdateEventType>(eventName, { detail: { uppdate: update, avsender: 'aktivitetsplan' } })
+    );
+}
+
+function isUpdateEvent(toBeDetermined: Event): toBeDetermined is CustomEvent<UpdateEventType> {
+    if ((toBeDetermined as CustomEvent<UpdateEventType>).type) {
+        return true;
+    }
+
+    return false;
+}
+
+export function UppdateEventHandler() {
+    const dispatch = useDispatch();
+
+    useEventListener(eventName, event => {
+        if (!isUpdateEvent(event)) {
+            return;
+        }
+
+        const updateType = event.detail.uppdate;
+        const avsennder = event.detail.avsender;
+
+        if (avsennder === 'aktivitetsplan') {
+            return;
+        }
+
+        switch (updateType) {
+            case UpdateTypes.Aktivitet:
+                return dispatch(hentAktiviteter());
+            case UpdateTypes.Dialog:
+                return dispatch(hentDialog());
+            case UpdateTypes.Oppfolging:
+                return dispatch(hentOppfolging());
+        }
+    });
+
+    return <></>;
+}
