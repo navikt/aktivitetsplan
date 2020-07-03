cff16b995712e4953b039a2e493518825d0f9a68
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-07-03 14:34:44 +0200

    Kunn oppdatert sistOppdatert ved henting

--- src/moduler/dialog/dialog-reducer.js
+++ src/moduler/dialog/dialog-reducer.js
@@ -8,19 +8,7 @@ export const HENTER = 'dialog/hent';
 export const HENTET = 'dialog/hent/ok';
 export const HENTING_FEILET = 'dialog/hent/fail';
 
-export const OPPRETTER_HENVENDELSE = 'dialog/henvendelse/opprett';
-export const OPPRETTET_HENVENDELSE = 'dialog/henvendelse/opprett/ok';
-export const OPPRETT_HENVENDELSE_FEILET = 'dialog/henvendelse/opprett/fail';
-
-export const DIALOG_LEST = 'dialog/lest';
-export const DIALOG_LEST_OK = 'dialog/lest/ok';
-export const DIALOG_LEST_FEILET = 'dialog/lest/fail';
-
-export const OPPDATER_DIALOG = 'dialog/oppdater';
-export const OPPDATER_DIALOG_OK = 'dialog/oppdater/ok';
-export const OPPDATER_DIALOG_FEILET = 'dialog/oppdater/fail';
 export const ESKALERINGS_FILTER_TYPE = 'dialog/eskalering';
-export const ESKALERINGS_FILTER = { type: ESKALERINGS_FILTER_TYPE };
 
 export const SEND_FORHANDSORIENTERING_OK = 'dialog/forhandsorientering/ok';
 export const SEND_FORHANDSORIENTERING_FEILET = 'dialog/forhandsorientering/fail';
@@ -42,16 +30,13 @@ function nyStateMedOppdatertDialog(state, dialog) {
     } else {
         nyData.unshift(dialog); // prepend
     }
-    return { ...state, status: STATUS.OK, data: nyData, sistOppdatert: moment().toISOString() };
+    return { ...state, status: STATUS.OK, data: nyData };
 }
 
 // Reducer
 export default function reducer(state = initalState, action) {
     const { data } = action;
     switch (action.type) {
-        case OPPRETTER_HENVENDELSE:
-        case OPPDATER_DIALOG:
-            return { ...state, status: STATUS.RELOADING };
         case HENTET:
             return {
                 ...state,
@@ -65,14 +50,8 @@ export default function reducer(state = initalState, action) {
                 esklaringsFilter: !state.esklaringsFilter,
             };
         case HENTING_FEILET:
-        case OPPRETT_HENVENDELSE_FEILET:
-        case DIALOG_LEST_FEILET:
-        case OPPDATER_DIALOG_FEILET:
         case SEND_FORHANDSORIENTERING_FEILET:
             return { ...state, status: STATUS.ERROR, feil: data };
-        case OPPRETTET_HENVENDELSE:
-        case DIALOG_LEST_OK:
-        case OPPDATER_DIALOG_OK:
         case SEND_FORHANDSORIENTERING_OK:
             widowEvent(UpdateTypes.Dialog);
             return nyStateMedOppdatertDialog(state, data);
@@ -87,38 +66,6 @@ export function hentDialog() {
         OK: HENTET,
         FEILET: HENTING_FEILET,
         PENDING: HENTER,
-    });
-}
-
-export function nyHenvendelse(henvendelse) {
-    return doThenDispatch(() => Api.nyHenvendelse(henvendelse), {
-        OK: OPPRETTET_HENVENDELSE,
-        FEILET: OPPRETT_HENVENDELSE_FEILET,
-        PENDING: OPPRETTER_HENVENDELSE,
-    });
-}
-
-export function markerDialogSomLest(dialogId) {
-    return doThenDispatch(() => Api.markerDialogSomLest(dialogId), {
-        OK: DIALOG_LEST_OK,
-        FEILET: DIALOG_LEST_FEILET,
-        PENDING: DIALOG_LEST,
-    });
-}
-
-export function oppdaterFerdigbehandlet(dialogId, erFerdigbehandlet) {
-    return doThenDispatch(() => Api.oppdaterFerdigbehandlet(dialogId, erFerdigbehandlet), {
-        OK: OPPDATER_DIALOG_OK,
-        FEILET: OPPDATER_DIALOG_FEILET,
-        PENDING: OPPDATER_DIALOG,
-    });
-}
-
-export function oppdaterVenterPaSvar(dialogId, venterPaSvar) {
-    return doThenDispatch(() => Api.oppdaterVenterPaSvar(dialogId, venterPaSvar), {
-        OK: OPPDATER_DIALOG_OK,
-        FEILET: OPPDATER_DIALOG_FEILET,
-        PENDING: OPPDATER_DIALOG,
     });
 }
 
