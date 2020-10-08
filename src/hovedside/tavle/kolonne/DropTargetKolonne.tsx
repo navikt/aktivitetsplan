381cc5a02eeab4b3d47a2695087c36ba1af1a9f7
Thea Hove Johansen <thea.hove.johansen@nav.no>
2020-10-08 10:41:06 +0200

    Legge tilbake original funksjonalitet på drag and drop

--- src/hovedside/tavle/kolonne/DropTargetKolonne.tsx
+++ src/hovedside/tavle/kolonne/DropTargetKolonne.tsx
@@ -1,32 +1,13 @@
 import React, { ReactNode } from 'react';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import PT from 'prop-types';
+import { useDispatch } from 'react-redux';
 import classNames from 'classnames';
 import { useDrop } from 'react-dnd';
 import { useHistory } from 'react-router-dom';
-import {
-    GRUPPE_AKTIVITET_TYPE,
-    MOTE_TYPE,
-    SAMTALEREFERAT_TYPE,
-    STATUS_AVBRUTT,
-    STATUS_FULLFOERT,
-    TILTAK_AKTIVITET_TYPE,
-    UTDANNING_AKTIVITET_TYPE,
-} from '../../../constant';
+import { STATUS_AVBRUTT, STATUS_FULLFOERT } from '../../../constant';
 import { flyttAktivitet } from '../../../moduler/aktivitet/aktivitet-actions';
 import { flyttetAktivitetMetrikk } from '../../../felles-komponenter/utils/logging';
 import { avbrytAktivitetRoute, fullforAktivitetRoute } from '../../../routes';
-import { selectErBruker } from '../../../moduler/identitet/identitet-selector';
 import { Aktivitet, AktivitetStatus } from '../../../types';
-import { selectErUnderOppfolging } from '../../../moduler/oppfolging-status/oppfolging-selector';
-
-function sjekkErDroppbar(aktivitet: Aktivitet, erBruker: boolean) {
-    const { type, status, nesteStatus, historisk } = aktivitet;
-    const erArenaAktivitet = [TILTAK_AKTIVITET_TYPE, GRUPPE_AKTIVITET_TYPE, UTDANNING_AKTIVITET_TYPE].includes(type);
-    const erFerdig = [STATUS_FULLFOERT, STATUS_AVBRUTT].includes(status);
-    const brukerKanOppdater = [SAMTALEREFERAT_TYPE, MOTE_TYPE].includes(type) && erBruker;
-    return !nesteStatus && !historisk && !erFerdig && !erArenaAktivitet && !brukerKanOppdater;
-}
 
 interface Props {
     status: AktivitetStatus;
@@ -43,13 +24,10 @@ export const DROP_TYPE = 'AktivitetsKort';
 function DropTargetKolonne({ status, children }: Props) {
     const dispatch = useDispatch();
     const history = useHistory();
-    const erBruker = useSelector(selectErBruker, shallowEqual);
-    const erUnderOppfolging = useSelector(selectErUnderOppfolging, shallowEqual);
 
     const [collectedProps, drop] = useDrop({
         accept: DROP_TYPE,
-        canDrop: ({ aktivitet }: DragItem) =>
-            status !== aktivitet.status && sjekkErDroppbar(aktivitet, erBruker) && erUnderOppfolging,
+        canDrop: ({ aktivitet }: DragItem) => status !== aktivitet.status,
         drop: ({ aktivitet }: DragItem) => {
             flyttetAktivitetMetrikk('dragAndDrop', aktivitet, status);
             if (status === STATUS_FULLFOERT) {
@@ -79,10 +57,5 @@ function DropTargetKolonne({ status, children }: Props) {
         </div>
     );
 }
-
-DropTargetKolonne.propTypes = {
-    status: PT.string.isRequired,
-    children: PT.node,
-};
 
 export default DropTargetKolonne;
