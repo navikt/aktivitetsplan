adceee7fc16a955d947706e8a0d7a141b8132f17
hakon planke holm <hakon.planke.holm@nav.no>
2020-11-12 14:13:52 +0100

    min id pasport fiks

--- src/moduler/feilmelding/HovedsideFeilmelding.tsx
+++ src/moduler/feilmelding/HovedsideFeilmelding.tsx
@@ -6,6 +6,7 @@ import Feilmelding from './Feilmelding';
 import { selectLestFeilMelding } from '../lest/lest-reducer';
 import { selectDialogFeilmeldinger } from '../dialog/dialog-selector';
 import { useSelector, shallowEqual } from 'react-redux';
+import { selectNivaa4Feilmeldinger } from '../tilgang/tilgang-selector';
 
 export default function HovedsideFeilmelding() {
     const oppfFeil = useSelector(selectOppfolgingFeilmeldinger, shallowEqual);
@@ -13,8 +14,9 @@ export default function HovedsideFeilmelding() {
     const aktivitetFeil = useSelector(selectAktivitetListeFeilMelding);
     const lestFeil = useSelector(selectLestFeilMelding, shallowEqual);
     const dialogFeil = useSelector(selectDialogFeilmeldinger, shallowEqual);
+    const nivaa4Feil = useSelector(selectNivaa4Feilmeldinger, shallowEqual);
 
-    const alleFeil = oppfFeil.concat(identitetFeil, aktivitetFeil, lestFeil, dialogFeil);
+    const alleFeil = oppfFeil.concat(identitetFeil, aktivitetFeil, lestFeil, dialogFeil, nivaa4Feil);
 
     return <Feilmelding feilmeldinger={alleFeil} className="container" />;
 }
---
+++ src/moduler/feilmelding/IkkeNiva4.tsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { selectNivaa4 } from '../tilgang/tilgang-selector';
+import classNames from 'classnames';
+import React from 'react';
+
+import styles from './Feilmelding.module.less';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+
+const Nivaa4Feilmelding = () => {
+    const niva4 = useSelector(selectNivaa4);
+
+    if (niva4) {
+        return null;
+    }
+
+    return (
+        <div className={classNames(styles.feilmelding)}>
+            <AlertStripeAdvarsel>
+                Systemet får ikke sjekket om denne brukeren er en digital eller manuell bruker. <br />
+                Dette er et midlertidig problem på grunn av ny teknisk løsning etter koronasituasjonen.
+                <ul>
+                    <li>
+                        Hvis brukeren svarer på digital dialog, så kan du kommunisere digitalt. Forhåndsorientering og
+                        varsel vil ikke fungere på denne brukeren.
+                        <br />
+                    </li>
+                    <li>Hvis brukeren ikke svarer på digital dialog, så setter du brukeren til manuell bruker.</li>
+                </ul>
+            </AlertStripeAdvarsel>
+        </div>
+    );
+};
+
+export default Nivaa4Feilmelding;
