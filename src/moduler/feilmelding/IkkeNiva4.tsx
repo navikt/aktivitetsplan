4cb614e4302bd8e96af2f1e75ebbff874d9a7773
hakon planke holm <hakon.planke.holm@nav.no>
2020-11-18 12:37:20 +0100

    endre nivaa 4 feilmeldingen

--- src/moduler/feilmelding/IkkeNiva4.tsx
+++ src/moduler/feilmelding/IkkeNiva4.tsx
@@ -1,10 +1,26 @@
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import Hjelpetekst from 'nav-frontend-hjelpetekst';
+import { PopoverOrientering } from 'nav-frontend-popover';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { selectErBrukerManuell, selectReservasjonKRR } from '../oppfolging-status/oppfolging-selector';
 import { selectNivaa4, selectNivaa4LastetOk } from '../tilgang/tilgang-selector';
 import styles from './Feilmelding.module.less';
-import { selectErBrukerManuell, selectReservasjonKRR } from '../oppfolging-status/oppfolging-selector';
+
+const Mere = () => (
+    <Hjelpetekst type={PopoverOrientering.Under}>
+        Dette er et midlertidig problem på grunn av ny teknisk løsning etter koronasituasjonen.
+        <ul>
+            <li>
+                Hvis brukeren svarer på digital dialog, så kan du kommunisere digitalt. Forhåndsorientering og varsel
+                vil ikke fungere på denne brukeren.
+                <br />
+            </li>
+            <li>Hvis brukeren ikke svarer på digital dialog, så setter du brukeren til manuell bruker.</li>
+        </ul>
+    </Hjelpetekst>
+);
 
 const Nivaa4Feilmelding = () => {
     const niva4 = useSelector(selectNivaa4);
@@ -19,16 +35,7 @@ const Nivaa4Feilmelding = () => {
     return (
         <div className={styles.feilmelding}>
             <AlertStripeAdvarsel>
-                Systemet får ikke sjekket om denne brukeren er en digital eller manuell bruker. <br />
-                Dette er et midlertidig problem på grunn av ny teknisk løsning etter koronasituasjonen.
-                <ul>
-                    <li>
-                        Hvis brukeren svarer på digital dialog, så kan du kommunisere digitalt. Forhåndsorientering og
-                        varsel vil ikke fungere på denne brukeren.
-                        <br />
-                    </li>
-                    <li>Hvis brukeren ikke svarer på digital dialog, så setter du brukeren til manuell bruker.</li>
-                </ul>
+                Systemet får ikke sjekket om denne brukeren er en digital eller manuell bruker. <Mere />
             </AlertStripeAdvarsel>
         </div>
     );
