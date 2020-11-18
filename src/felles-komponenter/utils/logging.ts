77f173417718b3ece13237beec14627a3fbd3b33
Izis Frøyhov <izisfroyhov@gmail.com>
2020-11-18 11:39:23 +0100

    Bruke npm pakke for navspa

--- src/felles-komponenter/utils/logging.ts
+++ src/felles-komponenter/utils/logging.ts
@@ -8,7 +8,7 @@ interface Frontendlogger {
 
 export default function loggEvent(eventNavn: string, feltObjekt?: object, tagObjekt?: object) {
     const frontendlogger: Frontendlogger = (window as any).frontendlogger;
-    if (frontendlogger) {
+    if (frontendlogger && frontendlogger.event) {
         frontendlogger.event(eventNavn, feltObjekt || {}, tagObjekt || {});
     } else {
         // eslint-disable-next-line
