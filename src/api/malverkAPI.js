6402208d13f62e2ad4d62159c165c3de38cf2b86
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-03-12 08:12:23 +0100

    Skrive om api-filer til ts

--- src/api/malverkAPI.js
+++
@@ -1,6 +0,0 @@
-import { MALVERK_BASE_URL } from '../environment';
-import { postAsJson } from './utils';
-
-export function hentMalverkMedType(type) {
-    return postAsJson(`${MALVERK_BASE_URL}/mal`, { type });
-}
--- src/api/timeoutboxAPI.ts
+++ src/api/timeoutboxAPI.ts
@@ -1,6 +1,4 @@
 import { fetchToJson } from './utils';
 
 // eslint-disable-next-line import/prefer-default-export
-export function hentGjenstaendeInnloggetTid() {
-    return fetchToJson('/api/auth');
-}
+export const hentGjenstaendeInnloggetTid = () => fetchToJson('/api/auth');
