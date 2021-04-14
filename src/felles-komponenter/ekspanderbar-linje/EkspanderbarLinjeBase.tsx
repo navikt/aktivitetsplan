af23314f439620b82cad9c2bd8d37c55ce1afd77
hakon planke holm <hakon.planke.holm@nav.no>
2021-04-14 13:58:25 +0200

    rydde opp i ekspanderbar lingje

---
+++ src/felles-komponenter/ekspanderbar-linje/EkspanderbarLinjeBase.tsx
@@ -0,0 +1,59 @@
+import { Normaltekst } from 'nav-frontend-typografi';
+import React, { useEffect, useRef } from 'react';
+import { UnmountClosed } from 'react-collapse';
+
+import VisibleIfDiv from '../utils/visible-if-div';
+import styles from './EkspanderbarLinjeBase.module.less';
+
+export interface PropsBase {
+    tittel: string | React.ReactNode;
+    children: React.ReactNode;
+    kanToogle: boolean;
+    aapneTekst: string;
+    lukkeTekst: string;
+    erAapen: boolean;
+    onClick(): void;
+}
+
+const moveElementIntoView = (element: HTMLDivElement | null) => {
+    // moves the element into view after the react-collapse animation is finished
+    setTimeout(() => {
+        element?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }, 400);
+};
+
+const EkspanderbarLinjeBase = (props: PropsBase) => {
+    const { tittel, children, kanToogle, aapneTekst, lukkeTekst, onClick, erAapen } = props;
+
+    const aapenRef = useRef(erAapen);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (erAapen && !aapenRef.current) {
+            moveElementIntoView(contentRef.current);
+        }
+        aapenRef.current = erAapen;
+    }, [erAapen, aapenRef.current, contentRef.current]);
+
+    const tittelKomponent = typeof tittel === 'string' ? <Normaltekst>{tittel}</Normaltekst> : tittel;
+
+    return (
+        <section>
+            <button onClick={onClick} className={styles.endreContainer} aria-expanded={erAapen} disabled={!kanToogle}>
+                <div className={styles.endreVisning}>{tittelKomponent}</div>
+                <VisibleIfDiv visible={kanToogle} className={styles.endreKnapp}>
+                    <div className={styles.endreKnappInnhold}>{erAapen ? lukkeTekst : aapneTekst}</div>
+                    <div className={erAapen ? styles.endreIndikasjonLukket : styles.endreIndikasjonApen} />
+                </VisibleIfDiv>
+            </button>
+
+            <UnmountClosed isOpened={erAapen}>
+                <div ref={contentRef} className={styles.endreForm}>
+                    {children}
+                </div>
+            </UnmountClosed>
+        </section>
+    );
+};
+
+export default EkspanderbarLinjeBase;
--- src/moduler/aktivitet/visning/hjelpekomponenter/forhaandsorientering/Forhaandsorienteringsvisning.tsx
+++ src/moduler/aktivitet/visning/hjelpekomponenter/forhaandsorientering/Forhaandsorienteringsvisning.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Forhaandsorientering } from '../../../../../datatypes/aktivitetTypes';
-import EkspanderbarLinje from '../../../../../felles-komponenter/ekspanderbar-linje/EkspanderbarLinje';
+import EkspanderbarLinjeBase from '../../../../../felles-komponenter/ekspanderbar-linje/EkspanderbarLinjeBase';
 import { selectErBruker } from '../../../../identitet/identitet-selector';
 import styles from './Forhaandsorienteringsvisning.module.less';
 import LestDatoForhaandsorientering from './LestDatoForhaandsorientering';
@@ -18,16 +18,14 @@ interface Props {
 
 const Forhaandsorienteringsvisning = (props: Props) => {
     const { forhaandsorientering, markerSomLest, forhaandsorienteringLagtTil = false } = props;
+    const forhaandsorienteringTekst = forhaandsorientering?.tekst;
+    const forhaandsorienteringLestDato = forhaandsorientering?.lest;
+    const erLest = !!forhaandsorienteringLestDato;
 
     const erBruker = useSelector(selectErBruker);
 
-    const forhaandsorienteringTekst = forhaandsorientering?.tekst;
-    const forhaandsorienteringLestDato = forhaandsorientering?.lest;
-
-    const erLest = !!forhaandsorienteringLestDato;
-
     const ekspandertDefault = !erBruker ? forhaandsorienteringLagtTil : !erLest;
-    const [ekspandert, setEkspandert] = useState(ekspandertDefault);
+    const [erEkspandert, setErEkspandert] = useState(ekspandertDefault);
 
     if (!forhaandsorienteringTekst) {
         return null;
@@ -35,7 +33,7 @@ const Forhaandsorienteringsvisning = (props: Props) => {
 
     const onClickLestKnapp = () => {
         markerSomLest && markerSomLest();
-        setEkspandert(false);
+        setErEkspandert(false);
     };
 
     const advarselTittel = (
@@ -47,12 +45,21 @@ const Forhaandsorienteringsvisning = (props: Props) => {
 
     const tittel = erLest || !erBruker ? normalTittel : advarselTittel;
 
+    const onClick = () => setErEkspandert((ekspandert) => !ekspandert);
+
     return (
-        <EkspanderbarLinje tittel={tittel} kanToogle aapneTekst="Les" lukkeTekst="Lukk" defaultAapen={ekspandert}>
+        <EkspanderbarLinjeBase
+            tittel={tittel}
+            kanToogle
+            aapneTekst="Les"
+            lukkeTekst="Lukk"
+            erAapen={erEkspandert}
+            onClick={onClick}
+        >
             <Normaltekst className={styles.forhaandsorienteringTekst}>{forhaandsorienteringTekst}</Normaltekst>
             <LestDatoForhaandsorientering hidden={!erLest} lestDato={forhaandsorienteringLestDato} />
             <LestKnappForhaandsorientering hidden={!erBruker || erLest} onClick={onClickLestKnapp} />
-        </EkspanderbarLinje>
+        </EkspanderbarLinjeBase>
     );
 };
 
