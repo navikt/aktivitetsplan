182ec468f2f28b470891e9d287464ea48d30bc29
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-04-28 09:35:45 +0200

    Markere fho som ulest på aktivitetskortet (#757)

---
+++ src/moduler/aktivitet/ulest-markering/UlestMarkering.tsx
@@ -0,0 +1,25 @@
+import { Element } from 'nav-frontend-typografi';
+import React from 'react';
+
+import { ReactComponent as UlestViktigIkon } from './ikon_ulest_viktig.svg';
+import styles from './UlestMarkering.module.less';
+
+interface Props {
+    hidden: boolean;
+}
+const UlestMarkering = (props: Props) => {
+    const { hidden } = props;
+
+    if (hidden) {
+        return null;
+    }
+
+    return (
+        <div className={styles.ulestMarkering}>
+            <UlestViktigIkon />
+            <Element className={styles.ulestTekst}>Ulest</Element>
+        </div>
+    );
+};
+
+export default UlestMarkering;
