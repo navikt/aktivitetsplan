99780230174a108fd849caa08dcbcda2ab7abe72
izisfro <izis.froyhov@nav.no>
2021-06-18 09:51:51 +0200

    Flytter mappere med duplisert kode ut i egen fil

---
+++ src/utils/textMappers.ts
@@ -0,0 +1,21 @@
+export const aktivitetTypeMap = {
+    EGEN: 'Jobbrettet egenaktivitet',
+    STILLING: 'Stilling',
+    TILTAKSAKTIVITET: 'Tiltak gjennom NAV',
+    GRUPPEAKTIVITET: 'Gruppeaktivitet',
+    UTDANNINGSAKTIVITET: 'Utdanning',
+    SOKEAVTALE: 'Jobbsøking',
+    IJOBB: 'Jobb jeg har nå',
+    BEHANDLING: 'Behandling',
+    MOTE: 'Møte med NAV',
+    SAMTALEREFERAT: 'Samtalereferat',
+    REKRUTTERINGSBISTAND: 'Stilling fra nav',
+};
+
+export const aktivitetStatusMap = {
+    PLANLAGT: 'Planlegger',
+    BRUKER_ER_INTERESSERT: 'Forslag',
+    GJENNOMFORES: 'Gjennomfører',
+    FULLFORT: 'Fullført',
+    AVBRUTT: 'Avbrutt',
+};
