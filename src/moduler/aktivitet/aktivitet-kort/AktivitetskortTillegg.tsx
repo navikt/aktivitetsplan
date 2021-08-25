85b26a15b09ef8ba0a32cbf5494a0a64f526678a
Marc <Marc.Joseph.Ablian.Saputil@nav.no>
2021-08-25 16:50:49 +0200

    Endret tekst & vis info om at samtalereferat ikke er ferdig med bruker (#834)
    * endre vis/skjul til åpne/lukk
    
    * eksternbruker ser at samtalereferat ikke er ferdig
    
    * Refaktorerte ikke delt/ferdig-markering

--- src/moduler/aktivitet/aktivitet-kort/AktivitetskortTillegg.tsx
+++ src/moduler/aktivitet/aktivitet-kort/AktivitetskortTillegg.tsx
@@ -6,7 +6,7 @@ import { Henvendelse } from '../../../datatypes/dialogTypes';
 import { div as HiddenIfDiv } from '../../../felles-komponenter/hidden-if/hidden-if';
 import { selectDialogForAktivitetId } from '../../dialog/dialog-selector';
 import SokeStatusEtikett from '../etikett/SokeStatusEtikett';
-import IkkeDeltMarkering, { SkalIkkeDeltMarkeringVises } from '../ikke-delt-markering/IkkeDeltMarkering';
+import IkkeDeltFerdigMarkering, { skalMarkeringVises } from '../ikke-delt-ferdig-markering/IkkeDeltFerdigMarkering';
 import DialogIkon from '../visning/underelement-for-aktivitet/dialog/DialogIkon';
 import styles from './Aktivitetskort.module.less';
 import UlestAvtaltMarkering from './UlestAvtaltMarkering';
@@ -20,9 +20,9 @@ const AktivitetskortTillegg = ({ aktivitet }: Props) => {
     const dialog = useSelector((state) => selectDialogForAktivitetId(state, id), shallowEqual);
     const henvendelser = dialog ? dialog.henvendelser : [];
     const ulesteHenvendelser = henvendelser.filter((h: Henvendelse) => !h.lest).length;
-    const ikkeDelt = SkalIkkeDeltMarkeringVises(aktivitet);
+    const markeringSkalVises = skalMarkeringVises(aktivitet);
 
-    if (!(avtalt || !!etikett || !!dialog || ikkeDelt)) {
+    if (!(avtalt || !!etikett || !!dialog || markeringSkalVises)) {
         return null;
     }
 
@@ -30,7 +30,7 @@ const AktivitetskortTillegg = ({ aktivitet }: Props) => {
         <div className={styles.tillegg}>
             <div>
                 <UlestAvtaltMarkering aktivitet={aktivitet} />
-                <IkkeDeltMarkering visible={ikkeDelt} />
+                <IkkeDeltFerdigMarkering visible={markeringSkalVises} />
                 <SokeStatusEtikett hidden={!etikett} etikett={etikett} className={styles.etikett} />
             </div>
 
