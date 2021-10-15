38b9acff7efb2422380884e4164939be42d4bf75
Marc <Marc.Joseph.Ablian.Saputil@nav.no>
2021-10-15 10:23:37 +0200

    https://trello.com/c/kUGGlfJo (#855)

--- src/moduler/aktivitet/visning/dele-cv/tekster.ts
+++ src/moduler/aktivitet/visning/dele-cv/tekster.ts
@@ -1,2 +1,3 @@
 export const NeiSvarTekst = 'Nei, og jeg vil ikke at NAV skal dele CV-en min med arbeidsgiveren';
 export const JaSvarTekst = 'Ja, og NAV kan dele CV-en min med arbeidsgiveren';
+export const overskrift = 'Er du interessert i denne stillingen?';
