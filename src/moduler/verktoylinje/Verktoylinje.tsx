e976a052eb1d91d62a99ed07e65b2a6cf2d20fb0
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-11-03 10:56:25 +0100

    Skrive om vertoylinje til tsx

---
+++ src/moduler/verktoylinje/Verktoylinje.tsx
@@ -0,0 +1,62 @@
+import { Add } from '@navikt/ds-icons';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import InternLenke from '../../felles-komponenter/utils/InternLenke';
+import Lenkeknapp from '../../felles-komponenter/utils/Lenkeknapp';
+import loggEvent, { APNE_NY_AKTIVITET, APNE_OM_TJENESTEN } from '../../felles-komponenter/utils/logging';
+import { selectHarTilgangTilAktiviteter } from '../aktivitet/aktivitet-selector';
+import Filter from '../filtrering/filter';
+import VisValgtFilter from '../filtrering/filter-vis-label';
+import { selectViserHistoriskPeriode } from '../filtrering/filter/filter-selector';
+import PeriodeFilter from '../filtrering/filter/periode-filter';
+import { selectErUnderOppfolging, selectHarSkriveTilgang } from '../oppfolging-status/oppfolging-selector';
+
+const Verktoylinje = () => {
+    const underOppfolging = useSelector(selectErUnderOppfolging);
+    const viserHistoriskPeriode = useSelector(selectViserHistoriskPeriode);
+    const harSkriveTilgang = useSelector(selectHarSkriveTilgang);
+    const aktivitetLaster = useSelector(selectHarTilgangTilAktiviteter);
+
+    return (
+        <div className="verktoylinje">
+            <div className="verktoylinje__verktoy-container">
+                <div />
+                <Lenkeknapp
+                    type="hoved"
+                    href="/aktivitet/ny"
+                    className="ny-aktivitet-lenke"
+                    disabled={!aktivitetLaster}
+                    visible={!viserHistoriskPeriode && underOppfolging && harSkriveTilgang}
+                    onClick={() => loggEvent(APNE_NY_AKTIVITET)}
+                >
+                    <Add aria-label="Legg til ikon" role="img" focusable="false" />
+                    <span> Legg til aktivitet</span>
+                </Lenkeknapp>
+            </div>
+            <div className="verktoylinje__verktoy-container">
+                <div className="indre">
+                    <InternLenke
+                        href="/informasjon"
+                        className="knappelenke"
+                        onClick={() => loggEvent(APNE_OM_TJENESTEN)}
+                    >
+                        <span>Om aktivitetsplanen</span>
+                    </InternLenke>
+                    <InternLenke href="/utskrift" className="knappelenke utskrift-lenke">
+                        <span>Skriv ut</span>
+                    </InternLenke>
+                    <Filter className="verktoylinje__verktoy" />
+                </div>
+            </div>
+            <div className="verktoylinje__verktoy-container">
+                <VisValgtFilter />
+            </div>
+            <div className="verktoylinje__verktoy-container">
+                <PeriodeFilter className="verktoylinje__verktoy" skjulInneverende={!underOppfolging} />
+            </div>
+        </div>
+    );
+};
+
+export default Verktoylinje;
