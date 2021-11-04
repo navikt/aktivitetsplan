f7821157341ecc7985a7cbf2c332b619459a567a
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-11-04 10:46:56 +0100

    Legge til nytt dialogikon

--- src/moduler/aktivitet/visning/underelement-for-aktivitet/dialog/DialogIkon.tsx
+++ src/moduler/aktivitet/visning/underelement-for-aktivitet/dialog/DialogIkon.tsx
@@ -8,11 +8,22 @@ function DialogIkonUtenUleste(props: DialogIkonUtenUlesteProps) {
     const { className } = props;
 
     return (
-        <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" focusable="false">
-            <g stroke="#0067C5" fill="none">
-                <path d="M7.44 14.56A9.01 9.01 0 016 14.25l-5 2 2-3.5A6.53 6.53 0 01.5 7.69C.5 3.86 4.3.75 9 .75s8.5 3.1 8.5 6.94" />
-                <path d="M9.5 16.25c0 4.29 5 7.5 9.5 5.5l4 1.5-1.5-3a5 5 0 002-4c0-3.15-3.13-6-7-6s-7 2.85-7 6z" />
-            </g>
+        <svg
+            className={className}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            focusable="false"
+            role="img"
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M2 13.518L4.602 12H6v-1a4 4 0 014-4h6V4a2 2 0 00-2-2H4a2 2 0 00-2 2v9.518zM5.143 14H6v3a4 4 0 004 4h8.857L24 24V11a4 4 0 00-4-4h-2V4a4 4 0 00-4-4H4a4 4 0 00-4 4v13l5.143-3zm14.255 5L22 20.518V11a2 2 0 00-2-2H10a2 2 0 00-2 2v6a2 2 0 002 2h9.398zM11 15a1 1 0 100-2 1 1 0 000 2zm5-1a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2z"
+                fill="#0067C5"
+            />
         </svg>
     );
 }
@@ -28,15 +39,20 @@ function DialogIkonMedUleste(props: DialogIkonMedUlesteProps) {
 
     return (
         <div>
-            <svg className={className} xmlns="http://www.w3.org/2000/svg" width="40" height="32">
+            <svg
+                className={className}
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="32"
+                focusable="false"
+                role="img"
+            >
                 <svg y="8">
                     <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 13.518L4.602 12H6v-1a4 4 0 014-4h6V4a2 2 0 00-2-2H4a2 2 0 00-2 2v9.518zM5.143 14H6v3a4 4 0 004 4h8.857L24 24V11a4 4 0 00-4-4h-2V4a4 4 0 00-4-4H4a4 4 0 00-4 4v13l5.143-3zm14.255 5L22 20.518V11a2 2 0 00-2-2H10a2 2 0 00-2 2v6a2 2 0 002 2h9.398zM11 15a1 1 0 100-2 1 1 0 000 2zm5-1a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2z"
                         fill="#0067C5"
-                        d="M16.5 8.75c.48 0 .94.04 1.4.12.06-.38.1-.78.1-1.18 0-4.1-4.04-7.44-9-7.44S0 3.58 0 7.68c0 1.97.85 3.83 2.36 5.18L.56 16a.5.5 0 00.63.71l4.84-1.93c.43.12.88.22 1.34.28h.08l.68-.02c.67-3.51 4.23-6.29 8.37-6.29z"
-                    />
-                    <path
-                        fill="#0067C5"
-                        d="M16.5 9.75c-4.07 0-7.5 2.98-7.5 6.5 0 3.35 2.88 6.22 6.43 6.57h.01c1.3.13 2.57-.11 3.57-.53l3.81 1.43a.5.5 0 00.63-.7l-1.33-2.64A5.46 5.46 0 0024 16.25c0-3.52-3.43-6.5-7.5-6.5z"
                     />
                 </svg>
                 <circle opacity="1" fill="#c30000" cx="30" cy="10" r="10" />
