824d07228f8338a9a3bb8295e1317a6afc31f38c
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-11-10 13:14:51 +0100

    Oppdatere til nye ikoner (#863)

---
+++ src/moduler/filtrering/filteringslabel/FiltreringLabel.tsx
@@ -0,0 +1,16 @@
+import { Close } from '@navikt/ds-icons';
+import React from 'react';
+
+interface Props {
+    label: string;
+    slettFilter(): void;
+}
+
+const FiltreringLabel = ({ label, slettFilter }: Props) => (
+    <button type="button" aria-label="slett-filter" className="filtreringlabel typo-undertekst" onClick={slettFilter}>
+        <span className="filtreringlabel__label">{label}</span>
+        <Close className="filter-ikon-x-svg" aria-describedby="Fjern filter" role="img" focusable="false" />
+    </button>
+);
+
+export default FiltreringLabel;
