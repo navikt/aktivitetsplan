a64d814b93c618fbc5e2b4878c2b0c5dd9dd5356
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-11-26 00:23:57 -0800

    Svar på vegne av bruker dato (#875)
    Kan sette dato for når veileder snakket med bruker tidligst syv dager før opprettelse av kortet

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,3 +1,14 @@
-import { endOfToday, formatISO } from 'date-fns';
+import { endOfToday, formatISO, subDays } from 'date-fns';
+import { dateToISODateString } from 'nav-datovelger/lib/utils/dateFormatUtils';
+
+import { toDate } from '../utils';
 
 export const todayIsoString = () => formatISO(endOfToday());
+
+export const dagerSiden = (dato: string, antallDager: number) => {
+    let datoDate = toDate(dato);
+    if (!datoDate) return null;
+
+    const prevDate = subDays(datoDate, antallDager);
+    return dateToISODateString(prevDate);
+};
