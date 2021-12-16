374a165d28c3ea5d7828a2d0bd1774b2f4df6601
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-12-16 10:52:42 +0100

    Få i sync med dev

--- src/moduler/aktivitet/visning/detaljer/StillingFraNavDetaljer.tsx
+++ src/moduler/aktivitet/visning/detaljer/StillingFraNavDetaljer.tsx
@@ -7,19 +7,20 @@ import { loggStillingFraNavStillingslenkeKlikk } from '../../../../felles-kompon
 import { selectErVeileder } from '../../../identitet/identitet-selector';
 import Informasjonsfelt from '../hjelpekomponenter/Informasjonsfelt';
 import styles from './AktivitetDetaljer.module.less';
-import { KontaktInfoDetaljer } from './KontaktInfoDetaljer';
 
 type Props = {
     stillingFraNavData: StillingFraNavAktivitetData;
 };
-
+const deafultBasePath = 'https://www.nav.no/arbeid/stilling/';
 /* eslint-disable react/jsx-no-target-blank */
 export const StillingFraNavDetaljer = ({ stillingFraNavData }: Props) => {
     const erVeileder = useSelector(selectErVeileder);
 
     if (!stillingFraNavData) return null;
+    // @ts-ignore
+    const envBasePath = window?.aktivitetsplan?.STILLING_FRA_NAV_BASE_URL;
+    const basePath = envBasePath ? envBasePath : deafultBasePath;
 
-    const basePath = erVeileder ? '/rekrutteringsbistand/' : '/stillingFraNav/';
     const url = basePath + stillingFraNavData.stillingsId;
 
     return (
@@ -27,7 +28,6 @@ export const StillingFraNavDetaljer = ({ stillingFraNavData }: Props) => {
             <div className={styles.detaljer}>
                 <Informasjonsfelt key="arbeidsgiver" tittel="arbeidsgiver" innhold={stillingFraNavData.arbeidsgiver} />
                 <Informasjonsfelt key="arbeidssted" tittel="arbeidssted" innhold={stillingFraNavData.arbeidssted} />
-                <KontaktInfoDetaljer kontaktInfo={stillingFraNavData.kontaktpersonData} />
             </div>
             <a
                 href={url}
