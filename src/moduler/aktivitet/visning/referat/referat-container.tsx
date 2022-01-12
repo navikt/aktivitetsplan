89a1c78cef7d28d6dd85d87c3334077b741766ad
Marc <Marc.Joseph.Ablian.Saputil@nav.no>
2022-01-12 16:03:59 +0100

    fix avbrutte møter uten samtalereferat (#880)
    * bugfix for avbrutte møter uten samtalereferat

---
+++ src/moduler/aktivitet/visning/referat/referat-container.tsx
@@ -0,0 +1,74 @@
+import moment from 'moment';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { STATUS } from '../../../../api/utils';
+import { MOTE_TYPE, SAMTALEREFERAT_TYPE, STATUS_AVBRUTT, STATUS_FULLFOERT } from '../../../../constant';
+import { Aktivitet } from '../../../../datatypes/aktivitetTypes';
+import { selectErVeileder } from '../../../identitet/identitet-selector';
+import { selectUnderOppfolging } from '../../../oppfolging-status/oppfolging-selector';
+import { publiserReferat } from '../../aktivitet-actions';
+import { selectAktivitetStatus } from '../../aktivitet-selector';
+import DeleLinje from '../delelinje/delelinje';
+import OppdaterReferatForm from './oppdater-referat-form';
+import ReferatVisning from './referat-visning';
+
+interface Props {
+    aktivitet: Aktivitet;
+}
+
+const ReferatContainer = (props: Props) => {
+    const { aktivitet } = props;
+
+    const dispatch = useDispatch();
+    const [isOppdaterReferat, setOppdaterReferat] = useState(false);
+
+    const publiserer = useSelector(selectAktivitetStatus) === (STATUS.PENDING || STATUS.RELOADING);
+    const erVeileder = useSelector(selectErVeileder);
+    const underOppfolging = useSelector(selectUnderOppfolging);
+
+    const { referat, erReferatPublisert, type: aktivitetType } = aktivitet;
+
+    const kanHaReferat =
+        (aktivitetType === MOTE_TYPE && moment(aktivitet.fraDato).toISOString() < moment().toISOString()) ||
+        aktivitetType === SAMTALEREFERAT_TYPE;
+
+    const erAktivAktivitet =
+        !aktivitet.historisk &&
+        underOppfolging &&
+        aktivitet.status !== STATUS_AVBRUTT &&
+        aktivitet.status !== STATUS_FULLFOERT;
+
+    if (!kanHaReferat) return null;
+
+    const manglerReferat = erVeileder && !referat && erAktivAktivitet;
+    if (manglerReferat || isOppdaterReferat) {
+        return (
+            <section>
+                <OppdaterReferatForm aktivitet={aktivitet} onFerdig={() => setOppdaterReferat(false)} />
+                <DeleLinje />
+            </section>
+        );
+    }
+
+    if (!!referat) {
+        return (
+            <section>
+                <ReferatVisning
+                    referat={referat}
+                    erAktivAktivitet={erAktivAktivitet}
+                    erVeileder={erVeileder}
+                    dispatchPubliserReferat={() => dispatch(publiserReferat(aktivitet))}
+                    publiserer={publiserer}
+                    erReferatPublisert={!!erReferatPublisert}
+                    startOppdaterReferat={() => setOppdaterReferat(true)}
+                />
+                <DeleLinje />
+            </section>
+        );
+    }
+
+    return null;
+};
+
+export default ReferatContainer;
