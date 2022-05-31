04a467e4235a81b902fb8afeac453e8e6f85e661
stianStensli <stian.horn.stensli@nav.no>
2022-05-31 17:28:36 +0200

    Sender fnr som query param, legger til en ny metode som ikke automatisk legger til fnr

--- src/api/personAPI.ts
+++ src/api/personAPI.ts
@@ -1,6 +1,6 @@
 import { PERSON_BASE_URL } from '../environment';
-import { fetchToJson } from './utils';
+import { fetchToJson, fetchToJsonPlain } from './utils';
 
-export const hentPerson = (fnr: string) => fetchToJson(`${PERSON_BASE_URL}/v2/person/${fnr}`);
+export const hentPerson = (fnr: string) => fetchToJsonPlain(`${PERSON_BASE_URL}/v2/person?fnr=${fnr}`);
 
 export const hentHarNivaa4 = (fnr: string) => fetchToJson(`${PERSON_BASE_URL}/person/${fnr}/harNivaa4`);
