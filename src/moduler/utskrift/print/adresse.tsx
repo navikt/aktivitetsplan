7a57468694188c1def2eb31f84bded2201021a77
stianStensli <stian.horn.stensli@nav.no>
2022-06-07 16:03:53 +0200

    poststed og postnummer kan være null

--- src/moduler/utskrift/print/adresse.tsx
+++ src/moduler/utskrift/print/adresse.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import { Postadresse } from '../../../datatypes/types';
 import StoreForbokstaver from '../../../felles-komponenter/utils/StoreForbokstaver';
 
-interface Props {
+interface AdresseProps {
     adresse: Postadresse;
 }
 
-function Adresse({ adresse }: Props) {
+function Adresse({ adresse }: AdresseProps) {
     const adresselinje1 = adresse.adresselinje1;
-    const postnummer = adresse.postnummer;
-    const poststed = adresse.poststed;
 
     if (!adresselinje1) {
         return <div />;
@@ -20,8 +18,9 @@ function Adresse({ adresse }: Props) {
             <StoreForbokstaver tag="div">{`${adresselinje1}`}</StoreForbokstaver>
             {adresse.adresselinje2 && <StoreForbokstaver tag="div">{`${adresse.adresselinje2}`}</StoreForbokstaver>}
             {adresse.adresselinje3 && <StoreForbokstaver tag="div">{`${adresse.adresselinje3}`}</StoreForbokstaver>}
-            <StoreForbokstaver tag="div">{`${postnummer} ${poststed}`}</StoreForbokstaver>
-
+            {adresse.postnummer && adresse.poststed && (
+                <StoreForbokstaver tag="div">{`${adresse.postnummer} ${adresse.poststed}`}</StoreForbokstaver>
+            )}
             {adresse.land && adresse.type !== 'NORSKPOSTADRESSE' && (
                 <StoreForbokstaver tag="div">{`${adresse.land}`}</StoreForbokstaver>
             )}
