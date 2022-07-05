b2e8283c87898cf05c3fe371bc8c7761ed063040
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-07-05 11:27:58 +0200

    Unngår å prototype på Date

--- src/mocks/fixtures/stillingFraNavFixtures.js
+++ src/mocks/fixtures/stillingFraNavFixtures.js
@@ -1,10 +1,6 @@
 let id = 1000;
 let versjon = 5000000;
-Date.prototype.addYears = function (years) {
-    let date = new Date(this.valueOf());
-    date.setFullYear(date.getFullYear() + years);
-    return date;
-};
+
 export const etTidspunkt = (arstall) => (arstall ? arstall + '-01-31T10:46:25.801+01:00' : null);
 
 export const oddFellow = {
@@ -23,23 +19,30 @@ export const navAnsatt2 = {
     mobil: null,
 };
 
-export const enStillingFraNavAktivitet = ({ tittel, arstall }) => ({
-    versjon: (versjon += 1000),
-    id: (id += 1000),
-    tittel,
-    type: 'STILLING_FRA_NAV',
-    lenke: null,
-    status: 'GJENNOMFORES',
-    fraDato: etTidspunkt(arstall),
-    opprettetDato: etTidspunkt(arstall),
-    endretDato: arstall ? etTidspunkt(new Date(etTidspunkt(arstall)).addYears(2).toISOString()) : null,
-    endretAv: 'z990207',
-    historisk: true,
-    kontaktperson: 'Vidar Vidarsen,\n NAV-ansatt, 99 99 99 99,vidar.vidarsen@nav.no',
-    lagtInnAv: 'NAV',
-    transaksjonsType: 'STATUS_ENDRET',
-    stillingFraNavData: { ...enStillingFraNavData, soknadsstatus: null },
-});
+export const enStillingFraNavAktivitet = ({ tittel, arstall }) => {
+    const date = new Date(etTidspunkt(arstall));
+
+    let endretDate = new Date(etTidspunkt(arstall));
+    endretDate.setFullYear(endretDate.getFullYear() + 2);
+
+    return {
+        versjon: (versjon += 1000),
+        id: (id += 1000),
+        tittel,
+        type: 'STILLING_FRA_NAV',
+        lenke: null,
+        status: 'GJENNOMFORES',
+        fraDato: date,
+        opprettetDato: date,
+        endretDato: arstall ? endretDate : null,
+        endretAv: 'z990207',
+        historisk: true,
+        kontaktperson: 'Vidar Vidarsen,\n NAV-ansatt, 99 99 99 99,vidar.vidarsen@nav.no',
+        lagtInnAv: 'NAV',
+        transaksjonsType: 'STATUS_ENDRET',
+        stillingFraNavData: { ...enStillingFraNavData, soknadsstatus: null },
+    };
+};
 export const jaCvKanDeles = {
     kanDeles: true,
     endretTidspunkt: new Date(),
