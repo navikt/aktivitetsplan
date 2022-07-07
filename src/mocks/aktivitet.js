1ac65c0c383cd626ae5db470a4cca5db1fe818c0
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-07-07 17:00:51 +0200

    Forkorter lenke i samtalereferat

--- src/mocks/aktivitet.js
+++ src/mocks/aktivitet.js
@@ -176,10 +176,10 @@ const testAktiviteter = !visTestAktiviteter()
               forhaandsorientering: enLestForhaandsorientering,
           }),
           wrapAktivitet({
-              ...etSamtalereferat({ tittel: 'Inneholder et langt referat' }),
+              ...etSamtalereferat({ tittel: 'Inneholder et langt referat med lenke' }),
               erReferatPublisert: false,
               referat:
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Se også https://www.nav.no/',
           }),
           wrapAktivitet({
               ...etSamtalereferat({ tittel: 'Et rykte sprer seg raskt som en brann' }),
