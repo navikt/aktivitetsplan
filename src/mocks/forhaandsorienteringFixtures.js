81adb63d68d8b72aa5c08568eb2a4fc94c998d98
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-07-07 15:28:51 +0200

    Legger til aktiviteter som har lenker i beskrivelse og referat i testdata

---
+++ src/mocks/forhaandsorienteringFixtures.js
@@ -0,0 +1,11 @@
+export const enUlestForhaandsorientering = {
+    tekst: 'Det er viktig at du gjennomfører denne aktiviteten med NAV. Gjør du ikke det, kan det medføre at stønaden du mottar fra NAV bortfaller for en periode eller stanses. Hvis du ikke kan gjennomføre aktiviteten, ber vi deg ta kontakt med veilederen din så snart som mulig.',
+    type: 'SEND_FORHAANDSORIENTERING',
+    lestDato: null,
+};
+
+export const enLestForhaandsorientering = {
+    tekst: 'Det er viktig at du gjennomfører denne aktiviteten med NAV. Gjør du ikke det, kan det medføre at stønaden du mottar fra NAV bortfaller for en periode eller stanses. Hvis du ikke kan gjennomføre aktiviteten, ber vi deg ta kontakt med veilederen din så snart som mulig.',
+    type: 'SEND_FORHAANDSORIENTERING',
+    lestDato: '2021-05-30T10:46:40.459+00:00',
+};
