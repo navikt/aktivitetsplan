4e4a59b10d12245994c945bb183ff3cdbbb33c9f
sigurdgroneng <sigurdgroneng@nav.no>
2022-09-06 13:41:20 +0200

    Import css from each package, remove craco-less
    Co-authored-by: marc <marc.joseph.ablian.saputil@nav.no>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import 'moment-timezone';
 import 'moment/locale/nb';
+import 'nav-frontend-core/dist/main.css';
+import 'nav-frontend-alertstriper-style/dist/main.css';
+import 'nav-frontend-chevron-style/dist/main.css';
+import 'nav-frontend-ekspanderbartpanel-style/dist/main.css';
+import 'nav-frontend-hjelpetekst-style/dist/main.css';
+import 'nav-frontend-knapper-style/dist/main.css';
+import 'nav-frontend-lenkepanel-style/dist/main.css';
+import 'nav-frontend-lenker-style/dist/main.css';
+import 'nav-frontend-lesmerpanel-style/dist/main.css';
+import 'nav-frontend-lukknapp-style/dist/main.css';
+import 'nav-frontend-modal-style/dist/main.css';
+import 'nav-frontend-paneler-style/dist/main.css';
+import 'nav-frontend-popover-style/dist/main.css';
+import 'nav-frontend-skjema-style/dist/main.css';
+import 'nav-frontend-snakkeboble-style/dist/main.css';
+import 'nav-frontend-spinner-style/dist/main.css';
+import 'nav-frontend-typografi-style/dist/main.css';
+import 'nav-frontend-veileder-style/dist/main.css';
+import 'nav-frontend-veilederpanel-style/dist/main.css';
+import 'nav-datovelger/lib/styles/main.css';
 
 import './polyfill';
 
