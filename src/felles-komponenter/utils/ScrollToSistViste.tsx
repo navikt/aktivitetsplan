1737aafbeb44c1b6c0dcafc04c092ddb7689560f
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-10-17 14:36:43 +0200

    Scroller til sist viste element når man routes

---
+++ src/felles-komponenter/utils/ScrollToSistViste.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement, useEffect } from 'react';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import { RouteComponentProps, RouteProps, withRouter } from 'react-router-dom';
+
+import { Aktivitet } from '../../datatypes/aktivitetTypes';
+
+const ScrollToSistViste = (props: RouteProps): ReactElement<RouteComponentProps<any>> => {
+    let sistVisteAktivitetId: string = useSelector<RootStateOrAny, string>(
+        (state) =>
+            `aktivitetskort-` +
+            state?.view?.visteAktiviteterMedEndringer?.reduce((a: Aktivitet | null, b: Aktivitet) => b, null)?.id
+    );
+
+    useEffect(() => {
+        document.getElementById(sistVisteAktivitetId)?.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+        });
+    }, [props.location, sistVisteAktivitetId]);
+
+    return <>{props.children}</>;
+};
+
+export default withRouter(ScrollToSistViste);
