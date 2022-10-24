e0af958e80ef3fdb9da7780ac8f455ad3f2923a3
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-10-24 12:49:55 +0200

    Endrer tekst og url fra dittnav til minside

--- src/moduler/verktoylinje/navigasjonslinje.tsx
+++ src/moduler/verktoylinje/navigasjonslinje.tsx
@@ -12,14 +12,15 @@ import { selectDialoger, selectSistOppdatert } from '../dialog/dialog-selector';
 import { selectErVeileder } from '../identitet/identitet-selector';
 import styles from './navigasjonslinje.module.less';
 
-const DITTNAV_PATH = '/dittnav/';
+const DITTNAV_PATH = '/minside/';
 const DIALOG_PATH = '/arbeidsrettet-dialog';
 
 function Navigasjonslinje() {
     const erVeileder = useSelector(selectErVeileder, shallowEqual);
     const sistOppdatert = useSelector(selectSistOppdatert, shallowEqual);
-    const antallUlesteDialoger: number = useSelector(selectDialoger, shallowEqual).filter((d: Dialog) => !d.lest)
-        .length;
+    const antallUlesteDialoger: number = useSelector(selectDialoger, shallowEqual).filter(
+        (d: Dialog) => !d.lest
+    ).length;
 
     const dispatch = useDispatch();
 
@@ -51,7 +52,7 @@ function Navigasjonslinje() {
         return (
             <div className={styles.navigasjonslinje}>
                 <LenkepanelBase className={styles.tilDittNav} href={DITTNAV_PATH}>
-                    <span className={styles.tilDittNavTekst}>Ditt NAV</span>
+                    <span className={styles.tilDittNavTekst}>Min side</span>
                 </LenkepanelBase>
                 <Undertittel className={styles.tittel} tag="h1">
                     Aktivitetsplan
