ca1379198c323c0fd9c55aa5adbe43ab97d4cb50
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-12-19 12:50:26 +0100

    Tar inn kun statusene som skal ha skjulte aktiviteter og gir kompileringsfeil dersom kolonner med andre aktivitetstatuser skjuler aktiviteter

--- src/hovedside/tavle/kolonne/KolonneSomSkjulerEldreAktiviteter.tsx
+++ src/hovedside/tavle/kolonne/KolonneSomSkjulerEldreAktiviteter.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 
-import { AktivitetStatus, AlleAktiviteter } from '../../../datatypes/aktivitetTypes';
+import { STATUS_AVBRUTT, STATUS_FULLFOERT } from '../../../constant';
+import { AlleAktiviteter } from '../../../datatypes/aktivitetTypes';
 import DragbartAktivitetskort from '../../../moduler/aktivitet/aktivitet-kort/DragbartAktivitetskort';
 import { sorterAktiviteter, splitIEldreOgNyereAktiviteter } from '../../../moduler/aktivitet/aktivitet-util';
 import { selectAktivitetListe } from '../../../moduler/aktivitet/aktivitetlisteSelector';
@@ -10,7 +11,16 @@ import KolonneHeader from './KolonneHeader';
 import SkjulEldreAktiviteterFraKolonne from './SkjulEldreAktiviteterFraKolonne';
 
 interface Props {
-    status: AktivitetStatus;
+    status: typeof STATUS_FULLFOERT | typeof STATUS_AVBRUTT;
+}
+
+function aktivitetTekst(status: typeof STATUS_FULLFOERT | typeof STATUS_AVBRUTT): string {
+    switch (status) {
+        case 'FULLFORT':
+            return 'eldre fullførte aktiviteter';
+        case 'AVBRUTT':
+            return 'eldre avbrutte aktiviteter';
+    }
 }
 
 function KolonneSomSkjulerEldreAktiviteter({ status }: Props) {
@@ -24,16 +34,13 @@ function KolonneSomSkjulerEldreAktiviteter({ status }: Props) {
         <DragbartAktivitetskort key={aktivitet.id} aktivitet={aktivitet} />
     ));
 
-    const aktivitetTekst =
-        status === 'FULLFORT' ? 'eldre fullførte aktiviteter' : /* status === "AVBRUTT" */ 'eldre avbrutte aktiviteter';
-
     return (
         <DropTargetKolonne status={status}>
             <KolonneHeader status={status} />
             <div>
                 {aktivitetsListe}
                 <SkjulEldreAktiviteterFraKolonne
-                    aktivitetTekst={aktivitetTekst}
+                    aktivitetTekst={aktivitetTekst(status)}
                     aktiviteteterTilDatoMerEnnToManederSiden={eldreAktiviteter}
                 />
             </div>
