340d4924f41ae0f11c010fa641565b04120a75ec
sigurdgroneng <sigurd.groneng@nav.no>
2023-02-24 11:13:38 +0100

    Try to pre bundle in dev

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import svgr from 'vite-plugin-svgr';
 
 export default {
     plugins: [svgr()],
+    optimizeDeps: {
+        entries: [
+            'src/api/**/*.{jsx,tsx,js,ts}',
+            'src/datatypes/**/*.{jsx,tsx,js,ts}',
+            'src/felles-komponenter/**/*.{jsx,tsx,js,ts}',
+            'src/hocs/**/*.{jsx,tsx,js,ts}',
+            'src/hovedside/**/*.{jsx,tsx,js,ts}',
+            'src/ikoner/**/*.{jsx,tsx,js,ts}',
+            'src/mocks/**/*.{jsx,tsx,js,ts}',
+            'src/moduler/**/*.{jsx,tsx,js,ts}',
+            'src/utils/**/*.{jsx,tsx,js,ts}',
+        ],
+        include: ['react'],
+    },
 };
