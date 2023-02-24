8bdd25475f6c218126210df4b7916201f883fe5c
sigurdgroneng <sigurd.groneng@nav.no>
2023-02-24 11:21:55 +0100

    Fix error icon

--- src/felles-komponenter/modal/ModalHeader.tsx
+++ src/felles-komponenter/modal/ModalHeader.tsx
@@ -1,9 +1,8 @@
+import { Locked } from '@navikt/ds-icons';
 import { BodyShort, Heading } from '@navikt/ds-react';
 import classNames from 'classnames';
 import React from 'react';
 
-import VisibleIfSpan from '../utils/visible-if-span';
-import HengeLasIkon from './HengelaasIkon';
 import Tilbakeknapp from './Tilbakeknapp';
 
 interface Props {
@@ -18,13 +17,15 @@ const ModalHeader = (props: Props) => {
     const { tilbakeTekst, headerTekst, className, onTilbakeClick, aktivitetErLaast = false, ...rest } = props;
 
     return (
-        <div className={classNames('modal-header-wrapper', className)} {...rest}>
-            <span aria-live="assertive" className="sr-only">
-                {aktivitetErLaast ? 'Denne aktiviteten kan ikke redigeres.' : ''}
-            </span>
-            <VisibleIfSpan className="modal-header-skillestrek" visible={aktivitetErLaast}>
-                <HengeLasIkon />
-            </VisibleIfSpan>
+        <div className={classNames('flex space-x-2', className)} {...rest}>
+            {aktivitetErLaast ? (
+                <div>
+                    <Locked />
+                    <span aria-live="assertive" className="sr-only">
+                        {aktivitetErLaast ? 'Denne aktiviteten kan ikke redigeres.' : ''}
+                    </span>
+                </div>
+            ) : null}
             {tilbakeTekst && onTilbakeClick && <Tilbakeknapp tekst={tilbakeTekst} onClick={onTilbakeClick} />}
             {headerTekst && <BodyShort>{headerTekst}</BodyShort>}
         </div>
