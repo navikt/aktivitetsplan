06884163ed8d59e944aad49d1fb91f5563f25aa5
sigurdgroneng <sigurd.groneng@nav.no>
2023-02-28 10:13:55 +0100

    js -> ts

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,6 @@ export const DIALOG_BASE_URL = '/veilarbdialog/api';
 export const AKTIVITET_BASE_URL = '/veilarbaktivitet/api';
 export const OPPFOLGING_BASE_URL = '/veilarboppfolging/api';
 export const VEILARBLEST_BASE_URL = '/veilarblest/api';
-export const VEILARBVEDTAKINFO_BASE_URL = '/veilarbvedtakinfo/api';
 export const PERSON_BASE_URL = '/veilarbperson/api';
 export const MALVERK_BASE_URL = '/veilarbmalverk/api';
 export const VEILEDER_BASE_URL = '/veilarbveileder/api';
