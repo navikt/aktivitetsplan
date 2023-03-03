ea245749f95bd708cb3d67e6372308641c8eb463
sigurdgroneng <sigurd.groneng@nav.no>
2023-03-03 10:31:07 +0100

    Outdir build instead of dist

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
 
     return {
+        build: {
+            outDir: 'build',
+        },
         plugins: [
             react(),
             svgr(),
