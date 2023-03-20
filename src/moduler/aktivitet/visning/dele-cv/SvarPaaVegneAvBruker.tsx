598f38860c8dda9bebfc3a9c8f62dcf42aa407b4
sigurdgroneng <sigurd.groneng@nav.no>
2023-03-20 11:14:48 +0100

    Add missing file to last commit

--- src/moduler/aktivitet/visning/dele-cv/SvarPaaVegneAvBruker.tsx
+++ src/moduler/aktivitet/visning/dele-cv/SvarPaaVegneAvBruker.tsx
@@ -36,24 +36,17 @@ export const SvarPaaVegneAvBruker = ({ formhandler, datoBegrensninger }: Props) 
 
     const feil = formhandler.touched ? formhandler.error : undefined;
     return (
-        <div className="mb-4 bg-surface-alt-3-subtle border-border-alt-3 border rounded-md p-4">
+        <div className="mb-4 bg-surface-alt-3-subtle border-border-alt-3 border rounded-md p-4 space-y-4">
             <div className="flex justify-between">
-                <Heading size="medium" level="3">
+                <Heading size="small" level="3">
                     Svar på vegne av brukeren
                 </Heading>
                 <Detail>FOR NAV-ANSATT</Detail>
             </div>
+            <BodyShort>Når var du i dialog med brukeren om å dele CV-en deres med denne arbeidsgiveren</BodyShort>
             <div onClick={preventCloseOnInsideClick}>
                 <DatePicker {...datepickerProps} onOpenToggle={() => setPopoverOpen(!popoverOpen)} open={popoverOpen}>
-                    <DatePicker.Input
-                        {...inputProps}
-                        error={feil}
-                        label={
-                            <BodyShort>
-                                Når var du i dialog med brukeren om å dele CV-en deres med denne arbeidsgiveren
-                            </BodyShort>
-                        }
-                    />
+                    <DatePicker.Input {...inputProps} error={feil} label={'Dato (obligatorisk)'} />
                 </DatePicker>
             </div>
         </div>
