5dcae646b5598928435396c724a1595e3a46a9ad
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-03-22 13:06:49 +0100

    fix gh pages base_urls for msw

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,12 @@
 const BASE_URL = import.meta.env.BASE_URL;
 
-const stripPrependingSlash = (url: string) => (url.startsWith('/') ? url.slice(1) : url);
+const erGHpages = import.meta.env.VITE_USE_HASH_ROUTER === 'true';
+
+const stripPrependingSlash = (url: string) => {
+    if (erGHpages) return '';
+
+    return url.startsWith('/') ? url.slice(1) : url;
+};
 export const DIALOG_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarbdialog/api';
 export const AKTIVITET_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarbaktivitet/api';
 export const OPPFOLGING_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarboppfolging/api';
@@ -8,11 +14,3 @@ export const VEILARBLEST_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarble
 export const PERSON_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarbperson/api';
 export const MALVERK_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarbmalverk/api';
 export const VEILEDER_BASE_URL = stripPrependingSlash(BASE_URL) + '/veilarbveileder/api';
-
-// export const DIALOG_BASE_URL = '/veilarbdialog/api';
-// export const AKTIVITET_BASE_URL = '/veilarbaktivitet/api';
-// export const OPPFOLGING_BASE_URL = '/veilarboppfolging/api';
-// export const VEILARBLEST_BASE_URL = '/veilarblest/api';
-// export const PERSON_BASE_URL = '/veilarbperson/api';
-// export const MALVERK_BASE_URL = '/veilarbmalverk/api';
-// export const VEILEDER_BASE_URL = '/veilarbveileder/api';
