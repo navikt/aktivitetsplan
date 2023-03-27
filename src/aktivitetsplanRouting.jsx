c8cb5ad80d3c74bf5c4a0a629e6ee90e6dc74daf
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-03-27 12:48:05 +0200

    fix more imports

--- src/aktivitetsplanRouting.jsx
+++ src/aktivitetsplanRouting.jsx
@@ -4,14 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import AktivitetRoutes from './moduler/aktivitet/aktivitet-routes';
 import InformasjonModal from './moduler/informasjon/informasjon-modal';
 import Aktivitetsmal from './moduler/mal/mal';
-import AktivitetsplanPrint from './moduler/utskrift/aktivitetsplanprint';
 
 const AktivitetsplanRouting = () => {
-    return (<Switch>
-        <Route path={'/mal'} component={Aktivitetsmal}/>
-        <Route path={'/aktivitet'} component={AktivitetRoutes}/>
-        <Route path={'/utskrift'} component={AktivitetsplanPrint}/>
-    </Switch>)
+    return (
+        <Switch>
+            <Route path={'/mal'} component={Aktivitetsmal} />
+            <Route path={'/aktivitet'} component={AktivitetRoutes} />
+        </Switch>
+    );
 };
 
 function Public() {
