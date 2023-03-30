760470598f963054b80f7d74fc4dda3651270c44
sigurdgroneng <sigurd.groneng@nav.no>
2023-03-30 15:00:44 +0200

    --amend

--- src/moduler/identitet/identitet-selector.js
+++ src/moduler/identitet/identitet-selector.js
@@ -12,11 +12,6 @@ export function selectIdentitetStatus(state) {
     return selectIdentitetSlice(state).status;
 }
 
-/*
-export function selectErVeileder(state) {
-    return selectIdentitetSlice(state).data.erVeileder;
-}*/
-
 export function selectErBruker(state) {
     return selectIdentitetSlice(state).data.erBruker;
 }
