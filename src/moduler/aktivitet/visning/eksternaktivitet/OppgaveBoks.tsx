4b282687b81da5f369c09db4b2bc58eb7527e348
sigurdgroneng <sigurd.groneng@nav.no>
2023-03-30 14:35:19 +0200

    --amend

--- src/moduler/aktivitet/visning/eksternaktivitet/OppgaveBoks.tsx
+++ src/moduler/aktivitet/visning/eksternaktivitet/OppgaveBoks.tsx
@@ -1,6 +1,5 @@
 import { Alert, Button, Heading } from '@navikt/ds-react';
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import { Oppgave, OppgaveLenke } from '../../../../datatypes/eksternAktivitetTypes';
 import { useErVeileder } from '../../../../Provider';
