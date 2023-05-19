6c097bcc467c83a7faa118059f4a7ab82dd117e4
sigurdgroneng <sigurd.groneng@nav.no>
2023-05-19 14:41:01 +0200

    Set users in sentry

--- src/felles-komponenter/utils/logging.ts
+++ src/felles-komponenter/utils/logging.ts
@@ -3,29 +3,13 @@ import { VeilarbAktivitet } from '../../datatypes/internAktivitetTypes';
 import { Lest } from '../../datatypes/lestTypes';
 import { Oppfolgingsperiode } from '../../datatypes/oppfolgingTypes';
 import { AKTIVITET_BASE_URL } from '../../environment';
+import { hash } from './hash';
 
 interface FrontendEvent {
     name: string;
     fields?: {};
     tags?: {};
 }
-
-// Shameless steal from https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
-const hash = (str: string | undefined, seed = 0): string | undefined => {
-    if (str === undefined) return undefined;
-    let h1 = 0xdeadbeef ^ seed,
-        h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-
-    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
-    return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString();
-};
 
 export default function loggEvent(eventNavn: string, feltObjekt?: object, tagObjekt?: object) {
     const event: FrontendEvent = { name: eventNavn, fields: feltObjekt, tags: tagObjekt };
