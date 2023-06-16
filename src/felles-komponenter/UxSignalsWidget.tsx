56d064d76aa2854397b878683542fabbbcf6c307
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-06-16 12:29:17 +0200

    move visibility logic inside component and hide on mobile

--- src/felles-komponenter/UxSignalsWidget.tsx
+++ src/felles-komponenter/UxSignalsWidget.tsx
@@ -1,20 +1,31 @@
 import React, { useEffect } from 'react';
 
-const UxSignalsWidget = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = 'https://uxsignals-frontend.uxsignals.app.iterate.no/embed.js';
-        document.body.appendChild(script);
+import { ER_PROD, USE_HASH_ROUTER } from '../constant';
+import { useErVeileder } from '../Provider';
 
-        return () => {
-            try {
-                document.body.removeChild(script);
-            } catch {
-                /* empty */
-            }
-        };
-    }, []);
+const UxSignalsWidget = () => {
+    const erVeileder = useErVeileder();
+    const isDesktopOnRender = window.innerWidth >= 992;
+    const showUxSignalsWidget = !erVeileder && !USE_HASH_ROUTER && ER_PROD && isDesktopOnRender;
+
+    useEffect(() => {
+        if (showUxSignalsWidget) {
+            const script = document.createElement('script');
+            script.async = true;
+            script.src = 'https://uxsignals-frontend.uxsignals.app.iterate.no/embed.js';
+            document.body.appendChild(script);
+
+            return () => {
+                try {
+                    document.body.removeChild(script);
+                } catch {
+                    /* empty */
+                }
+            };
+        }
+    }, [showUxSignalsWidget]);
+
+    if (!showUxSignalsWidget) return null;
 
     return <div data-uxsignals-embed="study-b6mh8y7gdw" className="uxsignalswidget max-w-[620px] mx-auto pb-8" />;
 };
