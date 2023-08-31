5c9a735dec465a5fbe3ae653f9eeb3dae7eeb45c
johannetronstad <johanne.tronstad@nav.no>
2023-08-31 10:20:06 +0200

    ux-signals show in dev

--- src/felles-komponenter/UxSignalsWidget.tsx
+++ src/felles-komponenter/UxSignalsWidget.tsx
@@ -6,7 +6,7 @@ import { useErVeileder } from '../Provider';
 const UxSignalsWidget = () => {
     const erVeileder = useErVeileder();
     const isDesktopOnRender = window.innerWidth >= 992;
-    const showUxSignalsWidget = !erVeileder && !USE_HASH_ROUTER && ER_PROD && isDesktopOnRender;
+    const showUxSignalsWidget = !erVeileder && !USE_HASH_ROUTER && isDesktopOnRender;
 
     useEffect(() => {
         if (showUxSignalsWidget) {
@@ -27,7 +27,7 @@ const UxSignalsWidget = () => {
 
     if (!showUxSignalsWidget) return null;
 
-    return <div data-uxsignals-embed="panel-838jxh69e" data-uxsignals-mode="demo" className="uxsignalswidget max-w-[620px] mx-auto pb-8" />;
+    return <div data-uxsignals-embed="panel-838jxh69e" data-uxsignals-mode={ER_PROD ? "" : "demo"} className="uxsignalswidget max-w-[620px] mx-auto pb-8" />;
 };
 
 export default UxSignalsWidget;
