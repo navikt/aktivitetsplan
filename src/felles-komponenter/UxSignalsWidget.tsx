2839e6e2f9a739c1703634e85c31d495bfa1e447
johannetronstad <johanne.tronstad@nav.no>
2023-08-31 10:09:00 +0200

    ux-signals demo mode

--- src/felles-komponenter/UxSignalsWidget.tsx
+++ src/felles-komponenter/UxSignalsWidget.tsx
@@ -27,7 +27,7 @@ const UxSignalsWidget = () => {
 
     if (!showUxSignalsWidget) return null;
 
-    return <div data-uxsignals-embed="panel-838jxh69e" className="uxsignalswidget max-w-[620px] mx-auto pb-8" />;
+    return <div data-uxsignals-embed="panel-838jxh69e" data-uxsignals-mode="demo" className="uxsignalswidget max-w-[620px] mx-auto pb-8" />;
 };
 
 export default UxSignalsWidget;
