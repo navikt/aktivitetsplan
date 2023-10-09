d0f98648d2a2fe5f43efa5d80ef163fd1a8ffa77
Sigurd Grøneng <sigurd.groneng@nav.no>
2023-10-09 15:47:41 +0200

    Hotfix modal (#1199)
    * Proper unmount

--- src/rootWrapper.tsx
+++ src/rootWrapper.tsx
@@ -3,27 +3,21 @@ import '@navikt/ds-css';
 import './tailwind.css';
 import './index.less';
 
-import { Provider as AkselModalMountProvider } from '@navikt/ds-react';
 import * as Sentry from '@sentry/react';
 import React from 'react';
-import * as ReactDOM from 'react-dom';
 
 import App from './app';
 import { hash } from './felles-komponenter/utils/hash';
 import Provider from './Provider';
 import { SentryRoutes } from './sentry';
+import { createRoot } from 'react-dom/client';
 
 export const renderAsReactRoot = (appElement: HTMLElement, props?: { fnr?: string }) => {
-    const rootElement = document.getElementById('root') || undefined;
-    Sentry.setUser({
-        id: hash(props?.fnr),
-    });
-    ReactDOM.render(
-        <AkselModalMountProvider rootElement={rootElement} appElement={appElement}>
-            <Provider fnr={props?.fnr}>
-                <App Routes={SentryRoutes} key={'1'} />
-            </Provider>
-        </AkselModalMountProvider>,
-        appElement,
+    const rootElement = createRoot(appElement || document.getElementById('root')!);
+    Sentry.setUser({ id: hash(props?.fnr) });
+    rootElement.render(
+        <Provider fnr={props?.fnr}>
+            <App Routes={SentryRoutes} key={'1'} />
+        </Provider>,
     );
 };
