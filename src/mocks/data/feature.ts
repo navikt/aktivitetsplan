c5d6543ab692c51edd0145534d93db1f8b1c2887
sigurdgroneng <sigurd.groneng@nav.no>
2023-10-09 16:01:40 +0200

    Remove stuff

--- src/mocks/data/feature.ts
+++ src/mocks/data/feature.ts
@@ -1,5 +1,3 @@
-import { Features, VIS_SPRAKSJEKK } from '../../moduler/feature/feature';
+import { Features } from '../../moduler/feature/feature';
 
-export const features: Features = {
-    [VIS_SPRAKSJEKK]: true,
-};
+export const features: Features = {};
