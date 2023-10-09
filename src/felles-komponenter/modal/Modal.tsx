612939633a69a8ff8e1e3badb27149ed933fc85d
sigurdgroneng <sigurd.groneng@nav.no>
2023-10-09 13:52:20 +0200

    Fix test for ref callback version

--- src/felles-komponenter/modal/Modal.tsx
+++ src/felles-komponenter/modal/Modal.tsx
@@ -63,7 +63,7 @@ const Modal = (props: Props) => {
     // }, [modalRef.current]);
 
     return (
-        <AkselModal ref={onRefSet} portal onClose={closeFuncOrDefault} className="lg:w-120">
+        <AkselModal portal ref={onRefSet} onClose={closeFuncOrDefault} className="lg:w-120">
             <AkselModal.Header closeButton={true} aria-labelledby="modal-heading">
                 <div className="space-y-2">
                     <Heading id="modal-heading" size="large">
