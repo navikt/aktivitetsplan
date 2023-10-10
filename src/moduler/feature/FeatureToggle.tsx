3a1f92839553bca08371d2c21f7eeef008144f8f
sigurdgroneng <sigurd.groneng@nav.no>
2023-10-10 16:02:32 +0200

    Dont fetch no toggles

--- src/moduler/feature/FeatureToggle.tsx
+++ src/moduler/feature/FeatureToggle.tsx
@@ -1,20 +1,21 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode } from 'react';
 
-import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
-import { useErVeileder } from '../../Provider';
-import { hentFeatures } from './feature-slice';
+// import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
+// import { useErVeileder } from '../../Provider';
+// import { hentFeatures } from './feature-slice';
 
 const FeatureToggle = ({ children }: { children: ReactNode }) => {
-    const dispatch = useAppDispatch();
-    const erVeileder = useErVeileder();
+    // const dispatch = useAppDispatch();
+    // const erVeileder = useErVeileder();
 
-    useEffect(() => {
-        // NB: Vi har foreløpig feature toggles som kun påvirker veiledere
-        // Dispatcher kun for veiledere for å unngå unødvendig kall for bruker
-        if (erVeileder) {
-            dispatch(hentFeatures());
-        }
-    }, []);
+    // useEffect(() => {
+    // NB: Vi har foreløpig feature toggles som kun påvirker veiledere
+    // Dispatcher kun for veiledere for å unngå unødvendig kall for bruker
+    // if (erVeileder) {
+    // NB: Vi har foreløpig ingen toggles, utkommenter når vi trenger toggles
+    // dispatch(hentFeatures());
+    // }
+    // }, []);
 
     return <>{children}</>;
 };
