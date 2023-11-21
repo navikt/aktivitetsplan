76372461b9ff1d18edf367fd99181d9a40391704
Sigurd Grøneng <sigurd.groneng@nav.no>
2023-11-21 13:43:45 +0100

    Test arena aktivitet utenfor oppfolg (#1224)
    * Test arena-aktivitet utenfor oppfolg
    
    * Test aktivite endret før all oppfolg

--- src/moduler/filtrering/filter/PeriodeFilter.test.tsx
+++ src/moduler/filtrering/filter/PeriodeFilter.test.tsx
@@ -44,6 +44,19 @@ const gammelArenaAktivitet = {
     id: 'ARENATA22',
     opprettetDato: '2017-02-30T10:46:10.971+01:00', // I gammel oppfølgingsperiode
 };
+// Start nyeste periode '2018-01-31T10:46:10.971+01:00',
+const arenaAktivitetUtenforPeriode = {
+    ...arenaMockAktiviteter[0],
+    tittel: 'Aktivitet utenfor perioder',
+    id: 'ARENATA33',
+    opprettetDato: '2018-01-02T10:46:10.971+01:00', // I mellom periodene
+};
+const arenaAktivitetForOppfolging = {
+    ...arenaMockAktiviteter[0],
+    tittel: 'Aktivitet før all oppfolging',
+    id: 'ARENATA44',
+    opprettetDato: '2017-01-01T10:46:10.971+01:00', // Før alle periodene
+};
 const veilarbAktivitet = {
     ...mockTestAktiviteter[0],
     tittel: 'Veilarbaktivitet',
@@ -77,7 +90,7 @@ const initialStore = {
         },
         arenaAktiviteter: {
             status: Status.OK,
-            data: [arenaAktivitet, gammelArenaAktivitet],
+            data: [arenaAktivitet, gammelArenaAktivitet, arenaAktivitetUtenforPeriode],
         },
         oppfolging: {
             status: Status.OK,
@@ -135,6 +148,24 @@ describe('PeriodeFilter.tsx', () => {
             await userEvent.selectOptions(screen.getByLabelText('Periode'), '30. Jan 2017 - 31. Dec 2017');
             getByText('Gammel Arenaaktivitet');
             expect(queryByText('Arenaaktivitet')).not.toBeTruthy();
+        });
+        it('skal vise aktivitet endret før siste oppfølginsperiode men etter tidligere oppfølgingsperiode i siste oppfølgingsperode', async () => {
+            const store = configureStore({
+                reducer,
+                preloadedState: initialStore,
+            });
+            const { getByText } = render(<WrappedHovedside store={store} />);
+            getByText(arenaAktivitetUtenforPeriode.tittel);
+        });
+        it('skal ikke vise aktivitet endret før oppfølging i noen av periodene', async () => {
+            const store = configureStore({
+                reducer,
+                preloadedState: initialStore,
+            });
+            const { queryByText } = render(<WrappedHovedside store={store} />);
+            expect(queryByText(arenaAktivitetForOppfolging.tittel)).not.toBeTruthy();
+            await userEvent.selectOptions(screen.getByLabelText('Periode'), '30. Jan 2017 - 31. Dec 2017');
+            expect(queryByText(arenaAktivitetForOppfolging.tittel)).not.toBeTruthy();
         });
         describe('datoErIPeriode-filter', () => {
             const gammelOpprettetDato = gammelArenaAktivitet.opprettetDato;
