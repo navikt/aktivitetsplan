6aeee176e627af8560fe85fc997cc646e343b0fa
sigurdgroneng <sigurd.groneng@nav.no>
2024-02-13 14:05:23 +0100

    Set numPages onLoadSuccess

--- src/moduler/journalforing/JournalforingPage.tsx
+++ src/moduler/journalforing/JournalforingPage.tsx
@@ -1,12 +1,14 @@
 import Innholdslaster from '../../felles-komponenter/utils/Innholdslaster';
-import React from 'react';
+import React, { useState } from 'react';
 import { hentPdfTilForhaandsvisning, selectArkivStatus, selectPdf } from '../verktoylinje/arkivering/arkivering-slice';
 import { Button } from '@navikt/ds-react';
 import { Status } from '../../createGenericSlice';
 import { useSelector } from 'react-redux';
 import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
 import { selectVistOppfolgingsperiode } from '../aktivitet/aktivitetlisteSelector';
-import { Document, pdfjs } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+
 // @ts-ignore
 import workerUrl from 'pdfjs-dist/build/pdf.worker.min.js?url';
 
@@ -34,9 +36,17 @@ export const JournalforingPage = () => {
     const arkiverer = [Status.PENDING, Status.RELOADING].includes(useSelector(selectArkivStatus));
     const pdf = useSelector(selectPdf);
 
+    const [numPages, setNumPages] = useState(0);
+    const onDocumentLoadSuccess = ({ numPages: nextNumPages }: PDFDocumentProxy): void => {
+        setNumPages(nextNumPages);
+    };
+
+    const containerWidth = 600;
+    const maxWidth = 600;
+
     return (
-        <section className="flex flex-col justify-center items-center p-8">
-            <div className="aktivitetsplanprint flex justify-center items-center">
+        <section className="grow flex flex-col justify-center items-center p-8">
+            <div className="w-full flex flex-col justify-center items-center">
                 <Innholdslaster avhengigheter={[]}>
                     <div className="px-12 print:border-none space-x-4">
                         <Button
@@ -55,8 +65,16 @@ export const JournalforingPage = () => {
                         </Button>
                     </div>
                     {pdf && (
-                        <div className="border">
-                            <Document file={pdf} />
+                        <div className="mt-4 border h-80 w-full">
+                            <Document onLoadSuccess={onDocumentLoadSuccess} file={createBlob(pdf)}>
+                                {Array.from(new Array(numPages), (el, index) => (
+                                    <Page
+                                        key={`page_${index + 1}`}
+                                        pageNumber={index + 1}
+                                        width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+                                    />
+                                ))}
+                            </Document>
                         </div>
                     )}
                 </Innholdslaster>
