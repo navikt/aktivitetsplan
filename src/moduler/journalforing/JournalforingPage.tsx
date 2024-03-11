526c8ee7ecf847f7defb8c46eedfea5164bc51b0
Mads Lee Giil <mads.lee.giil@nav.no>
2024-03-11 09:19:32 +0100

    Lag sidebar for journalføringsside

--- src/moduler/journalforing/JournalforingPage.tsx
+++ src/moduler/journalforing/JournalforingPage.tsx
@@ -1,64 +1,17 @@
 import Innholdslaster from '../../felles-komponenter/utils/Innholdslaster';
 import React from 'react';
-import {
-    hentPdfTilForhaandsvisning,
-    arkiver,
-    selectArkivStatus,
-    selectPdf,
-    selectForhaandsvisningOpprettet,
-} from '../verktoylinje/arkivering/arkivering-slice';
-import { Button, Heading } from '@navikt/ds-react';
-import { Status } from '../../createGenericSlice';
+import { selectArkivStatus, selectPdf } from '../verktoylinje/arkivering/arkivering-slice';
 import { useSelector } from 'react-redux';
-import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
-import { selectVistOppfolgingsperiode } from '../aktivitet/aktivitetlisteSelector';
 import { PdfViewer } from './PdfViewer';
-import { useRoutes } from '../../routes';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 export const JournalforingPage = () => {
-    const dispatch = useAppDispatch();
-    const vistOppfolgingsperiode = useSelector(selectVistOppfolgingsperiode);
     const arkivStatus = useSelector(selectArkivStatus);
-    const arkiverer = [Status.PENDING, Status.RELOADING].includes(arkivStatus);
     const pdf = useSelector(selectPdf);
-    const forhaandsvisningOpprettet = useSelector(selectForhaandsvisningOpprettet);
-    const { hovedsideRoute } = useRoutes();
-
-    const sendTilArkiv = () => {
-        if (forhaandsvisningOpprettet) {
-            dispatch(arkiver({ oppfolgingsperiodeId: vistOppfolgingsperiode!!.uuid, forhaandsvisningOpprettet }));
-        }
-    };
 
     return (
-        <section className="grow flex flex-col justify-center items-center py-8 h-full sticky">
-            <div className="flex flex-col justify-center items-start container space-y-4">
-                <Heading size="large">Journalføring</Heading>
-                <div className=" print:border-none space-x-8 flex items-center pb-4 self-stretch">
-                    <ReactRouterLink
-                        className="text-text-action underline hover:no-underline"
-                        to={hovedsideRoute()}
-                        tabIndex={0}
-                    >
-                        Til aktivitetsplanen
-                    </ReactRouterLink>
-                    <Button
-                        disabled={arkiverer || !forhaandsvisningOpprettet}
-                        variant="secondary"
-                        onClick={() => sendTilArkiv()}
-                    >
-                        Journalfør
-                    </Button>
-                    <Button
-                        disabled={arkiverer}
-                        variant="secondary"
-                        onClick={() => dispatch(hentPdfTilForhaandsvisning(vistOppfolgingsperiode!!.uuid))}
-                    >
-                        Forhåndsvisning
-                    </Button>
-                </div>
-            </div>
+        <section className="flex flex-row py-8">
+            <Sidebar />
 
             {pdf && (
                 <Innholdslaster avhengigheter={[arkivStatus]}>
---
+++ src/moduler/journalforing/Sidebar.tsx
@@ -0,0 +1,61 @@
+import React, { FunctionComponent } from 'react';
+import { BodyShort, Button, Heading } from '@navikt/ds-react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import {
+    arkiver,
+    hentPdfTilForhaandsvisning,
+    selectArkivStatus,
+    selectForhaandsvisningOpprettet,
+} from '../verktoylinje/arkivering/arkivering-slice';
+import { Status } from '../../createGenericSlice';
+import { useRoutes } from '../../routes';
+import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { selectVistOppfolgingsperiode } from '../aktivitet/aktivitetlisteSelector';
+
+const Sidebar: FunctionComponent = () => {
+    const dispatch = useAppDispatch();
+    const vistOppfolgingsperiode = useSelector(selectVistOppfolgingsperiode);
+    const forhaandsvisningOpprettet = useSelector(selectForhaandsvisningOpprettet);
+    const arkivStatus = useSelector(selectArkivStatus);
+    const arkiverer = [Status.PENDING, Status.RELOADING].includes(arkivStatus);
+    const { hovedsideRoute } = useRoutes();
+
+    const sendTilArkiv = () => {
+        if (forhaandsvisningOpprettet) {
+            dispatch(arkiver({ oppfolgingsperiodeId: vistOppfolgingsperiode!!.uuid, forhaandsvisningOpprettet }));
+        }
+    };
+
+    return (
+        <div className="items-start container space-y-4">
+            <Heading size="large">Journalføring</Heading>
+            <div className="print:border-none space-y-8 flex flex-col pb-4">
+                <ReactRouterLink
+                    className="text-text-action underline hover:no-underline"
+                    to={hovedsideRoute()}
+                    tabIndex={0}
+                >
+                    Til aktivitetsplanen
+                </ReactRouterLink>
+                <BodyShort>Aktiviteter og dialogtråder tilknyttet KVP blir ikke inkludert i journalføringen.</BodyShort>
+                <Button
+                    disabled={arkiverer || !forhaandsvisningOpprettet}
+                    variant="primary"
+                    onClick={() => sendTilArkiv()}
+                >
+                    Journalfør
+                </Button>
+                <Button
+                    disabled={arkiverer}
+                    variant="primary"
+                    onClick={() => dispatch(hentPdfTilForhaandsvisning(vistOppfolgingsperiode!!.uuid))}
+                >
+                    Forhåndsvisning
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
