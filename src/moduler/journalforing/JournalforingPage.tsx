b4ee2f57b2566753568eaec2d901918f865f4c53
johannetronstad <johanne.tronstad@nav.no>
2024-03-12 13:55:52 +0100

    Bruk vår egen spinner isteden for innholdslaster

--- src/moduler/journalforing/JournalforingPage.tsx
+++ src/moduler/journalforing/JournalforingPage.tsx
@@ -23,11 +23,9 @@ export const JournalforingPage = () => {
         <div className="flex flex-col grow ">
             <section className="flex md:flex-row flex-col">
                 <Sidebar />
-                <Innholdslaster avhengigheter={[arkivStatus]}>
-                    <div className="h-full grow bg-bg-subtle max-h-100vh overflow-x-scroll overflow-y-hidden pb-4">
-                        {<PdfViewer pdf={pdf} />}
-                    </div>
-                </Innholdslaster>
+                <div className="h-full grow bg-bg-subtle max-h-100vh overflow-x-scroll overflow-y-hidden pb-4">
+                    <PdfViewer pdf={pdf} />
+                </div>
             </section>
         </div>
     );
