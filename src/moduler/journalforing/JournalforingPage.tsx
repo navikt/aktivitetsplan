3ce314695eca502cb079cef9083afb21b508cec7
johannetronstad <johanne.tronstad@nav.no>
2024-03-13 14:54:15 +0100

    Gjør sidebar sticky

--- src/moduler/journalforing/JournalforingPage.tsx
+++ src/moduler/journalforing/JournalforingPage.tsx
@@ -1,13 +1,10 @@
-import Innholdslaster from '../../felles-komponenter/utils/Innholdslaster';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { hentPdfTilForhaandsvisning, selectArkivStatus, selectPdf } from '../verktoylinje/arkivering/arkivering-slice';
 import { useSelector } from 'react-redux';
 import { PdfViewer } from './PdfViewer';
 import Sidebar from './Sidebar';
 import { selectVistOppfolgingsperiode } from '../aktivitet/aktivitetlisteSelector';
 import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
-import { Status } from '../../createGenericSlice';
-import { Loader } from '@navikt/ds-react';
 
 export const JournalforingPage = () => {
     const arkivStatus = useSelector(selectArkivStatus);
@@ -17,11 +14,12 @@ export const JournalforingPage = () => {
 
     useEffect(() => {
         dispatch(hentPdfTilForhaandsvisning(vistOppfolgingsperiode!!.uuid));
+        console.log('arkivstatus: ', arkivStatus);
     }, []);
 
     return (
-        <div className="flex flex-col grow ">
-            <section className="flex md:flex-row flex-col">
+        <div className="flex flex-col grow">
+            <section className="flex md:flex-row flex-col relative">
                 <Sidebar />
                 <div className="h-full grow bg-bg-subtle max-h-100vh overflow-x-scroll overflow-y-hidden pb-4">
                     <PdfViewer pdf={pdf} />
--- src/moduler/journalforing/Sidebar.tsx
+++ src/moduler/journalforing/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { BodyShort, Button, Heading, Select } from '@navikt/ds-react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import {
@@ -13,7 +13,7 @@ import useAppDispatch from '../../felles-komponenter/hooks/useAppDispatch';
 import { useSelector } from 'react-redux';
 import { selectVistOppfolgingsperiode } from '../aktivitet/aktivitetlisteSelector';
 import { selectOppfolgingsPerioder } from '../oppfolging-status/oppfolging-selector';
-import { formaterDatoKortManed, formaterDatoManed } from '../../utils/dateUtils';
+import { formaterDatoKortManed } from '../../utils/dateUtils';
 
 const Sidebar: FunctionComponent = () => {
     const dispatch = useAppDispatch();
@@ -35,7 +35,7 @@ const Sidebar: FunctionComponent = () => {
     };
 
     return (
-        <div className="items-start space-y-4 max-w-96 py-8 px-8">
+        <div className="items-start space-y-4 max-w-96 py-8 px-8 bg-white sticky top-0 h-screen">
             <Heading size="large">Journalføring</Heading>
             <div className="print:border-none space-y-8 flex flex-col pb-4">
                 <ReactRouterLink
