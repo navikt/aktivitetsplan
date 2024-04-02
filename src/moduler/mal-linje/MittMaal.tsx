7a9010baaadf1d05463a566b3a5a1b35f34520ea
johannetronstad <johanne.tronstad@nav.no>
2024-04-02 10:33:46 +0200

    Legg til Alert på kunne ikke hente mål

--- src/moduler/mal-linje/MittMaal.tsx
+++ src/moduler/mal-linje/MittMaal.tsx
@@ -52,7 +52,7 @@ function MittMaal() {
 
     const noeHarFeilet = avhengigheter === 'ERROR';
     const { mal: malPromise, oppfolging: oppfolgingPromise } = useRouteLoaderData('root') as InitialPageLoadResult;
-    // if (noeHarFeilet) return <Alert variant={'error'}>Kunne ikke hente mål</Alert>;
+    if (noeHarFeilet) return <Alert variant={'error'}>Kunne ikke hente mål</Alert>;
     return (
         <>
             <div
