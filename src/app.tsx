3893cb04741ab20ce22b2dd9d4dca40fcee9946c
Mads Lee Giil <mads.lee.giil@nav.no>
2024-04-11 13:50:55 +0200

    Forsøk å bruke useAktivEnhet uten å gå via store

--- src/app.tsx
+++ src/app.tsx
@@ -6,9 +6,8 @@ import useAppDispatch from './felles-komponenter/hooks/useAppDispatch';
 import Timeoutbox from './felles-komponenter/timeoutbox/Timeoutbox';
 import { UpdateEventHandler } from './utils/UpdateHandler';
 import { HiddenIf } from './utils/utils';
-import { useAktivEnhet, useErVeileder } from './Provider';
+import { useErVeileder } from './Provider';
 import { Dispatch } from './store';
-import { selectAktivEnhet } from './moduler/aktivEnhet/aktivenhet-slice';
 
 function App({
     createRoutesForUser,
@@ -16,9 +15,7 @@ function App({
     createRoutesForUser: (dispatch: Dispatch, isVeileder: boolean) => ReturnType<typeof createBrowserRouter>;
 }) {
     const erVeileder = useErVeileder();
-    const aktivEnhet = useAktivEnhet();
     const dispatch = useAppDispatch();
-    dispatch(selectAktivEnhet(aktivEnhet));
     const routes = createRoutesForUser(dispatch, erVeileder);
     return (
         <div className="aktivitetsplanfs" id={AKTIVITETSPLAN_ROOT_NODE_ID}>
