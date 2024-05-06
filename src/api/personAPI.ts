84222f2924c95e939d61ef7652dd26acac6deddd
Mads Lee Giil <mads.lee.giil@nav.no>
2024-05-06 12:28:41 +0200

    Bruk nytt endepunkt for å hente navn og adresse som aksepterer behandlingsnummer

--- src/api/personAPI.ts
+++ src/api/personAPI.ts
@@ -1,11 +1,11 @@
 import { HarLoggetInnRespons, RegoppslagDto } from '../datatypes/types';
 import { PERSON_BASE_URL } from '../environment';
-import { fetchToJson, fetchToJsonPlain } from './utils';
+import { fetchToJson, fetchToJsonPlain, postAsJson } from './utils';
 
 export const hentPerson = (fnr: string) => fetchToJsonPlain(`${PERSON_BASE_URL}/v2/person?fnr=${fnr}`);
 
 export const hentAdresse = (fnr: string): Promise<RegoppslagDto> =>
-    fetchToJsonPlain(`${PERSON_BASE_URL}/v2/person/postadresse?fnr=${fnr}`);
+    postAsJson(`${PERSON_BASE_URL}/v3/person/hent-postadresse`, { fnr, behandlingsnummer: 'B579' });
 
 export const hentHarNivaa4 = (fnr: string): Promise<HarLoggetInnRespons> =>
     fetchToJson(`${PERSON_BASE_URL}/person/${fnr}/harNivaa4`);
