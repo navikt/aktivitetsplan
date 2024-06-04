c6effda354af10fb7838983f14e73dd9418e1691
Mads Lee Giil <mads.lee.giil@nav.no>
2024-06-04 15:46:23 +0200

    Bruk endringstekst fra GraphQl og marker endringsagent i bold

--- src/moduler/aktivitet/visning/versjoner/EndringsLinje.tsx
+++ src/moduler/aktivitet/visning/versjoner/EndringsLinje.tsx
@@ -1,21 +1,33 @@
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort } from '@navikt/ds-react';
 import React from 'react';
 
 import { formaterDatoEllerTidSiden } from '../../../../utils/dateUtils';
 import { Endring } from '../../../../datatypes/Historikk';
-import { hentBrukeravhengigTekst } from './brukeravhengigTekst';
 import { useErVeileder } from '../../../../Provider';
+
+const isUpperCase = (s: string) => s.toUpperCase() === s;
 
 export const EndringsLinje = ({ endring }: { endring: Endring }) => {
     const erBruker = !useErVeileder();
-    const brukeravhengigTekst = (
-        <Label className="inline">
-            {hentBrukeravhengigTekst(erBruker, endring.endretAvType as any, endring.endretAv)}
-        </Label>
+
+    const beskrivelse = erBruker ? endring.beskrivelseForBruker : endring.beskrivelseForVeileder;
+
+    const splittet = beskrivelse.split(' ').reduce(
+        (result, current) => {
+            const starterMedUppercase = isUpperCase(current.charAt(0));
+            const erPåStartenAvSetningen = result[1].length === 0;
+            if (starterMedUppercase && erPåStartenAvSetningen) {
+                return [`${result[0]} ${current}`, result[1]];
+            } else {
+                return [result[0], `${result[1]} ${current}`];
+            }
+        },
+        ['', ''],
     );
+
     return (
         <div className="pb-4">
-            {brukeravhengigTekst} {erBruker ? endring.beskrivelseForBruker : endring.beskrivelseForVeileder}
+            <b>{splittet[0]}</b> {splittet[1]}
             <BodyShort>{formaterDatoEllerTidSiden(endring.tidspunkt)}</BodyShort>
         </div>
     );
