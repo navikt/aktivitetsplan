afa876585a814bf3aa21c4297183ecbf94389cc6
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-10-03 09:09:17 +0200

    text forandring på under 18 info hva er aktivitetsplan

--- src/moduler/informasjon/innsynsrettPanel.tsx
+++ src/moduler/informasjon/innsynsrettPanel.tsx
@@ -24,14 +24,14 @@ export const InnsynsrettPanel = () => {
                     Er du under 18 år?
                 </Heading>
                 <BodyShort className="pb-4">
-                    Når du er under 18 år så har dine foreldre eller foresatte rett til innsyn i din sak. Det betyr at
-                    foreldrene dine kan be om å få se alt du skriver i NAV sine tjenester. Derfor bør du ikke skrive noe
-                    her som du ikke vil at foreldrene dine skal lese.
+                    Når du er under 18 år så har dine foresatte rett til å be om å få se alt du skriver i NAV sine tjenester.
+                    Derfor bør du ikke skrive noe her som du ikke vil at foresatte dine skal lese.
+
                 </BodyShort>
 
                 <BodyShort>
-                    Når du blir 18 år vil de ikke kunne lese nye ting du skriver, men de kan fortsatt lese det du skrev
-                    før du fylte 18 år
+                    Når du blir 18 år vil de ikke kunne lese nye ting du skriver,
+                    men de kan fortsatt lese det du skrev før du fylte 18 år.
                 </BodyShort>
             </Accordion.Content>
         </Accordion.Item>
