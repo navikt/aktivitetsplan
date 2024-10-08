af2044bb5f7ec69147a0d36ca9b6df1a01e24095
sigurdgroneng <sigurd.groneng@nav.no>
2024-10-08 13:40:28 +0200

    Fix sentry import

--- src/sentry.ts
+++ src/sentry.ts
@@ -10,19 +10,6 @@ import {
     createBrowserRouter,
 } from 'react-router-dom';
 
-export enum Env {
-    Local = 'local',
-    Dev = 'dev',
-    Prod = 'prod',
-}
-
-export const getEnv = (): string => {
-    const { hostname } = window.location;
-    if (hostname.includes('dev.nav.no')) return Env.Dev;
-    if (hostname.includes('nav.no')) return Env.Prod;
-    return Env.Local;
-};
-
 const fnrRegexRegel = {
     regex: /[0-9]{11}/g,
     erstatning: '&lt;fnr&gt;',
