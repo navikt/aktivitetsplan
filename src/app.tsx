4fbdc31e2e3e9346f62a7049f93540f1ae1d467b
Sigurd Grøneng <sigurd.groneng@nav.no>
2024-10-09 10:36:05 +0200

    Slett timeoutbox (#1314)
    * Slett timeoutbox
    
    * Update reducer also

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { AKTIVITETSPLAN_ROOT_NODE_ID, ER_INTERN_FLATE } from './constant';
+import { AKTIVITETSPLAN_ROOT_NODE_ID } from './constant';
 import useAppDispatch from './felles-komponenter/hooks/useAppDispatch';
-import Timeoutbox from './felles-komponenter/timeoutbox/Timeoutbox';
 import { UpdateEventHandler } from './utils/UpdateHandler';
-import { HiddenIf } from './utils/utils';
 import { useErVeileder, useFnrOgEnhetContext } from './Provider';
 import { Dispatch } from './store';
 
@@ -26,9 +24,6 @@ function App({
         <div className="aktivitetsplanfs" id={AKTIVITETSPLAN_ROOT_NODE_ID}>
             <div className="aktivitetsplan-wrapper w-full">
                 <RouterProvider router={routes} />
-                <HiddenIf hidden={ER_INTERN_FLATE}>
-                    <Timeoutbox />
-                </HiddenIf>
             </div>
             <UpdateEventHandler />
         </div>
