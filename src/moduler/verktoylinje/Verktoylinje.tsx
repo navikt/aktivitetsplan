9849a27c569d036213d923e8eb449e839a649469
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-10-16 15:07:03 +0200

    Fjernet gamle "legg til aktivitet" knapp, bare nye som gjennstår

--- src/moduler/verktoylinje/Verktoylinje.tsx
+++ src/moduler/verktoylinje/Verktoylinje.tsx
@@ -2,9 +2,8 @@ import { PlusIcon } from '@navikt/aksel-icons';
 import { Button, Dropdown} from '@navikt/ds-react';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import loggEvent, { APNE_NY_AKTIVITET } from '../../felles-komponenter/utils/logging';
 import { useRoutes } from '../../routing/useRoutes';
 import { selectAktivitetStatus } from '../aktivitet/aktivitet-selector';
 import Filter from '../filtrering/Filter';
@@ -19,29 +18,15 @@ const Verktoylinje = () => {
     const underOppfolging: boolean = useSelector(selectErUnderOppfolging);
     const viserHistoriskPeriode = useSelector(selectViserHistoriskPeriode);
     const aktivitetStatus = useSelector(selectAktivitetStatus);
-    const navigate = useNavigate();
     const { nyAktivitetRoute } = useRoutes();
     const nyAktivitetBasePath = nyAktivitetRoute();
 
     const erVeileder = useErVeileder();
 
-
     return (
         <div className="flex flex-col gap-y-6">
             <div className="flex gap-y-4 sm:flex-row flex-col-reverse ">
                 <div className="flex gap-4 items-start flex-col sm:flex-row w-full">
-                    <Button
-                        loading={[Status.RELOADING, Status.PENDING].includes(aktivitetStatus)}
-                        className="self-stretch sm:self-auto"
-                        icon={<PlusIcon role="img" aria-hidden fontSize="1.5rem" />}
-                        disabled={viserHistoriskPeriode || aktivitetStatus === Status.ERROR}
-                        onClick={() => {
-                            loggEvent(APNE_NY_AKTIVITET);
-                            navigate(nyAktivitetRoute());
-                        }}
-                    >
-                        Legg til aktivitet
-                    </Button>
                     <div className="min-h-32">
                         <Dropdown>
                             <Button
