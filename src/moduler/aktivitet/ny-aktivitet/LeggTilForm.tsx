4f7298b45c9827c4a53f9e21a904e1a039a8cd0a
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-10-22 08:53:53 +0200

    endret til riktig text og lagt til divider

--- src/moduler/aktivitet/ny-aktivitet/LeggTilForm.tsx
+++ src/moduler/aktivitet/ny-aktivitet/LeggTilForm.tsx
@@ -29,7 +29,7 @@ const veilederItems = (nyAktivitetBasePath: string) =>([{
 ])
 
 const brukerItems = (nyAktivitetBasePath: string) =>([{
-        grouppeTittel : "For bruker og NAV-ansatt",
+        grouppeTittel : "Velg type aktivitet",
         grouppedItems: [
             { tittle: " En jobb jeg vil søke på" , link : `${nyAktivitetBasePath}/stilling`},
             { tittle: " En jobb jeg har nå" , link : `${nyAktivitetBasePath}/ijobb`},
@@ -47,35 +47,34 @@ const LeggTilForm = () => {
 
     const erVeileder = useErVeileder();
     const menuItemsGroup = erVeileder ? veilederItems(nyAktivitetBasePath) : brukerItems(nyAktivitetBasePath);
-
     return (
-        <div className="self-stretch sm:self-auto">
-            <Dropdown>
-                <Button
-                    as={Dropdown.Toggle}
-                    loading={[Status.RELOADING, Status.PENDING].includes(aktivitetStatus)}
-                    className="relative w-full"
-                    icon={<PlusIcon role="img" aria-hidden fontSize="1.5rem" />}
-                    disabled={viserHistoriskPeriode || aktivitetStatus === Status.ERROR}>
-                    Legg til aktivitet
-                </Button>
-                <Dropdown.Menu>
-                            {menuItemsGroup.map((item) => (
-                              <Dropdown.Menu.GroupedList>
-                                <Dropdown.Menu.GroupedList.Heading>
-                                    {item.grouppeTittel}
-                                </Dropdown.Menu.GroupedList.Heading>
-                                {item.grouppedItems.map((item) =>
-                                  <Dropdown.Menu.GroupedList.Item as={Link} to={item.link}>
-                                      {item.tittle}
-                                  </Dropdown.Menu.GroupedList.Item>)
-                                }
-
-                              </Dropdown.Menu.GroupedList>))
-                            }
-                </Dropdown.Menu>
-            </Dropdown>
-        </div>
+      <div className="self-stretch sm:self-auto">
+          <Dropdown>
+              <Button
+                as={Dropdown.Toggle}
+                loading={[Status.RELOADING, Status.PENDING].includes(aktivitetStatus)}
+                className="relative w-full"
+                icon={<PlusIcon role="img" aria-hidden fontSize="1.5rem" />}
+                disabled={viserHistoriskPeriode || aktivitetStatus === Status.ERROR}>
+                  Legg til aktivitet
+              </Button>
+              <Dropdown.Menu>
+                  {menuItemsGroup.map((item, index) => (
+                    <Dropdown.Menu.GroupedList key={index}>
+                        <Dropdown.Menu.GroupedList.Heading>
+                            {item.grouppeTittel}
+                        </Dropdown.Menu.GroupedList.Heading>
+                        {item.grouppedItems.map((subItem, subIndex) => (
+                          <Dropdown.Menu.GroupedList.Item as={Link} to={subItem.link} key={subIndex}>
+                              {subItem.tittle}
+                          </Dropdown.Menu.GroupedList.Item>
+                        ))}
+                        {erVeileder && index === 0 && <Dropdown.Menu.Divider />}
+                    </Dropdown.Menu.GroupedList>
+                  ))}
+              </Dropdown.Menu>
+          </Dropdown>
+      </div>
     );
 };
 
