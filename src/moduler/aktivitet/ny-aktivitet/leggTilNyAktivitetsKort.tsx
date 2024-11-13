0cc46057cd0455f59390598ca3d09121741d9245
henriktheboss <122083502+henriktheboss@users.noreply.github.com>
2024-11-13 12:45:22 +0100

    Ny aktivitet - dropdown istedetfor modaler (#1316)
    * lagt til toggle drop down menu ved siden av gamle kanpp, ikke lagt til veileder sjekk
    
    * lagt til sjekk for om er veileder som legger til ekstra aktivitet valg til veilederen
    
    * Glemt å legge til i forgje commit
    
    * Justering av text på dropdown.menu
    
    * Fix loading problem når opner lager nytt aktivittetskort
    
    * *
    
    * Fjernet gamle "legg til aktivitet" knapp, bare nye som gjennstår
    
    * flyttet knapp til egen fil og gjort til komponent
    
    * lagt tilbake i router config
    
    * fjernet div som ikke trengs
    
    * legg til aktivitet knapp er responsiv etter skjerm størrelse
    
    * litt ui fix
    
    * fjernet erveilder sjekk som ikke trenges, og litt clean up
    
    * små ting
    
    * gjort om render metode
    
    * endret til riktig text og lagt til divider
    
    * bedee sjekk og retur av divider linje
    
    * navn forbedring
    
    * fjernet knapp som ikke trengs lenger og skapte en bugg

---
+++ src/moduler/aktivitet/ny-aktivitet/leggTilNyAktivitetsKort.tsx
@@ -0,0 +1,82 @@
+import { PlusIcon } from '@navikt/aksel-icons';
+import { Button, Dropdown} from '@navikt/ds-react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Status } from '../../../createGenericSlice';
+import { useErVeileder } from '../../../Provider';
+import { selectAktivitetStatus } from '../aktivitet-selector';
+import { selectViserHistoriskPeriode } from '../../filtrering/filter/filter-selector';
+import { useRoutes } from '../../../routing/useRoutes';
+
+const veilederAktivitetsValg = (nyAktivitetBasePath: string) =>([{
+    groupTittel : "For NAV-ansatt",
+    groupedItems: [
+        { tittle: "Avtale om å søke jobber" , link : `${nyAktivitetBasePath}/sokeavtale`},
+        { tittle: "Møte med NAV" , link : `${nyAktivitetBasePath}/mote`},
+        { tittle: "Samtalereferat" , link: `${nyAktivitetBasePath}/samtalereferat`},
+        ]
+},
+    {
+        groupTittel : "For bruker og NAV-ansatt",
+        groupedItems: [
+            { tittle: " En jobb jeg vil søke på" , link : `${nyAktivitetBasePath}/stilling`},
+            { tittle: " En jobb jeg har nå" , link : `${nyAktivitetBasePath}/ijobb`},
+            { tittle: "Jobbrettet egenaktivitet " , link: `${nyAktivitetBasePath}/egen`},
+            { tittle: " Medisinsk behandling" , link: `${nyAktivitetBasePath}/behandling`},
+        ]
+    }
+])
+
+const brukerAktivitetsValg = (nyAktivitetBasePath: string) =>([{
+        groupTittel : "Velg type aktivitet",
+        groupedItems: [
+            { tittle: " En jobb jeg vil søke på" , link : `${nyAktivitetBasePath}/stilling`},
+            { tittle: " En jobb jeg har nå" , link : `${nyAktivitetBasePath}/ijobb`},
+            { tittle: "Jobbrettet egenaktivitet " , link: `${nyAktivitetBasePath}/egen`},
+            { tittle: " Medisinsk behandling" , link: `${nyAktivitetBasePath}/behandling`},
+        ]
+    }])
+
+const LeggTilNyAktivitetsKort = () => {
+    const viserHistoriskPeriode = useSelector(selectViserHistoriskPeriode);
+    const aktivitetStatus = useSelector(selectAktivitetStatus);
+    const { nyAktivitetRoute } = useRoutes();
+    const nyAktivitetBasePath = nyAktivitetRoute();
+
+
+
+    const erVeileder = useErVeileder();
+    const menuItemsGroup = erVeileder ? veilederAktivitetsValg(nyAktivitetBasePath) : brukerAktivitetsValg(nyAktivitetBasePath);
+    return (
+      <div className="self-stretch sm:self-auto">
+          <Dropdown>
+              <Button
+                as={Dropdown.Toggle}
+                loading={[Status.RELOADING, Status.PENDING].includes(aktivitetStatus)}
+                className="relative w-full"
+                icon={<PlusIcon role="img" aria-hidden fontSize="1.5rem" />}
+                disabled={viserHistoriskPeriode || aktivitetStatus === Status.ERROR}>
+                  Legg til aktivitet
+              </Button>
+              <Dropdown.Menu>
+                  {menuItemsGroup.map((item, index) => (
+                    <Dropdown.Menu.GroupedList key={index}>
+                        <Dropdown.Menu.GroupedList.Heading>
+                            {item.groupTittel}
+                        </Dropdown.Menu.GroupedList.Heading>
+                        {item.groupedItems.map((subItem, subIndex) => (
+                          <Dropdown.Menu.GroupedList.Item as={Link} to={subItem.link} key={subIndex}>
+                              {subItem.tittle}
+                          </Dropdown.Menu.GroupedList.Item>
+                        ))}
+                        {index != menuItemsGroup.length - 1  ? <Dropdown.Menu.Divider />: null}
+                    </Dropdown.Menu.GroupedList>
+                  ))}
+              </Dropdown.Menu>
+          </Dropdown>
+      </div>
+    );
+};
+
+export default LeggTilNyAktivitetsKort;
