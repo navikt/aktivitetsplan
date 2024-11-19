fddf47a452a31e98472ac3bc329ef810d1695575
erikb <erik.bosnes@nav.no>
2024-11-19 14:22:21 +0100

    legg til import for post

--- src/api/modiaContextHolder.ts
+++ src/api/modiaContextHolder.ts
@@ -1,4 +1,4 @@
-import { putAsJson } from './utils';
+import { postAsJson } from './utils';
 import { MODIA_CONTEXT_BASE_URL, VEILARBLEST_BASE_URL } from '../environment';
 
 export const lastAltPaaNyttMedNyBruker = (fnr: string): Promise<void> => postAsJson(`${MODIA_CONTEXT_BASE_URL}/context`, {eventType: "NY_AKTIV_BRUKER", verdi: fnr});
