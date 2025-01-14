83b3a648a94c4d97fdc8e408ead1f18c9057c183
johannetronstad <johanne.tronstad@nav.no>
2025-01-14 12:34:45 +0100

    Fiks skriveleif

--- src/moduler/aktivitet/visning/dele-cv/MeldInteresseForStilling.tsx
+++ src/moduler/aktivitet/visning/dele-cv/MeldInteresseForStilling.tsx
@@ -141,7 +141,7 @@ export const MeldInteresseForStilling = ({ aktivitet }: PropTypes) => {
                     <Radio value={SvarType.NEI.toString()}>{NeiSvarTekst}</Radio>
                 </RadioGroup>
                 <ReadMore header="Les om deling av CV">
-                    Arbeidsgiveren får tilgang til CV-en dersom du gir aksept til å dele og Nav deler den til arbeidsgiveren. En delt CV vil opphører å eksistere for arbeidsgiver dersom du ikke lenger følges opp av Nav for å skaffe jobb, eller senest seks måneder etter at Nav sendte CV-en til arbeidsgiver.
+                    Arbeidsgiveren får tilgang til CV-en dersom du gir aksept til å dele og Nav deler den til arbeidsgiveren. En delt CV vil opphøre å eksistere for arbeidsgiver dersom du ikke lenger følges opp av Nav for å skaffe jobb, eller senest seks måneder etter at Nav sendte CV-en til arbeidsgiver.
                     <br/><br/>
                     Hvis du angrer på at du ga aksept til å dele CV så kan du snakke med din veileder som kan trekke delingen av CV-en, slik at arbeidsgiver ikke lenger har tilgang til den. Hvis du oppdaterer CV-en din så er det alltid den siste versjonen arbeidsgiver vil se.
                 </ReadMore>
