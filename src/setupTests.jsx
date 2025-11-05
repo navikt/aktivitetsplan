1903892261972f930e579eed90689273028b8806
sigurdgroneng <sigurd.groneng@nav.no>
2025-11-05 08:52:16 +0100

    Make fetchHarFlereAktorId mock return a promise

--- src/setupTests.jsx
+++ src/setupTests.jsx
@@ -4,7 +4,7 @@ import.meta.env.VITE_API_URL_BASE = 'http://localhost:3000'; // Dette er det som
 
 HTMLDialogElement.prototype.showModal = () => {};
 window.IntersectionObserver = vi.fn();
-window.matchMedia = ()  => ({ matches: false, addEventListener: vi.fn(), removeEventListener: vi.fn() });
+window.matchMedia = () => ({ matches: false, addEventListener: vi.fn(), removeEventListener: vi.fn() });
 // Mocked because react-dnd uses es6 import and have to be transpiled to work in these tests
 vi.mock('react-dnd', () => ({
     useDrag: () => {
@@ -47,6 +47,6 @@ vi.mock('./api/oppfolgingAPI', async (importOriginal) => {
     const actual = await importOriginal();
     return {
         ...actual,
-        fetchHarFlereAktorId: vi.fn(),
+        fetchHarFlereAktorId: vi.fn(() => Promise.resolve()),
     };
 });
