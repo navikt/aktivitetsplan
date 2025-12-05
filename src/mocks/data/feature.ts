45b13dc3e52bf6d86978ffdc9b7bb420a256c367
Mads.Lee.Giil <mads.lee.giil@nav.no>
2025-12-05 20:08:50 +0100

    Vis journalføringsknapp lokalt

--- src/mocks/data/feature.ts
+++ src/mocks/data/feature.ts
@@ -2,4 +2,5 @@ import { Features } from '../../moduler/feature/feature';
 
 export const features: Features = {
     'aktivitetsplan.tryggtekst': true,
+    'aktivitetsplan.journalforing': true,
 };
