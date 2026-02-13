c10b438caea80ad011cce3beb00d167b9a8a98fe
sigurdgroneng <sigurd.groneng@nav.no>
2026-02-13 08:08:54 +0100

    Ikke kall contextholder for eksternbrukere

--- src/api/modiaContextHolder.ts
+++ src/api/modiaContextHolder.ts
@@ -1,4 +1,8 @@
 import { postAsJson } from './utils';
 import { MODIA_CONTEXT_BASE_URL } from '../environment';
+import { ER_INTERN_FLATE } from '../constant';
 
-export const lastAltPaaNyttMedNyBruker = (fnr: string): Promise<void> => postAsJson(`${MODIA_CONTEXT_BASE_URL}/context`, {eventType: "NY_AKTIV_BRUKER", verdi: fnr});
+export const lastAltPaaNyttMedNyBruker = async (fnr: string): Promise<void> => {
+    if (!ER_INTERN_FLATE) return;
+    return postAsJson(`${MODIA_CONTEXT_BASE_URL}/context`, { eventType: 'NY_AKTIV_BRUKER', verdi: fnr });
+};
