accb8e915ddbe65a1701785516e71c2ff901247d
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-03-18 12:16:30 +0100

    Slett eldre samtalereferatkladder når app mountes

--- src/moduler/aktivitet/aktivitet-forms/samtalereferat/useSamtalereferatKladd.ts
+++ src/moduler/aktivitet/aktivitet-forms/samtalereferat/useSamtalereferatKladd.ts
@@ -13,14 +13,31 @@ interface KladdInnslag {
     tidspunkt: number;
 }
 
+const localeStorageKeyPrefix = "samtalereferatKladd"
+
+export const slettGamleSamtalereferatKladder = () => {
+    const nå = Date.now();
+    const åtteTimerMillis = 3600000 * 8;
+    const lagredeItems = Object.keys(localStorage).filter((key) => key.startsWith(localeStorageKeyPrefix));
+
+    lagredeItems.forEach((key) => {
+        const kladdInnslag = localStorage.getItem(key);
+        if (kladdInnslag) {
+            const parsedKladdInnslag: KladdInnslag = JSON.parse(kladdInnslag);
+            const harUtlopt = (nå - åtteTimerMillis) > parsedKladdInnslag.tidspunkt;
+            if (harUtlopt) {
+                localStorage.removeItem(key);
+            }
+        }
+    })
+}
+
 export const useSamtalereferatKladd = (brukerFnr: string, aktivitetId?: string) => {
     const debouncedDelay = 500;
     const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
-    const localeStorageKeyPrefix = "samtalereferatKladd"
     const localStorageKey = aktivitetId ? `${localeStorageKeyPrefix}-${aktivitetId}` : `${localeStorageKeyPrefix}-${brukerFnr}`;
 
     const lagreSamtalereferatKladd = useCallback((samtalereferat: SamtalereferatKladd) => {
-        slettEldreSamtalereferatKladder();
         clearTimeout(timeoutRef.current);
         timeoutRef.current = setTimeout(() => {
             const kladdInnslag = {samtalereferat, tidspunkt: Date.now()}
@@ -29,7 +46,6 @@ export const useSamtalereferatKladd = (brukerFnr: string, aktivitetId?: string) 
     }, [localStorageKey]);
 
     const lagreSamtalereferatKladdLagretAktivitet = useCallback((referatKladd: string) => {
-        slettEldreSamtalereferatKladder();
         clearTimeout(timeoutRef.current);
         timeoutRef.current = setTimeout(() => {
             localStorage.setItem(localStorageKey, JSON.stringify(referatKladd));
@@ -57,23 +73,6 @@ export const useSamtalereferatKladd = (brukerFnr: string, aktivitetId?: string) 
 
     const slettSamtaleReferatKladd = () => {
         localStorage.removeItem(localStorageKey);
-    }
-
-    const slettEldreSamtalereferatKladder = () => {
-        const nå = Date.now();
-        const åtteTimerMillis = 3600000 * 8;
-        const lagredeItems = Object.keys(localStorage).filter((key) => key.startsWith(localeStorageKeyPrefix));
-
-        lagredeItems.forEach((key) => {
-            const kladdInnslag = localStorage.getItem(key);
-            if (kladdInnslag) {
-                const parsedKladdInnslag: KladdInnslag = JSON.parse(kladdInnslag);
-                const harUtlopt = (nå - åtteTimerMillis) > parsedKladdInnslag.tidspunkt;
-                if (harUtlopt) {
-                    localStorage.removeItem(key);
-                }
-            }
-        })
     }
 
     return { lagreSamtalereferatKladd, lagreSamtalereferatKladdLagretAktivitet, hentSamtaleReferatKladd, slettSamtaleReferatKladd, hentSamtaleReferatKladdLagretAktivitet};
