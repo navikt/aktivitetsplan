e4cd250b2fd5182327efac811ffd3390618d3481
johannetronstad <johanne.tronstad@nav.no>
2026-07-07 14:20:42 +0200

    fjern vm stripe 🥲

--- src/BasePage.tsx
+++ src/BasePage.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import OppfolgingStatus from './moduler/oppfolging-status/OppfolgingStatus';
-import InternVmAlertStripe from './moduler/varslinger/InternVmAlertStripe';
 
 export const BasePage = () => {
     return (
         <OppfolgingStatus>
-            <InternVmAlertStripe />
             <Outlet />
         </OppfolgingStatus>
     );
