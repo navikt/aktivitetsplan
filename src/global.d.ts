c319ce3e8fcf8dc477433d31f577d7403565493c
Sigurd Grøneng <sigurd.groneng@nav.no>
2026-07-07 13:10:03 +0200

    Bruk bedre Object.entries typer fra type-fest (#1455)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite-plugin-svgr/client" />
 
+import { Entries } from './types/entries';
+
 declare module '*.module.less';
 
 declare module '*.png';
@@ -11,3 +13,9 @@ declare module '*.svg' {
 // https://vitejs.dev/guide/features.html#css
 declare module '*.less?inline';
 declare module '*.css?inline';
+
+declare global {
+    interface ObjectConstructor {
+        entries<T extends object>(o: T): Entries<T>;
+    }
+}
