758bbc1394a5da65fec684e32712710cb0b5b669
sigurdgroneng <sigurd.groneng@nav.no>
2026-07-15 16:22:38 +0200

    Fjern setUser

--- src/rootWrapper.tsx
+++ src/rootWrapper.tsx
@@ -5,17 +5,14 @@ import './apmInit';
 import React from 'react';
 
 import App from './app';
-import { hash } from './felles-komponenter/utils/hash';
 import Provider from './Provider';
 import { createRoot } from 'react-dom/client';
 import { createRouterWithWrapper } from './routing/routerConfig';
-import { setUser } from '@nais/apm';
 
 const createRoutesForUser = createRouterWithWrapper();
 
 export const renderAsReactRoot = (appElement: HTMLElement, props?: { fnr?: string }) => {
     const rootElement = createRoot(appElement || document.getElementById('root')!);
-    setUser({ id: hash(props?.fnr) });
     rootElement.render(
         <Provider fnr={props?.fnr}>
             <App createRoutesForUser={createRoutesForUser} key={'1'} />
