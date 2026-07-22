b0746a48312f5b1673087be43770f531d99f4ac4
sigurdgroneng <sigurd.groneng@nav.no>
2026-07-22 10:04:12 +0200

    Update ignore error, they appear different in faro vs sentry

--- src/apmInit.ts
+++ src/apmInit.ts
@@ -8,10 +8,9 @@ init({
     // Don't check if version is automatically resolved before enabling or removing
     // version: import.meta.env.VITE_RELEASE,
     ignoreErrors: [
-        /^Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.$/,
-        /^Uventet feil fra dekoratøren: NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node. \[object Object]$/,
-        /^Uventet feil fra dekoratøren: NotFoundError: The object can not be found here. \[object Object]$/,
-        /^The object can not be found here.$/,
+        /Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.$/,
+        /Failed to fetch content for main-menu$/,
+        /^NotFoundError: The object can not be found here.$/,
     ],
     tracing: true,
     sessionReplay: { enabled: false },
