4031bc689099c58271f9f9a8e09c5eb5bb06f503
sigurdgroneng <sigurd.groneng@nav.no>
2026-08-06 13:18:32 +0200

    Vis at referat har kladd på aktivitetskortvisning

--- src/moduler/aktivitet/visning/referat/ReferatVisning.tsx
+++ src/moduler/aktivitet/visning/referat/ReferatVisning.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Heading } from '@navikt/ds-react';
+import { Alert, Button, Heading, HelpText, Tag } from '@navikt/ds-react';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -7,8 +7,10 @@ import { div as HiddenIfDiv } from '../../../../felles-komponenter/hidden-if/hid
 import { useErVeileder } from '../../../../Provider';
 import { selectPubliserReferatFeil } from '../../../feilmelding/feil-selector';
 import Feilmelding from '../../../feilmelding/Feilmelding';
+import { useSamtalereferatKladd } from '../../aktivitet-forms/samtalereferat/useSamtalereferatKladd';
 
 interface Props {
+    aktivitetId: string;
     referat: string;
     erAktivAktivitet: boolean;
     dispatchPubliserReferat: () => void;
@@ -24,11 +26,25 @@ const ReferatVisning = (props: Props) => {
 
     const feil = useSelector(selectPubliserReferatFeil);
 
+    const { hentSamtaleReferatKladdLagretAktivitet } = useSamtalereferatKladd({ aktivitetId: props.aktivitetId });
+    const kladd = hentSamtaleReferatKladdLagretAktivitet();
+
     return (
         <section className="my-4 border-t border-ax-border-neutral pt-8">
-            <Heading level="2" size="large" className="mb-4">
-                Samtalereferat
-            </Heading>
+            <div className="flex justify-between">
+                <Heading level="2" size="large" className="mb-4">
+                    Samtalereferat
+                </Heading>
+                {kladd ? (
+                    <div className="flex gap-2 items-center mb-4">
+                        <Tag data-color="meta-purple">Referatet inneholder en kladd</Tag>
+                        <HelpText title="Hvor kommer dette fra?">
+                            Du har en ulagret kladd på dette referatet i nettleseren. Vi har tatt vare på den for deg
+                            😉. Klikk 'Endre referat' for å se den ulagrede endringen.
+                        </HelpText>
+                    </div>
+                ) : null}
+            </div>
             <EkspanderbartTekstomrade tekst={referat} antallTegn={275} />
             {!erAktivAktivitet && !erReferatPublisert ? (
                 <Button hidden={erReferatPublisert} onClick={dispatchPubliserReferat} loading={publiserer}>
